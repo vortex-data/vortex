8ea500e09f233470b0c439330a021d35cf29d21a
Andrew Duffy <andreweduffy@gmail.com>
2024-11-04 14:52:10 -0500

    just an example of world's dumbest buffer pool

--- vortex-serde/src/io/read.rs
+++ vortex-serde/src/io/read.rs
@@ -140,3 +140,93 @@ impl VortexReadAt for Buffer {
         self.len() as u64
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use bytes::Bytes;
+
+    /// A read-only wrapper around a `Bytes` that holds some extra bookkeeping information
+    /// to enable possible reuse of the allocation.
+    struct BytesGuard {
+        inner: Option<bytes::Bytes>,
+
+        // Custom deallocator. Allows for reuse of the allocated inner Bytes.
+        dealloc: fn(Bytes, [u64; 2]),
+
+        // an opaque 16 bytes of data that allow custom deallocator to store bookkeeping.
+        user_data: [u64; 2],
+    }
+
+    // We can call the custom Drop implementation, if applicable.
+    impl Drop for BytesGuard {
+        fn drop(&mut self) {
+            // take the bytes. This hands ownership of the allocation to the custom deallocator
+            let bytes = self.inner.take().expect("bytes must be present");
+            (self.dealloc)(bytes, self.user_data);
+        }
+    }
+
+    /// deallocation that puts the bytes back into the buffer pool
+    fn dealloc_pool(bytes: Bytes, user_data: [u64; 2]) {
+        // user_data contains two values.
+        // the first is a pointer to a `BufferPool`.
+        // The second is an index for the buffer in the pool
+        let pool: &mut BufferPool = unsafe { &mut *(user_data[0] as *mut _) };
+        let index = user_data[1] as usize;
+        pool.insert(bytes, index);
+    }
+
+    struct BufferPool {
+        // All of the buffers
+        buffers: Vec<Option<Bytes>>,
+    }
+
+    impl BufferPool {
+        pub fn get(&mut self) -> BytesGuard {
+            // dumbest implementation ever: find the first available bytes instance.
+            for (idx, buffer) in self.buffers.iter_mut().enumerate() {
+                if let Some(buffer) = buffer.take() {
+                    let ptr = self as *const _ as u64;
+                    let offset = idx as u64;
+                    return BytesGuard {
+                        inner: Some(buffer),
+                        dealloc: dealloc_pool,
+                        user_data: [ptr, offset],
+                    };
+                }
+            }
+
+            // in real life we'd grow the pool or block or something
+            panic!("the pool has no more buffers")
+        }
+
+        pub fn insert(&mut self, bytes: Bytes, idx: usize) {
+            println!("inserting buffer of len={} @ {idx}", bytes.len(),);
+            if self.buffers[idx].is_some() {
+                panic!("something is horribly wrong!");
+            } else {
+                self.buffers[idx] = Some(bytes);
+            }
+        }
+    }
+
+    #[test]
+    fn test_bytes() {
+        let mut pool = BufferPool {
+            buffers: vec![
+                Some(Bytes::copy_from_slice(b"this is first buffer")),
+                Some(Bytes::copy_from_slice(b"this is second larger buffer")),
+            ],
+        };
+
+        let buffer1 = pool.get();
+        {
+            // this drop is called.
+            let buffer2 = pool.get();
+            println!("buffer2 drop will be called next");
+            drop(buffer2);
+        }
+        println!("buffer1 drop will be called next");
+        drop(buffer1);
+    }
+}
