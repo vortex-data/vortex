e9facf9fdcbb9af7da96e16c24e9eb87cce46ad9
Adam Gutglick <adam@spiraldb.com>
2024-11-14 22:00:15 +0000

    more benchmarks

--- bench-vortex/src/tpch/execute.rs
+++ bench-vortex/src/tpch/execute.rs
@@ -1,7 +1,6 @@
 use arrow_array::RecordBatch;
 use datafusion::prelude::SessionContext;
 use datafusion_common::Result;
-use datafusion_physical_plan::collect;
 
 use crate::tpch::Format;
 
@@ -44,9 +43,11 @@ pub async fn run_tpch_query(
 
 pub async fn execute_query(ctx: &SessionContext, query: &str) -> Result<Vec<RecordBatch>> {
     let plan = ctx.sql(query).await?;
-    let (state, plan) = plan.into_parts();
-    let optimized = state.optimize(&plan)?;
-    let physical_plan = state.create_physical_plan(&optimized).await?;
-    let result = collect(physical_plan.clone(), state.task_ctx()).await?;
-    Ok(result)
+    plan.collect().await
+    // use datafusion_physical_plan::collect;
+    // let (state, plan) = plan.into_parts();
+    // let optimized = state.optimize(&plan)?;
+    // let physical_plan = state.create_physical_plan(&optimized).await?;
+    // let result = collect(physical_plan.clone(), state.task_ctx()).await?;
+    // Ok(result)
 }
