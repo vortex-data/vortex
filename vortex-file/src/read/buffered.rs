a93d82d2ae441c2072a4b76074f23eb8a843a740
Daniel King <dan@spiraldb.com>
2024-11-19 17:37:15 -0500

    binary search for split

--- vortex-file/src/read/buffered.rs
+++ vortex-file/src/read/buffered.rs
@@ -4,7 +4,7 @@ use std::mem;
 use vortex_array::array::ChunkedArray;
 use vortex_array::compute::unary::scalar_at;
 use vortex_array::{ArrayDType, ArrayData, IntoArrayData};
-use vortex_error::{VortexExpect as _, VortexResult};
+use vortex_error::{vortex_bail, VortexExpect as _, VortexResult};
 use vortex_scalar::BoolScalar;
 
 use crate::read::mask::RowMask;
@@ -104,21 +104,17 @@ impl BufferedLayoutReader {
             return Ok(false);
         };
 
-        for (index, (chunk_begin, chunk_end)) in self.splits.iter() {
-            if !(*chunk_begin == begin && *chunk_end == end) {
-                continue;
-            }
+        let needle = (begin, end);
+        let Ok(index) = self
+            .splits
+            .binary_search_by(|(_, probe)| probe.cmp(&needle))
+        else {
+            vortex_bail!("could not find {}-{} in this layout reader", begin, end)
+        };
 
-            let chunk_is_pruned = BoolScalar::try_from(&scalar_at(chunk_mask, *index)?)?
-                .value()
-                .vortex_expect("chunk_mask should be nonnullable");
-            // println!(
-            //     "is_pruned: index {} {}-{} {}",
-            //     index, begin, end, chunk_is_pruned
-            // );
-            return Ok(chunk_is_pruned);
-        }
-        // println!("could not find {} {} in layouts", begin, end);
-        Ok(false)
+        let chunk_is_pruned = BoolScalar::try_from(&scalar_at(chunk_mask, index)?)?
+            .value()
+            .vortex_expect("chunk_mask should be nonnullable");
+        Ok(chunk_is_pruned)
     }
 }
