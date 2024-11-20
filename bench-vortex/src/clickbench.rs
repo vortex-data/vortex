0185944a508576b453695313c6ebea9516b98a57
Robert Kruszewski <github@robertk.io>
2024-11-20 11:28:11 +0000

    new

--- bench-vortex/src/clickbench.rs
+++ bench-vortex/src/clickbench.rs
@@ -2,18 +2,20 @@ use std::path::Path;
 use std::sync::{Arc, LazyLock};
 
 use arrow_schema::{DataType, Field, Schema, TimeUnit};
-use datafusion::execution::object_store::ObjectStoreUrl;
+use datafusion::datasource::listing::{
+    ListingOptions, ListingTable, ListingTableConfig, ListingTableUrl,
+};
 use datafusion::prelude::{ParquetReadOptions, SessionContext};
 use tokio::fs::{create_dir_all, OpenOptions};
 use vortex::aliases::hash_map::HashMap;
 use vortex::array::{ChunkedArray, StructArray};
 use vortex::dtype::DType;
+use vortex::error::vortex_err;
 use vortex::file::{VortexFileWriter, VORTEX_FILE_EXTENSION};
 use vortex::sampling_compressor::SamplingCompressor;
 use vortex::variants::StructArrayTrait;
 use vortex::{ArrayDType, ArrayData, IntoArrayData, IntoArrayVariant};
-use vortex_datafusion::persistent::config::{VortexFile, VortexTableOptions};
-use vortex_datafusion::SessionContextExt;
+use vortex_datafusion::persistent::format::VortexFormat;
 
 use crate::{idempotent_async, CTX};
 
@@ -211,27 +213,20 @@ pub async fn register_vortex_file(
     })
     .await?;
 
-    let f = OpenOptions::new()
-        .read(true)
-        .write(true)
-        .open(&vtx_file)
-        .await?;
-    let file_size = f.metadata().await?.len();
-
-    let schema_ref = Arc::new(schema.clone());
-
-    session.register_disk_vortex_opts(
-        table_name,
-        ObjectStoreUrl::local_filesystem(),
-        VortexTableOptions::new(
-            schema_ref,
-            vec![VortexFile::new(
-                vtx_file.to_str().unwrap().to_string(),
-                file_size,
-            )],
-            CTX.clone(),
-        ),
+    let format = Arc::new(VortexFormat::new(&CTX));
+    let table_url = ListingTableUrl::parse(
+        vtx_file
+            .to_str()
+            .ok_or_else(|| vortex_err!("Path is not valid UTF-8"))?,
     )?;
+    let config = ListingTableConfig::new(table_url)
+        .with_listing_options(ListingOptions::new(format as _))
+        .infer_schema(&session.state())
+        .await?;
+
+    let listing_table = Arc::new(ListingTable::try_new(config)?);
+
+    session.register_table(table_name, listing_table as _)?;
 
     Ok(())
 }
