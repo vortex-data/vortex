184f03855ba1aaae056c83f5c1bbd3dcd711b638
Andrew Duffy <andreweduffy@gmail.com>
2024-11-22 14:39:16 -0500

    fix: allocate aligned buffers when reading through ObjectStore

--- vortex-io/src/object_store.rs
+++ vortex-io/src/object_store.rs
@@ -13,6 +13,52 @@ use vortex_buffer::{Alignment, ByteBuffer, ByteBufferMut};
 use vortex_error::{VortexExpect, VortexResult};
 
 use crate::{IoBuf, VortexReadAt, VortexWrite};
+// use bytes::Bytes;
+// use futures_util::StreamExt;
+// use object_store::path::Path;
+// use object_store::{GetOptions, GetRange, ObjectStore, WriteMultipart};
+// use vortex_buffer::io_buf::IoBuf;
+// use vortex_buffer::Buffer;
+// use vortex_error::{vortex_panic, VortexError, VortexResult};
+
+// use crate::{VortexBufReader, VortexReadAt, VortexWrite, BUFFER_ALIGNMENT};
+
+// pub trait ObjectStoreExt {
+//     fn vortex_read(
+//         &self,
+//         location: &Path,
+//         range: Range<usize>,
+//     ) -> impl Future<Output = VortexResult<VortexBufReader<impl VortexReadAt>>>;
+
+//     fn vortex_reader(&self, location: &Path) -> impl VortexReadAt;
+
+//     fn vortex_writer(
+//         &self,
+//         location: &Path,
+//     ) -> impl Future<Output = VortexResult<impl VortexWrite>>;
+// }
+
+// impl ObjectStoreExt for Arc<dyn ObjectStore> {
+//     async fn vortex_read(
+//         &self,
+//         location: &Path,
+//         range: Range<usize>,
+//     ) -> VortexResult<VortexBufReader<impl VortexReadAt>> {
+//         let bytes = self.get_range(location, range).await?;
+//         Ok(VortexBufReader::new(Buffer::from(bytes)))
+//     }
+
+//     fn vortex_reader(&self, location: &Path) -> impl VortexReadAt {
+//         ObjectStoreReadAt::new(self.clone(), location.clone())
+//     }
+
+//     async fn vortex_writer(&self, location: &Path) -> VortexResult<impl VortexWrite> {
+//         Ok(ObjectStoreWriter::new(WriteMultipart::new_with_chunk_size(
+//             self.put_multipart(location).await?,
+//             10 * 1024 * 1024,
+//         )))
+//     }
+// }
 
 #[derive(Clone)]
 pub struct ObjectStoreReadAt {
@@ -38,6 +84,7 @@ impl VortexReadAt for ObjectStoreReadAt {
     ) -> io::Result<ByteBuffer> {
         let object_store = self.object_store.clone();
         let location = self.location.clone();
+
         let start = usize::try_from(range.start).vortex_expect("range.start");
         let end = usize::try_from(range.end).vortex_expect("range.end");
         let len: usize = end - start;
