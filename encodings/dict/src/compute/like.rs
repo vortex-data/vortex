4847ea9257c525026275df3d699d116aac933be9
Nicholas Gates <nick@nickgates.com>
2024-12-02 15:29:56 -0500

    SQL LIKE

--- encodings/dict/src/compute/like.rs
+++ encodings/dict/src/compute/like.rs
@@ -7,6 +7,6 @@ use crate::{DictArray, DictEncoding};
 impl LikeFn<DictArray> for DictEncoding {
     fn like(&self, array: &DictArray, pattern: &ArrayData) -> VortexResult<ArrayData> {
         let values = like(&array.values(), pattern)?;
-        Ok(DictArray::try_new(array.codes().clone(), values)?.into_array())
+        Ok(DictArray::try_new(array.codes(), values)?.into_array())
     }
 }
