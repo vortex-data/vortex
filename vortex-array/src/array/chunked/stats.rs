db38e31d43c15f874db1be82ea810b331d637077
Will Manning <will@willmanning.io>
2024-12-03 18:02:52 -0500

    revert dumb thing

--- vortex-array/src/array/chunked/stats.rs
+++ vortex-array/src/array/chunked/stats.rs
@@ -6,14 +6,6 @@ use crate::stats::{ArrayStatistics, Stat, StatisticsVTable, StatsSet};
 
 impl StatisticsVTable<ChunkedArray> for ChunkedEncoding {
     fn compute_statistics(&self, array: &ChunkedArray, stat: Stat) -> VortexResult<StatsSet> {
-        if stat == Stat::IsConstant {
-            // Short-circuiting implementation of is_constant
-            return Ok(StatsSet::from_iter([(
-                Stat::IsConstant,
-                is_constant(array).into(),
-            )]));
-        }
-
         // for UncompressedSizeInBytes, we end up with sum of chunk uncompressed sizes
         // this ignores the `chunk_offsets` array child, so it won't exactly match self.nbytes()
         Ok(array
@@ -29,8 +21,4 @@ impl StatisticsVTable<ChunkedArray> for ChunkedEncoding {
             })
             .unwrap_or_default())
     }
-}
-
-fn is_constant(array: &ChunkedArray) -> bool {
-    array.chunks().all(|c| c.is_constant())
 }
