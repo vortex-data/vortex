7554c1a1abd52998da2ee9f896f00e24e96fc4dc
Robert Kruszewski <github@robertk.io>
2024-12-06 10:04:31 -0500

    test

--- vortex-array/src/array/chunked/compute/fill_null.rs
+++ vortex-array/src/array/chunked/compute/fill_null.rs
@@ -12,8 +12,36 @@ impl FillNullFn<ChunkedArray> for ChunkedEncoding {
                 .chunks()
                 .map(|c| fill_null(c, fill_value.clone()))
                 .collect::<VortexResult<Vec<_>>>()?,
-            array.dtype().clone(),
+            array.dtype().as_nonnullable(),
         )
         .map(|a| a.into_array())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use arrow_buffer::BooleanBuffer;
+    use vortex_dtype::{DType, Nullability};
+
+    use crate::array::{BoolArray, ChunkedArray};
+    use crate::compute::fill_null;
+    use crate::validity::Validity;
+    use crate::{ArrayDType, IntoArrayData};
+
+    #[test]
+    fn fill_null_chunks() {
+        let chunked = ChunkedArray::try_new(
+            vec![
+                BoolArray::try_new(BooleanBuffer::new_set(5), Validity::AllInvalid)
+                    .unwrap()
+                    .into_array(),
+                BoolArray::new(BooleanBuffer::new_set(5), Nullability::Nullable).into_array(),
+            ],
+            DType::Bool(Nullability::Nullable),
+        )
+        .unwrap();
+
+        let filled = fill_null(chunked, false.into()).unwrap();
+        assert_eq!(*filled.dtype(), DType::Bool(Nullability::NonNullable));
     }
 }
