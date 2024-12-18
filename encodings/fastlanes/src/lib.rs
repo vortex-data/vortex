b54f54690cf392a346dfe0e65ce3cb6e502273f1
Nicholas Gates <nick@nickgates.com>
2024-12-18 19:30:55 +0000

    Buffer alignment

--- encodings/fastlanes/src/lib.rs
+++ encodings/fastlanes/src/lib.rs
@@ -11,7 +11,3 @@ pub use r#for::*;
 mod bitpacking;
 mod delta;
 mod r#for;
-
-/// FastLanes is built around the idea of 1024-bit virtual SIMD registers, therefore we enforce
-/// an alignment of 128 bytes.
-pub(crate) const FASTLANES_ALIGNMENT: usize = 128;
