105070289631fcbfc461188348d58e75131a5683
Daniel King <dan@spiraldb.com>
2024-12-18 20:14:22 -0500

    slim down diff

--- vortex-array/src/array/chunked/canonical.rs
+++ vortex-array/src/array/chunked/canonical.rs
@@ -156,7 +156,7 @@ fn pack_lists(chunks: &[ArrayData], validity: Validity, dtype: &DType) -> Vortex
                 .maybe_null_slice::<i64>()
                 .iter()
                 .skip(1)
-                .map(|off| *off + adjustment_from_previous - first_offset_value as i64),
+                .map(|off| off + adjustment_from_previous - first_offset_value as i64),
         );
     }
     let chunked_elements = ChunkedArray::try_new(elements, elem_dtype.clone())?.into_array();
