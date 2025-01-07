06930e0a176d94e98973c63c565c6e707a0ce192
Andrew Duffy <andrew@a10y.dev>
2025-01-07 13:27:14 -0500

    initial steps with FromAvro / ToAvro

---
+++ proc-macro-traits/src/lib.rs
@@ -0,0 +1,322 @@
+#![allow(unused)]
+use std::collections::HashMap;
+use std::io::Cursor;
+
+use apache_avro::schema::UnionSchema;
+use apache_avro::types::Value;
+use apache_avro::{from_avro_datum, to_avro_datum, BigDecimal, Decimal, Duration, Schema};
+use uuid::Uuid;
+use vortex_error::{vortex_bail, vortex_err, VortexError, VortexResult};
+
+/// AvroValue is based on `Value` from the Avro crate, but without the blanket impls. This is so we have control over how the
+/// conversions for primitives are implemented.
+#[derive(Debug)]
+pub enum AvroValue {
+    Null,
+    Boolean(bool),
+    Int(i32),
+    Float(f32),
+    Double(f64),
+    Bytes(Vec<u8>),
+    Fixed(usize, Vec<u8>),
+    Enum(u32, String),
+    Union(u32, Box<AvroValue>),
+    Array(Vec<AvroValue>),
+    Map(HashMap<String, AvroValue>),
+    Record(Vec<(String, AvroValue)>),
+    Date(i32),
+    Decimal(Decimal),
+    BigDecimal(BigDecimal),
+    TimeMillis(i32),
+    TimeMicros(i64),
+    TimestampMillis(i64),
+    TimestampMicros(i64),
+    TimestampNanos(i64),
+    LocalTimestampMillis(i64),
+    LocalTimestampMicros(i64),
+    LocalTimestampNanos(i64),
+    Duration(Duration),
+    Uuid(Uuid),
+    Long(i64),
+    String(String),
+}
+
+// Helper conversion into our AvroValue type from upstream `apache_avro::Value`.
+impl From<Value> for AvroValue {
+    fn from(value: Value) -> Self {
+        match value {
+            Value::Long(i) => AvroValue::Long(i),
+            Value::String(s) => AvroValue::String(s),
+            Value::Int(i) => AvroValue::Int(i),
+            Value::Null => AvroValue::Null,
+            Value::Boolean(b) => AvroValue::Boolean(b),
+            Value::Float(f) => AvroValue::Float(f),
+            Value::Double(d) => AvroValue::Double(d),
+            Value::Bytes(b) => AvroValue::Bytes(b),
+            Value::Fixed(size, bytes) => AvroValue::Fixed(size, bytes),
+            Value::Enum(i, s) => AvroValue::Enum(i, s),
+            Value::Union(i, v) => AvroValue::Union(i, Box::new((*v).into())),
+            Value::Array(items) => {
+                AvroValue::Array(items.into_iter().map(AvroValue::from).collect())
+            }
+            Value::Map(items) => {
+                AvroValue::Map(items.into_iter().map(|(k, v)| (k, v.into())).collect())
+            }
+            Value::Record(items) => {
+                AvroValue::Record(items.into_iter().map(|(k, v)| (k, v.into())).collect())
+            }
+            Value::Date(d) => AvroValue::Date(d),
+            Value::Decimal(d) => AvroValue::Decimal(d),
+            Value::BigDecimal(d) => AvroValue::BigDecimal(d),
+            Value::TimeMillis(t) => AvroValue::TimeMillis(t),
+            Value::TimeMicros(t) => AvroValue::TimeMicros(t),
+            Value::TimestampMillis(t) => AvroValue::TimestampMillis(t),
+            Value::TimestampMicros(t) => AvroValue::TimestampMicros(t),
+            Value::TimestampNanos(t) => AvroValue::TimestampNanos(t),
+            Value::LocalTimestampMillis(t) => AvroValue::LocalTimestampMillis(t),
+            Value::LocalTimestampMicros(t) => AvroValue::LocalTimestampMicros(t),
+            Value::LocalTimestampNanos(t) => AvroValue::LocalTimestampNanos(t),
+            Value::Duration(d) => AvroValue::Duration(d),
+            Value::Uuid(u) => AvroValue::Uuid(u),
+        }
+    }
+}
+
+// Helper conversion into upstream `apache_avro::Value` from our `AvroValue` type.
+impl From<AvroValue> for Value {
+    fn from(value: AvroValue) -> Self {
+        match value {
+            AvroValue::Long(i) => Value::Long(i),
+            AvroValue::String(s) => Value::String(s),
+            AvroValue::Int(i) => Value::Int(i),
+            AvroValue::Null => Value::Null,
+            AvroValue::Boolean(b) => Value::Boolean(b),
+            AvroValue::Float(f) => Value::Float(f),
+            AvroValue::Double(d) => Value::Double(d),
+            AvroValue::Bytes(b) => Value::Bytes(b),
+            AvroValue::Fixed(size, bytes) => Value::Fixed(size, bytes),
+            AvroValue::Enum(i, s) => Value::Enum(i, s),
+            AvroValue::Union(i, v) => Value::Union(i, Box::new((*v).into())),
+            AvroValue::Array(items) => Value::Array(items.into_iter().map(Value::from).collect()),
+            AvroValue::Map(items) => {
+                Value::Map(items.into_iter().map(|(k, v)| (k, v.into())).collect())
+            }
+            AvroValue::Record(items) => {
+                Value::Record(items.into_iter().map(|(k, v)| (k, v.into())).collect())
+            }
+            AvroValue::Date(d) => Value::Date(d),
+            AvroValue::Decimal(d) => Value::Decimal(d),
+            AvroValue::BigDecimal(d) => Value::BigDecimal(d),
+            AvroValue::TimeMillis(t) => Value::TimeMillis(t),
+            AvroValue::TimeMicros(t) => Value::TimeMicros(t),
+            AvroValue::TimestampMillis(t) => Value::TimestampMillis(t),
+            AvroValue::TimestampMicros(t) => Value::TimestampMicros(t),
+            AvroValue::TimestampNanos(t) => Value::TimestampNanos(t),
+            AvroValue::LocalTimestampMillis(t) => Value::LocalTimestampMillis(t),
+            AvroValue::LocalTimestampMicros(t) => Value::LocalTimestampMicros(t),
+            AvroValue::LocalTimestampNanos(t) => Value::LocalTimestampNanos(t),
+            AvroValue::Duration(d) => Value::Duration(d),
+            AvroValue::Uuid(u) => Value::Uuid(u),
+        }
+    }
+}
+
+/// Types that can be converted into an Avro value type.
+///
+/// Types must provide a conversion into [`AvroValue`], the type-erased value type
+/// for the Avro binary format.
+///
+/// Additionally, types must provide a schema that can be used to write the type to the Avro binary format.
+pub trait ToAvro: Into<AvroValue> {
+    fn write_schema() -> Schema;
+}
+
+/// Types that can be deserialized from an Avro binary format.
+///
+/// Types must provide a conversion from [`AvroValue`], the type-erased value type
+/// for the Avro binary format.
+///
+/// Additionally, types must provide a schema that can be used to read the type from the Avro binary format.
+pub trait FromAvro: TryFrom<AvroValue, Error = VortexError> {
+    /// Retrieve the Avro schema that is used to read this type from the Avro binary format.
+    fn read_schema() -> Schema;
+}
+
+/// Convert a type into the Avro binary format.
+///
+/// This function will return an error if the type cannot be converted into the Avro binary format.
+pub fn to_avro_binary<T: ToAvro>(value: T) -> VortexResult<Vec<u8>> {
+    let avro_value: AvroValue = value.into();
+    to_avro_datum(&T::write_schema(), avro_value)
+        .map_err(|err| vortex_err!("Failed to convert type to Avro binary format: {err}"))
+}
+
+/// Read into a type from the Avro binary format.
+///
+/// This function will return an error if the type cannot be read from the Avro binary format.
+pub fn from_avro_binary<T: FromAvro>(schema: &Schema, avro_bytes: Vec<u8>) -> VortexResult<T> {
+    let value = from_avro_datum(schema, &mut Cursor::new(avro_bytes), None)
+        .map_err(|err| vortex_err!("Failed to read type from Avro binary format: {err}"))?;
+    <T as TryFrom<AvroValue>>::try_from(value.into()).map_err(|err| err.into())
+}
+
+macro_rules! impl_from_prim {
+    ($ty:ty, $inner:ty, $value_variant:path, $schema_variant:path) => {
+        impl From<$ty> for AvroValue {
+            fn from(value: $ty) -> Self {
+                $value_variant(value as $inner)
+            }
+        }
+
+        impl ToAvro for $ty {
+            fn write_schema() -> Schema {
+                $schema_variant
+            }
+        }
+
+        impl TryFrom<AvroValue> for $ty {
+            type Error = VortexError;
+
+            fn try_from(value: AvroValue) -> Result<Self, Self::Error> {
+                if let $value_variant(v) = value.into() {
+                    Ok(v as $ty)
+                } else {
+                    Err(vortex_err!(
+                        "Expected value to be a {} but it was not",
+                        stringify!($value_variant)
+                    ))
+                }
+            }
+        }
+
+        impl FromAvro for $ty {
+            fn read_schema() -> Schema {
+                $schema_variant
+            }
+        }
+    };
+}
+
+impl_from_prim!(i8, i32, AvroValue::Int, Schema::Int);
+impl_from_prim!(i16, i32, AvroValue::Int, Schema::Int);
+impl_from_prim!(i32, i32, AvroValue::Int, Schema::Int);
+impl_from_prim!(u8, i32, AvroValue::Int, Schema::Int);
+impl_from_prim!(u16, i32, AvroValue::Int, Schema::Int);
+impl_from_prim!(u32, i32, AvroValue::Int, Schema::Int);
+impl_from_prim!(i64, i64, AvroValue::Long, Schema::Long);
+impl_from_prim!(u64, i64, AvroValue::Long, Schema::Long);
+
+impl From<String> for AvroValue {
+    fn from(value: String) -> Self {
+        AvroValue::String(value)
+    }
+}
+
+impl ToAvro for String {
+    fn write_schema() -> Schema {
+        Schema::String
+    }
+}
+
+impl TryFrom<AvroValue> for String {
+    type Error = VortexError;
+
+    fn try_from(value: AvroValue) -> Result<Self, Self::Error> {
+        if let AvroValue::String(s) = value {
+            Ok(s)
+        } else {
+            Err(vortex_err!("Expected value to be a String but it was not"))
+        }
+    }
+}
+
+impl FromAvro for String {
+    fn read_schema() -> Schema {
+        Schema::String
+    }
+}
+
+// Handling for Option<T>
+impl<T> From<Option<T>> for AvroValue
+where
+    T: Into<AvroValue>,
+{
+    fn from(value: Option<T>) -> Self {
+        match value {
+            Some(v) => AvroValue::Union(1, Box::new(v.into())),
+            None => AvroValue::Union(0, Box::new(AvroValue::Null)),
+        }
+    }
+}
+
+impl<T> TryFrom<AvroValue> for Option<T>
+where
+    T: FromAvro,
+{
+    type Error = T::Error;
+
+    fn try_from(value: AvroValue) -> Result<Self, Self::Error> {
+        if let AvroValue::Union(idx, value) = value {
+            if idx == 0 {
+                Ok(None)
+            } else {
+                Ok(Some(T::try_from(*value)?))
+            }
+        } else {
+            vortex_bail!("Option<T> Avro binary value must be a union with a null and the type T")
+        }
+    }
+}
+
+impl<T> FromAvro for Option<T>
+where
+    T: FromAvro,
+{
+    fn read_schema() -> Schema {
+        Schema::Union(
+            UnionSchema::new(vec![Schema::Null, T::read_schema()]).expect("Option<T> schema"),
+        )
+    }
+}
+
+impl<T> ToAvro for Option<T>
+where
+    T: ToAvro,
+{
+    fn write_schema() -> Schema {
+        Schema::Union(
+            UnionSchema::new(vec![Schema::Null, T::write_schema()]).expect("Option<T> schema"),
+        )
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    macro_rules! test_roundtrip {
+        ($name:ident => $ty:ty, $value:expr) => {
+            #[test]
+            fn $name() {
+                let value = $value;
+                let avro_bytes = to_avro_binary(value).expect("to_avro_binary");
+                let value_read: $ty =
+                    from_avro_binary(&<$ty>::read_schema(), avro_bytes).expect("from_avro_binary");
+                assert_eq!($value, value_read);
+            }
+        };
+    }
+
+    test_roundtrip!(test_u8 => u8, u8::MAX);
+    test_roundtrip!(test_u16 => u16, u16::MAX);
+    test_roundtrip!(test_u32 => u32, u32::MAX);
+    test_roundtrip!(test_u64 => u64, u64::MAX);
+    test_roundtrip!(test_i8 => i8, i8::MAX);
+    test_roundtrip!(test_i16 => i16, i16::MAX);
+    test_roundtrip!(test_i32 => i32, i32::MAX);
+    test_roundtrip!(test_i64 => i64, i64::MAX);
+    test_roundtrip!(test_string_opt => Option<String>, Some("hello".to_string()));
+    test_roundtrip!(test_string_opt_none => Option<String>, None::<String>);
+    test_roundtrip!(test_u64_opt => Option<u64>, Some(u64::MAX));
+    test_roundtrip!(test_u64_opt_none => Option<u64>, None::<u64>);
+}
