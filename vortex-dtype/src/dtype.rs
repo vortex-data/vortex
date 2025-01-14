bb0e4843c409602599d1a10ba36750c74cf8428a
Adam Gutglick <adam@spiraldb.com>
2025-01-14 18:05:44 +0000

    inline struct to shortcircuite

--- vortex-dtype/src/dtype.rs
+++ vortex-dtype/src/dtype.rs
@@ -97,13 +97,11 @@ impl DType {
             (Binary(_), Binary(_)) => true,
             (List(lhs_dtype, _), List(rhs_dtype, _)) => lhs_dtype.eq_ignore_nullability(rhs_dtype),
             (Struct(lhs_dtype, _), Struct(rhs_dtype, _)) => {
-                let names_match = lhs_dtype.names() == rhs_dtype.names();
-                let types_match = lhs_dtype
-                    .dtypes()
-                    .zip(rhs_dtype.dtypes())
-                    .all(|(l, r)| l.eq_ignore_nullability(&r));
-
-                names_match && types_match
+                (lhs_dtype.names() == rhs_dtype.names())
+                    && (lhs_dtype
+                        .dtypes()
+                        .zip(rhs_dtype.dtypes())
+                        .all(|(l, r)| l.eq_ignore_nullability(&r)))
             }
             (Extension(lhs_extdtype), Extension(rhs_extdtype)) => {
                 lhs_extdtype.id() == rhs_extdtype.id()
