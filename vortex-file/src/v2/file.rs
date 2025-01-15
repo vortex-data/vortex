1368d2a2520b8c5d49c1f176d7c07e978095f5e0
Robert Kruszewski <github@robertk.io>
2025-01-15 17:09:27 +0000

    more

--- vortex-file/src/v2/file.rs
+++ vortex-file/src/v2/file.rs
@@ -137,7 +137,7 @@ impl<I: IoDriver> VortexFile<I> {
     where
         R: Iterator<Item = RowMask> + Send + 'static,
     {
-        let scanner = Scanner::new(self.dtype().clone(), scan.projection, scan.filter)?;
+        let scanner = Arc::new(Scanner::new(self.dtype().clone(), scan.projection, scan.filter)?);
 
         let result_dtype = scanner.result_dtype().clone();
 
