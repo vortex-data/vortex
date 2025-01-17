9560b493bb0def4bcfedabf2d0b323cfc9d77f5b
Joe Isaacs <joe.isaacs@live.co.uk>
2025-01-17 14:38:02 +0000

    fix

--- vortex-scalar/src/display.rs
+++ vortex-scalar/src/display.rs
@@ -33,11 +33,8 @@ impl Display for Scalar {
                     Some(buf) => {
                         write!(
                             f,
-                            "{}",
-                            buf.as_slice()
-                                .iter()
-                                .map(|b| format!("\"{b:x}\""))
-                                .format(",")
+                            "\"{}\"",
+                            buf.as_slice().iter().map(|b| format!("{b:x}")).format(",")
                         )
                     }
                 }
@@ -158,7 +155,10 @@ mod tests {
 
     #[test]
     fn display_utf8() {
-        assert_eq!(format!("{}", Scalar::from("Hello World!")), "Hello World!");
+        assert_eq!(
+            format!("{}", Scalar::from("Hello World!")),
+            "\"Hello World!\""
+        );
         assert_eq!(format!("{}", Scalar::null(DType::Utf8(Nullable))), "null");
     }
 
@@ -172,7 +172,7 @@ mod tests {
                     NonNullable
                 )
             ),
-            "48,65,6c,6c,6f,20,57,6f,72,6c,64,21"
+            "\"48,65,6c,6c,6f,20,57,6f,72,6c,64,21\""
         );
         assert_eq!(format!("{}", Scalar::null(DType::Binary(Nullable))), "null");
     }
