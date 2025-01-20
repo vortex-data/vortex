8f6c2ae82060f3c8344a9ee9351b136cdb1b52d3
Joe Isaacs <joe.isaacs@live.co.uk>
2025-01-20 17:56:47 +0000

    remove split by since its default

--- vortex-datafusion/src/persistent/opener.rs
+++ vortex-datafusion/src/persistent/opener.rs
@@ -88,8 +88,6 @@ impl FileOpener for VortexFileOpener {
                         .try_get(&file_meta.object_meta, this.object_store.clone())
                         .await?,
                 )
-                // Create larger splits in so that each chunk has more rows
-                .with_split_by(SplitBy::Layout)
                 .with_execution_mode(ExecutionMode::TokioRuntime(Handle::current()))
                 .open(read_at)
                 .await?;
