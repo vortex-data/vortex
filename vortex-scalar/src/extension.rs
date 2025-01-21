6e4371e00c0b8f6de7eaa737b59d260a3320da58
Daniel King <dan@spiraldb.com>
2025-01-21 11:14:21 +0000

    remove DType borrow from ExtensionScalar

--- vortex-scalar/src/extension.rs
+++ vortex-scalar/src/extension.rs
@@ -7,7 +7,6 @@ use crate::value::ScalarValue;
 use crate::Scalar;
 
 pub struct ExtScalar<'a> {
-    dtype: &'a DType,
     ext_dtype: &'a ExtDType,
     value: &'a ScalarValue,
 }
@@ -18,16 +17,7 @@ impl<'a> ExtScalar<'a> {
             vortex_bail!("Expected extension scalar, found {}", dtype)
         };
 
-        Ok(Self {
-            dtype,
-            ext_dtype,
-            value,
-        })
-    }
-
-    #[inline]
-    pub fn dtype(&self) -> &'a DType {
-        self.dtype
+        Ok(Self { ext_dtype, value })
     }
 
     /// Returns the storage scalar of the extension scalar.
@@ -36,13 +26,32 @@ impl<'a> ExtScalar<'a> {
     }
 
     pub(crate) fn cast(&self, dtype: &DType) -> VortexResult<Scalar> {
-        if self.dtype().eq_ignore_nullability(dtype)
-            || self.ext_dtype.storage_dtype().eq_ignore_nullability(dtype)
-        {
+        if self.value.is_null() && !dtype.is_nullable() {
+            vortex_bail!(
+                "cannot cast extension dtype with id {} and storage type {} to {}",
+                self.ext_dtype.id(),
+                self.ext_dtype.storage_dtype(),
+                dtype
+            );
+        }
+
+        if self.ext_dtype.storage_dtype().eq_ignore_nullability(dtype) {
+            // Casting from an extension type to the underlying storage type is OK.
             return Ok(Scalar::new(dtype.clone(), self.value.clone()));
         }
 
-        vortex_bail!("cannot cast {} to {}", self.dtype(), dtype);
+        if let DType::Extension(ext_dtype) = dtype {
+            if self.ext_dtype.eq_ignore_nullability(ext_dtype) {
+                return Ok(Scalar::new(dtype.clone(), self.value.clone()));
+            }
+        }
+
+        vortex_bail!(
+            "cannot cast extension dtype with id {} and storage type {} to {}",
+            self.ext_dtype.id(),
+            self.ext_dtype.storage_dtype(),
+            dtype
+        );
     }
 }
 
