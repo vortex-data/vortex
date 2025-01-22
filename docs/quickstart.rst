a832e846a2fe9e34c17fc245323194aee0bea7fa
Nicholas Gates <nick@nickgates.com>
2025-01-22 11:31:14 +0000

    Merge

--- docs/quickstart.rst
+++ docs/quickstart.rst
@@ -46,7 +46,7 @@ Use :func:`~vortex.encoding.compress` to compress the Vortex array and check the
 
    >>> cvtx = vortex.compress(vtx)
    >>> cvtx.nbytes
-   15903
+   15888
    >>> cvtx.nbytes / vtx.nbytes
    0.11...
 
--- vortex-sampling-compressor/tests/smoketest.rs
+++ vortex-sampling-compressor/tests/smoketest.rs
@@ -155,7 +155,7 @@ mod tests {
             );
             assert_eq!(
                 chunk.statistics().get(Stat::UncompressedSizeInBytes),
-                Some(Scalar::from(1392668_u64))
+                Some(Scalar::from(1392641_u64))
             );
         }
 
