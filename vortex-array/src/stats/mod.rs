d59f41d4787df18c09ff79a50d292c664b6716e6
Joe Isaacs <joe.isaacs@live.co.uk>
2025-02-03 20:28:16 +0000

    fix

--- vortex-array/src/stats/mod.rs
+++ vortex-array/src/stats/mod.rs
@@ -272,7 +272,7 @@ pub trait Statistics {
 
     /// Compute all the requested statistics (if not already present)
     /// Returns a StatsSet with the requested stats and any additional available stats
-    /// [deprecated]
+    // [deprecated]
     // TODO(joe): replace with `compute_statistics`
     fn compute_all(&self, stats: &[Stat]) -> VortexResult<StatsSet> {
         let mut stats_set = StatsSet::default();
