54dcf050ac8d84cc66bf566a55355dbf5b81403c
Nicholas Gates <nick@nickgates.com>
2025-02-03 21:12:34 +0000

    Default strategies

--- vortex-file/src/strategy/mod.rs
+++ vortex-file/src/strategy/mod.rs
@@ -9,40 +9,30 @@ use vortex_array::{Array, IntoArray, IntoCanonical};
 use vortex_dtype::DType;
 use vortex_error::{VortexExpect, VortexResult};
 use vortex_layout::layouts::chunked::writer::{ChunkedLayoutOptions, ChunkedLayoutWriter};
-use vortex_layout::layouts::flat::writer::FlatLayoutWriter;
+use vortex_layout::layouts::flat::writer::FlatLayoutOptions;
 use vortex_layout::layouts::struct_::writer::StructLayoutWriter;
 use vortex_layout::segments::SegmentWriter;
 use vortex_layout::{Layout, LayoutStrategy, LayoutWriter, LayoutWriterExt};
+use vortex_sampling_compressor::compressors::CompressionTree;
 use vortex_sampling_compressor::{SamplingCompressor, DEFAULT_COMPRESSORS};
 
 static COMPRESSOR: LazyLock<Arc<SamplingCompressor<'static>>> =
     LazyLock::new(|| Arc::new(SamplingCompressor::new(DEFAULT_COMPRESSORS)));
 
-struct SamplingCompressorStrategy(Arc<SamplingCompressor<'static>>);
-
-impl LayoutStrategy for SamplingCompressorStrategy {
-    fn new_writer(&self, dtype: &DType) -> VortexResult<Box<dyn LayoutWriter>> {
-        Ok(SamplingCompressorWriter {
-            compressor: self.0.clone(),
-            child: Box::new(FlatLayoutWriter::new(dtype.clone(), Default::default())),
-        }
-        .boxed())
-    }
-}
-
 struct SamplingCompressorWriter {
     compressor: Arc<SamplingCompressor<'static>>,
+    compress_like: Option<CompressionTree<'static>>,
     child: Box<dyn LayoutWriter>,
 }
 
 impl LayoutWriter for SamplingCompressorWriter {
     fn push_chunk(&mut self, segments: &mut dyn SegmentWriter, chunk: Array) -> VortexResult<()> {
-        self.child.push_chunk(
-            segments,
-            self.compressor
-                .compress(&chunk, None)
-                .map(|a| a.into_array())?,
-        )
+        let (compressed, tree) = self
+            .compressor
+            .compress(&chunk, self.compress_like.as_ref())?
+            .into_parts();
+        self.compress_like = tree;
+        self.child.push_chunk(segments, compressed)
     }
 
     fn finish(&mut self, segments: &mut dyn SegmentWriter) -> VortexResult<Layout> {
@@ -60,13 +50,18 @@ impl LayoutStrategy for VortexLayoutStrategy {
         } else {
             Ok(ColumnChunker::new(
                 dtype.clone(),
-                ChunkedLayoutWriter::new(
-                    dtype,
-                    ChunkedLayoutOptions {
-                        chunk_strategy: Arc::new(SamplingCompressorStrategy(COMPRESSOR.clone())),
-                        ..Default::default()
-                    },
-                )
+                SamplingCompressorWriter {
+                    compressor: COMPRESSOR.clone(),
+                    compress_like: None,
+                    child: ChunkedLayoutWriter::new(
+                        dtype,
+                        ChunkedLayoutOptions {
+                            chunk_strategy: Arc::new(FlatLayoutOptions::default()),
+                            ..Default::default()
+                        },
+                    )
+                    .boxed(),
+                }
                 .boxed(),
             )
             .boxed())
@@ -106,6 +101,11 @@ impl ColumnChunker {
                 // TODO(ngates): if we exceed a maximum block size, regardless of row count we should
                 //  flush the chunk. This can happen for columns with very large cells.
                 return Ok(());
+            }
+
+            if nblocks > 1 {
+                // TODO(ngates): if we have _too_ many blocks, then we might look into slicing
+                //  the chunks to be smaller blocks.
             }
 
             let mut chunks = Vec::with_capacity(self.chunks.len());
@@ -152,10 +152,20 @@ impl LayoutWriter for ColumnChunker {
         // We make sure the chunks are canonical so our nbytes measurement is accurate.
         let chunk = chunk.into_canonical()?.into_array();
 
-        self.row_count += chunk.len();
-        self.nbytes += chunk.nbytes();
-        self.chunks.push_back(chunk);
-        self.flush(segments)
+        // Split chunks into 8192 blocks to make sure we don't over-size them.
+        let mut offset = 0;
+        while offset < chunk.len() {
+            let end = (offset + Self::BLOCK_LEN).min(chunk.len());
+            let c = slice(&chunk, offset, end)?;
+            self.row_count += c.len();
+            self.nbytes += c.nbytes();
+            self.chunks.push_back(c);
+            offset = end;
+
+            self.flush(segments)?;
+        }
+
+        Ok(())
     }
 
     fn finish(&mut self, segments: &mut dyn SegmentWriter) -> VortexResult<Layout> {
