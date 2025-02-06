63d2ebb46131f2c9f8664024d51d1da673a76491
Adam Gutglick <adam@spiraldb.com>
2025-02-06 11:23:07 +0000

    .

--- encodings/runend/src/compress.rs
+++ encodings/runend/src/compress.rs
@@ -184,16 +184,14 @@ pub fn runend_decode_typed_primitive<T: NativePType>(
         Mask::AllTrue(_) => {
             let mut decoded: BufferMut<T> = BufferMut::with_capacity(length);
             for (end, value) in run_ends.zip_eq(values) {
-                assert!(end <= length);
+                assert!(end <= length, "Runend end must be less than overall length");
                 // SAFETY:
                 // We preallocate enough capacity because we know the total length
                 unsafe { decoded.push_n_unchecked(*value, end - decoded.len()) };
             }
             PrimitiveArray::new(decoded, values_nullability.into())
         }
-        Mask::AllFalse(_) => {
-            PrimitiveArray::new(buffer![T::default(); length], Validity::AllInvalid)
-        }
+        Mask::AllFalse(_) => PrimitiveArray::new(Buffer::<T>::zeroed(length), Validity::AllInvalid),
         Mask::Values(mask) => {
             let mut decoded = BufferMut::with_capacity(length);
             let mut decoded_validity = BooleanBufferBuilder::new(length);
@@ -203,7 +201,7 @@ pub fn runend_decode_typed_primitive<T: NativePType>(
                     .zip(mask.boolean_buffer().iter())
                     .map(|(&v, is_valid)| is_valid.then_some(v)),
             ) {
-                assert!(end <= length);
+                assert!(end <= length, "Runend end must be less than overall length");
                 match value {
                     None => {
                         decoded_validity.append_n(end - decoded.len(), false);
