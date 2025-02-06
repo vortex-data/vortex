de21dc0a19900f78173b9eb6fb99fd1610e6d579
Nicholas Gates <nick@nickgates.com>
2025-02-06 11:07:33 +0000

    In-Memory scan

---
+++ vortex-file/src/io/memory.rs
@@ -0,0 +1,79 @@
+use std::sync::Arc;
+
+use async_trait::async_trait;
+use futures::Stream;
+use futures_executor::block_on;
+use futures_util::future::BoxFuture;
+use futures_util::StreamExt;
+use vortex_array::stream::{ArrayStream, ArrayStreamAdapter};
+use vortex_array::{Array, ContextRef};
+use vortex_buffer::ByteBuffer;
+use vortex_dtype::DType;
+use vortex_error::{vortex_err, VortexResult};
+use vortex_layout::scan::{Scan, ScanDriver};
+use vortex_layout::segments::{AsyncSegmentReader, SegmentId};
+
+use crate::segments::NoOpSegmentCache;
+use crate::{FileLayout, Segment, VortexOpenOptions};
+
+impl VortexOpenOptions {
+    /// Open an in-memory file contained in the provided buffer.
+    pub fn open_memory<B: Into<ByteBuffer>>(self, buffer: B) -> VortexResult<InMemoryVortexFile> {
+        let buffer = buffer.into();
+
+        // No point caching anything, and we can block_on since I/O is basically free.
+        let file_layout = block_on(self.read_file_layout(&buffer, &NoOpSegmentCache))?;
+
+        Ok(InMemoryVortexFile {
+            buffer,
+            file_layout,
+            ctx: self.ctx.clone(),
+        })
+    }
+}
+
+#[derive(Clone)]
+pub struct InMemoryVortexFile {
+    buffer: ByteBuffer,
+    file_layout: FileLayout,
+    ctx: ContextRef,
+}
+
+impl InMemoryVortexFile {
+    pub fn scan(self) -> Scan<InMemoryVortexFile> {
+        let layout = self.file_layout.root_layout().clone();
+        let ctx = self.ctx.clone();
+        Scan::new(self, layout, ctx)
+    }
+}
+
+impl ScanDriver for InMemoryVortexFile {
+    fn segment_reader(&self) -> Arc<dyn AsyncSegmentReader> {
+        Arc::new(self.clone())
+    }
+
+    fn drive(
+        self,
+        dtype: DType,
+        stream: impl Stream<Item = BoxFuture<'static, VortexResult<Option<Array>>>>,
+    ) -> VortexResult<impl ArrayStream> {
+        let stream = stream.filter_map(|fut| async { fut.await.transpose() });
+        Ok(ArrayStreamAdapter::new(dtype, stream))
+    }
+}
+
+#[async_trait]
+impl AsyncSegmentReader for InMemoryVortexFile {
+    async fn get(&self, id: SegmentId) -> VortexResult<ByteBuffer> {
+        let segment: &Segment = self
+            .file_layout
+            .segment_map()
+            .get(*id as usize)
+            .ok_or_else(|| vortex_err!("segment not found"))?;
+
+        let start = usize::try_from(segment.offset).map_err(|_| vortex_err!("offset too large"))?;
+        let end = start + segment.length as usize;
+
+        Ok(self.buffer.slice(start..end))
+    }
+}
