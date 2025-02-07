e02d199681e5236fcb510dde62959450349d96b3
Alexander Droste <alexander.droste@protonmail.com>
2025-02-07 15:39:07 +0000

    bench: pushdown_compare & canonicalize_compare for fsst

--- encodings/fsst/benches/fsst_compress.rs
+++ encodings/fsst/benches/fsst_compress.rs
@@ -3,10 +3,12 @@
 use divan::Bencher;
 use rand::rngs::StdRng;
 use rand::{Rng, SeedableRng};
-use vortex_array::array::VarBinArray;
+use vortex_array::array::{ConstantArray, VarBinArray};
+use vortex_array::compute::{compare, Operator};
 use vortex_array::IntoCanonical;
 use vortex_dtype::{DType, Nullability};
 use vortex_fsst::{fsst_compress, fsst_train_compressor};
+use vortex_scalar::Scalar;
 
 fn main() {
     divan::main();
@@ -63,6 +65,26 @@ fn train_compressor(bencher: Bencher, args: (usize, usize, u8)) {
     let (string_count, avg_len, unique_chars) = args;
     let array = generate_test_data(string_count, avg_len, unique_chars);
     bencher.bench_local(move || fsst_train_compressor(&array).unwrap())
+}
+
+#[divan::bench(args = BENCH_ARGS)]
+fn pushdown_compare(bencher: Bencher, args: (usize, usize, u8)) {
+    let (string_count, avg_len, unique_chars) = args;
+    let array = generate_test_data(string_count, avg_len, unique_chars);
+
+    let constant = ConstantArray::new(Scalar::from(&b"const"[..]), array.len());
+    bencher.bench_local(|| compare(&array, &constant, Operator::Eq).unwrap());
+}
+
+#[divan::bench(args = BENCH_ARGS)]
+fn canonicalize_compare(bencher: Bencher, args: (usize, usize, u8)) {
+    let (string_count, avg_len, unique_chars) = args;
+    let array = generate_test_data(string_count, avg_len, unique_chars);
+
+    let constant = ConstantArray::new(Scalar::from(&b"const"[..]), array.len());
+    bencher.with_inputs(|| array.clone()).bench_values(|array| {
+        compare(array.into_canonical().unwrap(), &constant, Operator::Eq).unwrap()
+    });
 }
 
 // [(string_count, avg_len, unique_chars)]
