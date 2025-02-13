e22cc66199e9a7d0333d88629b47f756e968dfb8
Joe Isaacs <joe.isaacs@live.co.uk>
2025-02-13 14:19:30 +0000

    cleanup

--- encodings/dict/benches/chunked_dict_array_builder.rs
+++ encodings/dict/benches/chunked_dict_array_builder.rs
@@ -40,46 +40,25 @@ where
         .into_array()
 }
 
-// const BENCH_ARGS: &[(usize, usize, usize)] = &[
-//     // (32_000, 3000, 10),
-//     // (32_000, 3000, 100),
-//     // (32_000, 3000, 200),
-//     (32_000, 255, 400),
-// ];
-
 const BENCH_ARGS: &[(usize, usize, usize)] = &[
-    // (8000, 10, 20),
-    // (8000, 100, 20),
-    // (8000, 1000, 20),
+    (8000, 10, 20),
+    (8000, 100, 20),
+    (8000, 1000, 20),
     (8000, 10, 200),
     (8000, 100, 200),
     (8000, 1000, 200),
     (8000, 10, 1000),
     (8000, 100, 1000),
     (8000, 1000, 1000),
-    // (32_000, 10, 100),
-    // (32_000, 100, 100),
-    // (32_000, 1000, 100),
-    // (32_000, 10, 1000),
-    // (32_000, 100, 1000),
-    // (32_000, 1000, 1000),
+    (32_000, 10, 100),
+    (32_000, 100, 100),
+    (32_000, 1000, 100),
+    (32_000, 10, 1000),
+    (32_000, 100, 1000),
+    (32_000, 1000, 1000),
 ];
 
-// (array_len, unique_values, chunk_count)
-// const BENCH_ARGS: &[(usize, usize, usize)] = &[
-//     (1_000, 10, 10),
-//     (1_000, 100, 10),
-//     (1_000, 10, 1_000),
-//     (1_000, 100, 1_000),
-//     (10_000, 10, 100),
-//     (10_000, 100, 100),
-//     (10_000, 10, 1_000),
-//     (10_000, 100, 1_000),
-//     (100_000, 10, 100),
-//     (100_000, 100, 100),
-// ];
-
-#[divan::bench(types = [f32, f64], args=BENCH_ARGS)]
+#[divan::bench(types = [u32], args=BENCH_ARGS)]
 fn chunked_dict_primitive_canonical_into<T: NativePType>(
     bencher: Bencher,
     (len, unique_values, chunk_count): (usize, usize, usize),
@@ -97,7 +76,7 @@ fn chunked_dict_primitive_canonical_into<T: NativePType>(
         })
 }
 
-#[divan::bench(types = [f64], args=BENCH_ARGS)]
+#[divan::bench(types = [u32], args=BENCH_ARGS)]
 fn chunked_dict_primitive_into_canonical<T: NativePType>(
     bencher: Bencher,
     (len, unique_values, chunk_count): (usize, usize, usize),
@@ -110,44 +89,3 @@ fn chunked_dict_primitive_into_canonical<T: NativePType>(
         .with_inputs(|| chunk.clone())
         .bench_local_values(|chunk| chunk.into_canonical().vortex_unwrap())
 }
-
-// #[divan::bench(args=BENCH_ARGS)]
-// fn chunked_opt_bool_canonical_into(bencher: Bencher, (len, chunk_count): (usize, usize)) {
-//     let chunk = make_opt_bool_chunks(len, chunk_count);
-//
-//     bencher
-//         .with_inputs(|| chunk.clone())
-//         .bench_local_values(|chunk| {
-//             let mut builder = builder_with_capacity(chunk.dtype(), len * chunk_count);
-//             chunk
-//                 .clone()
-//                 .canonicalize_into(builder.as_mut())
-//                 .vortex_unwrap();
-//             builder.finish().vortex_unwrap()
-//         })
-// }
-
-// #[divan::bench(types = [u32, u64, f32, f64], args=BENCH_ARGS)]
-// fn validity_take<T: NativePType>(
-//     bencher: Bencher,
-//     (len, unique_values, _chunk_count): (usize, usize, usize),
-// ) where
-//     Standard: Distribution<T>,
-// {
-//     let mut rng = StdRng::seed_from_u64(0);
-//
-//     let values = (0..len)
-//         .map(|_| T::from(rng.gen_range(0..unique_values)).vortex_expect("valid value"))
-//         .collect::<PrimitiveArray>();
-//     let codes = (0..len)
-//         .map(|_| rng.gen_range(0..unique_values) as u32)
-//         .collect::<PrimitiveArray>();
-//
-//     bencher
-//         .with_inputs(|| (values.clone(), codes.clone()))
-//         .bench_local_values(|(values, codes)| {
-//             let validity = values.validity().take(codes.as_ref()).unwrap();
-//             let mask = validity.to_logical(values.len()).unwrap();
-//             mask
-//         })
-// }
