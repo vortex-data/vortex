cc0cea1fcc70e2c564ca79a5b91967fcca85101c
Andrew Duffy <andrew@a10y.dev>
2025-02-14 20:27:11 -0500

    reuse downscaling logic for float dicts

--- vortex-btrblocks/src/float/dictionary.rs
+++ vortex-btrblocks/src/float/dictionary.rs
@@ -1,6 +1,7 @@
 //! Float-specific dictionary encoding implementation.
 
 use vortex_array::array::PrimitiveArray;
+use vortex_array::validity::Validity;
 use vortex_array::IntoArray;
 use vortex_buffer::Buffer;
 use vortex_dict::DictArray;
@@ -17,10 +18,29 @@ macro_rules! typed_encode {
             .copied()
             .map(|x| <$typ>::from_bits(x))
             .collect();
-        let codes = <DictEncoder as Encode<$typ>>::encode(&values, $stats.src.as_slice::<$typ>());
-        // preserve the original validity
-        let codes = PrimitiveArray::new(codes, $validity).into_array();
-        DictArray::try_new(codes, values.into_array())
+
+        let max_code = values.len();
+        let codes = if max_code <= u8::MAX as usize {
+            let buf =
+                <DictEncoder as Encode<$typ, u8>>::encode(&values, $stats.src.as_slice::<$typ>());
+            PrimitiveArray::new(buf, $validity.clone()).into_array()
+        } else if max_code <= u16::MAX as usize {
+            let buf =
+                <DictEncoder as Encode<$typ, u16>>::encode(&values, $stats.src.as_slice::<$typ>());
+            PrimitiveArray::new(buf, $validity.clone()).into_array()
+        } else {
+            let buf =
+                <DictEncoder as Encode<$typ, u32>>::encode(&values, $stats.src.as_slice::<$typ>());
+            PrimitiveArray::new(buf, $validity.clone()).into_array()
+        };
+
+        let values_validity = match $validity {
+            Validity::NonNullable => Validity::NonNullable,
+            _ => Validity::AllValid,
+        };
+        let values = PrimitiveArray::new(values, values_validity).into_array();
+
+        DictArray::try_new(codes, values)
     }};
 }
 
@@ -35,22 +55,24 @@ pub fn dictionary_encode(stats: &FloatStats) -> VortexResult<DictArray> {
 
 struct DictEncoder;
 
-trait Encode<T> {
+trait Encode<T, I> {
     /// Using the distinct value set, turn the values into a set of codes.
-    fn encode(distinct: &[T], values: &[T]) -> Buffer<u32>;
+    fn encode(distinct: &[T], values: &[T]) -> Buffer<I>;
 }
 
 macro_rules! impl_encode {
-    ($typ:ty, $utyp:ty) => {
-        impl Encode<$typ> for DictEncoder {
+    ($typ:ty, $utyp:ty) => { impl_encode!($typ, $utyp, u8, u16, u32); };
+    ($typ:ty, $utyp:ty, $($ityp:ty),+) => {
+        $(
+        impl Encode<$typ, $ityp> for DictEncoder {
             #[allow(clippy::cast_possible_truncation)]
-            fn encode(distinct: &[$typ], values: &[$typ]) -> Buffer<u32> {
+            fn encode(distinct: &[$typ], values: &[$typ]) -> Buffer<$ityp> {
                 let mut codes =
-                    vortex_array::aliases::hash_map::HashMap::<$utyp, u32>::with_capacity(
+                    vortex_array::aliases::hash_map::HashMap::<$utyp, $ityp>::with_capacity(
                         distinct.len(),
                     );
                 for (code, &value) in distinct.iter().enumerate() {
-                    codes.insert(value.to_bits(), code as u32);
+                    codes.insert(value.to_bits(), code as $ityp);
                 }
 
                 let mut output = vortex_buffer::BufferMut::with_capacity(values.len());
@@ -63,6 +85,7 @@ macro_rules! impl_encode {
                 return output.freeze();
             }
         }
+        )*
     };
 }
 
