b93333be5389793cb1e681f34b5da2b08e6f96df
Alexander Droste <alexander.droste@protonmail.com>
2025-02-20 17:32:19 +0000

    bench: drop flaky buffer push benchmark parameter

--- vortex-buffer/benches/vortex_buffer.rs
+++ vortex-buffer/benches/vortex_buffer.rs
@@ -83,7 +83,7 @@ fn map_each<B: MapEach<i32, u32> + FromIterator<i32>>(bencher: Bencher, n: i32) 
         .bench_local_values(|buffer| B::map_each(buffer, |i| (i as u32) + 1));
 }
 
-#[divan::bench(args = [1, 100, 1_000, 10_000, 100_000, 1_000_000])]
+#[divan::bench(args = [100, 1_000, 10_000, 100_000, 1_000_000])]
 fn push_vortex_buffer(bencher: Bencher, length: i32) {
     bencher
         .with_inputs(|| BufferMut::<i32>::with_capacity(length as usize))
@@ -94,7 +94,7 @@ fn push_vortex_buffer(bencher: Bencher, length: i32) {
         });
 }
 
-#[divan::bench(args = [1, 100, 1_000, 10_000, 100_000, 1_000_000])]
+#[divan::bench(args = [100, 1_000, 10_000, 100_000, 1_000_000])]
 fn push_arrow_buffer(bencher: Bencher, length: i32) {
     bencher
         .with_inputs(|| {
