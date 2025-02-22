9216dfa7db05638336581e315253264f4d77c3f8
Nicholas Gates <nick@nickgates.com>
2025-02-22 20:31:15 +0000

    vortex-datafusion

--- vortex-array/src/arrays/varbinview/serde.rs
+++ vortex-array/src/arrays/varbinview/serde.rs
@@ -1,16 +1,25 @@
 use std::fmt::{Debug, Display, Formatter};
 
-use vortex_error::VortexResult;
+use vortex_buffer::Buffer;
+use vortex_dtype::DType;
+use vortex_error::{vortex_bail, VortexExpect, VortexResult};
+use vortex_flatbuffers::dtype::Binary;
 
-use crate::arrays::{VarBinViewArray, VarBinViewEncoding};
+use crate::arrays::{BinaryView, VarBinViewArray, VarBinViewEncoding};
+use crate::serde::ArrayParts;
+use crate::validity::Validity;
 use crate::vtable::SerdeVTable;
-use crate::{Array, ArrayBufferVisitor, ArrayChildVisitor, ArrayVisitorImpl, EmptyMetadata};
+use crate::{
+    Array, ArrayBufferVisitor, ArrayChildVisitor, ArrayRef, ArrayVisitorImpl, ContextRef,
+    EmptyMetadata,
+};
 
 impl ArrayVisitorImpl<EmptyMetadata> for VarBinViewArray {
     fn _buffers(&self, visitor: &mut dyn ArrayBufferVisitor) {
         for buffer in self.buffers() {
             visitor.visit_buffer(buffer);
         }
+        visitor.visit_buffer(&self.views().clone().into_byte_buffer());
     }
 
     fn _children(&self, visitor: &mut dyn ArrayChildVisitor) {
@@ -22,4 +31,32 @@ impl ArrayVisitorImpl<EmptyMetadata> for VarBinViewArray {
     }
 }
 
-impl SerdeVTable<&VarBinViewArray> for VarBinViewEncoding {}
+impl SerdeVTable<&VarBinViewArray> for VarBinViewEncoding {
+    fn decode(
+        &self,
+        parts: &ArrayParts,
+        ctx: &ContextRef,
+        dtype: DType,
+        len: usize,
+    ) -> VortexResult<ArrayRef> {
+        let mut buffers = parts.buffers()?;
+        let views = Buffer::<BinaryView>::from_byte_buffer(
+            buffers.pop().vortex_expect("Missing views buffer"),
+        );
+
+        if views.len() != len {
+            vortex_bail!("Expected {} views, got {}", len, views.len());
+        }
+
+        let validity = if parts.nchildren() == 0 {
+            Validity::from(dtype.nullability())
+        } else if parts.nchildren() == 1 {
+            let validity = parts.child(0).decode(ctx, Validity::DTYPE, len)?;
+            Validity::Array(validity)
+        } else {
+            vortex_bail!("Expected 0 or 1 children, got {}", parts.nchildren());
+        };
+
+        Ok(VarBinViewArray::try_new(views, buffers, dtype, validity)?.into_array())
+    }
+}
