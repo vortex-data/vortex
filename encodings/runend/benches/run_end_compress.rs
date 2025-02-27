b78fea551f8e24b342f43736f127fd6767839c89
Nicholas Gates <nick@nickgates.com>
2025-02-27 10:02:47 +0000

    Faster runend decompress

--- encodings/runend/benches/run_end_compress.rs
+++ encodings/runend/benches/run_end_compress.rs
@@ -2,10 +2,12 @@
 
 use divan::Bencher;
 use itertools::repeat_n;
+use num_traits::PrimInt;
 use vortex_array::Array;
 use vortex_array::arrays::PrimitiveArray;
 use vortex_array::validity::Validity;
 use vortex_buffer::Buffer;
+use vortex_dtype::NativePType;
 use vortex_runend::RunEndArray;
 use vortex_runend::compress::runend_encode;
 
@@ -38,13 +40,18 @@ fn compress(bencher: Bencher, (length, run_step): (usize, usize)) {
         .bench_refs(|values| runend_encode(values).unwrap());
 }
 
-#[divan::bench(args = BENCH_ARGS)]
-fn decompress(bencher: Bencher, (length, run_step): (usize, usize)) {
+#[divan::bench(types = [u8, u16, u32, u64], args = BENCH_ARGS)]
+fn decompress<T: NativePType + PrimInt>(bencher: Bencher, (length, run_step): (usize, usize)) {
     let values = PrimitiveArray::new(
         (0..=length)
             .step_by(run_step)
             .enumerate()
-            .flat_map(|(idx, x)| repeat_n(idx as u64, x))
+            .flat_map(|(idx, x)| {
+                repeat_n(
+                    T::from(idx % T::max_value().to_usize().unwrap()).unwrap(),
+                    x,
+                )
+            })
             .collect::<Buffer<_>>(),
         Validity::NonNullable,
     );
