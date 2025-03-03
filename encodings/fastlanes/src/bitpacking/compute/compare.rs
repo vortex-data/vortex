b3ef01f2bdb67c6268593185d2e00fa1401740cf
Joe Isaacs <joe.isaacs@live.co.uk>
2025-03-03 15:36:51 +0000

    with compare bytes

--- encodings/fastlanes/src/bitpacking/compute/compare.rs
+++ encodings/fastlanes/src/bitpacking/compute/compare.rs
@@ -1,10 +1,9 @@
-use arrow_buffer::{BooleanBuffer, bit_util};
 use fastlanes::{BitPacking, BitPackingCompare, FastLanesComparable};
-use vortex_array::arrays::BoolArray;
 use vortex_array::compute::{CompareFn, Operator};
 use vortex_array::variants::PrimitiveArrayTrait;
 use vortex_array::{Array, ArrayRef};
-use vortex_buffer::BufferMut;
+use vortex_buffer::{ByteBuffer, ByteBufferMut};
+use vortex_bytebool::ByteBoolArray;
 use vortex_dtype::{NativePType, PType};
 use vortex_error::{VortexError, VortexResult};
 use vortex_mask::Mask;
@@ -50,7 +49,7 @@ impl CompareFn<&BitPackedArray> for BitPackedEncoding {
             _ => return Ok(None),
         };
 
-        Ok(res?.map(|buffer| BoolArray::new(buffer, validity).into_array()))
+        Ok(res?.map(|buffer| ByteBoolArray::new(buffer, validity).into_array()))
     }
 }
 
@@ -58,15 +57,14 @@ fn compare_impl<T>(
     array: &BitPackedArray,
     scalar: Scalar,
     operator: Operator,
-) -> VortexResult<Option<BooleanBuffer>>
+) -> VortexResult<Option<ByteBuffer>>
 where
     T: NativePType + FastLanesComparable + TryFrom<Scalar, Error = VortexError>,
     T::Bitpacked: NativePType + BitPackingCompare + BitPacking,
 {
-    let mut bout = [false; 1024];
     const CHUNK_SIZE: usize = 1024;
     if array.bit_width() == 0 {
-        return Ok(Some(BooleanBuffer::new_unset(array.len())));
+        return Ok(Some(ByteBuffer::zeroed(array.len())));
     }
 
     let offset = array.offset() as usize;
@@ -78,8 +76,7 @@ where
         (offset + array.len()) % CHUNK_SIZE
     };
 
-    let output_len = bit_util::round_upto_multiple_of_64(array.len()) / 64;
-    let mut output = BufferMut::<u64>::with_capacity(output_len);
+    let mut output = ByteBufferMut::with_capacity(array.len);
     let packed = array.packed_slice::<T::Bitpacked>();
 
     // How many fastlanes vectors we will process.
@@ -98,61 +95,41 @@ where
         let chunk = &packed[i * elems_per_chunk..][..elems_per_chunk];
 
         unsafe {
-            // SAFETY: &[T] and &[MaybeUninit<T>] have the same layout
             unchecked_unpack_cmp_impl::<T>(
                 bit_width,
                 chunk,
-                &mut bout,
-                // SAFETY: &[T] and &[MaybeUninit<T>] have the same layout
-                &mut *(output.spare_capacity_mut().assume_init_mut()[i * 16..(i + 1) * 16]
-                    .as_mut_ptr() as *mut [u64; 16]),
+                &mut *(output.spare_capacity_mut().assume_init_mut()[i * 1024..(i + 1) * 1024]
+                    .as_mut_ptr() as *mut [bool; 1024]),
                 operator,
                 value,
             )
         }
     }
-    unsafe { output.set_len(full_chunks_range.len() * 16) }
+    unsafe { output.set_len(full_chunks_range.len() * 1024) }
 
     if last_chunk_is_sliced {
         let chunk = &packed[(num_chunks - 1) * elems_per_chunk..][..elems_per_chunk];
-        let mut decoded = [0u64; CHUNK_SIZE / u64::BITS as usize];
+        let mut decoded = [false; 1024];
         // SAFETY:
         // 1. chunk is elems_per_chunk.
         // 2. decoded is exactly 1024.
+        unsafe { unchecked_unpack_cmp_impl::<T>(bit_width, chunk, &mut decoded, operator, value) };
+        output.spare_capacity_mut()[0..last_chunk_length.div_ceil(16)].write_copy_of_slice(
+            unsafe { std::mem::transmute(&decoded[..last_chunk_length.div_ceil(16)]) },
+        );
         unsafe {
-            unchecked_unpack_cmp_impl::<T>(
-                bit_width,
-                chunk,
-                &mut bout,
-                &mut decoded,
-                operator,
-                value,
-            )
-        };
-        output.spare_capacity_mut()[0..last_chunk_length.div_ceil(16)]
-            .write_copy_of_slice(&decoded[..last_chunk_length.div_ceil(16)]);
-        unsafe {
-            output.set_len(full_chunks_range.len() * 16 + last_chunk_length.div_ceil(16));
+            output.set_len(full_chunks_range.len() * 1024 + last_chunk_length);
         }
     }
 
     // TODO(joe): fix conversion
-    Ok(Some(BooleanBuffer::new(
-        arrow_buffer::Buffer::from(output.to_vec()),
-        0,
-        array.len(),
-    )))
+    Ok(Some(output.freeze()))
 }
-
-// thread_local! {
-//     pub static BOUT: Cell<[bool; 1024]> = Cell::new([false; 1024]);
-// }
 
 unsafe fn unchecked_unpack_cmp_impl<T>(
     width: usize,
     input: &[T::Bitpacked],
-    bout: &mut [bool; 1024],
-    output: &mut [u64; 16],
+    output: &mut [bool; 1024],
     comparison: Operator,
     value: T,
 ) where
@@ -161,28 +138,27 @@ unsafe fn unchecked_unpack_cmp_impl<T>(
 {
     match comparison {
         Operator::Eq => unsafe {
-            BitPackingCompare::unchecked_unpack_cmp(width, input, bout, |a, b| a == b, value)
+            BitPackingCompare::unchecked_unpack_cmp(width, input, output, |a, b| a == b, value)
         },
         Operator::NotEq => unsafe {
-            BitPackingCompare::unchecked_unpack_cmp(width, input, bout, |a, b| a != b, value)
+            BitPackingCompare::unchecked_unpack_cmp(width, input, output, |a, b| a != b, value)
         },
         Operator::Gte => unsafe {
-            BitPackingCompare::unchecked_unpack_cmp(width, input, bout, |a, b| a >= b, value)
+            BitPackingCompare::unchecked_unpack_cmp(width, input, output, |a, b| a >= b, value)
         },
         Operator::Gt => unsafe {
-            BitPackingCompare::unchecked_unpack_cmp(width, input, bout, |a, b| a > b, value)
+            BitPackingCompare::unchecked_unpack_cmp(width, input, output, |a, b| a > b, value)
         },
         Operator::Lte => unsafe {
-            BitPackingCompare::unchecked_unpack_cmp(width, input, bout, |a, b| a <= b, value)
+            BitPackingCompare::unchecked_unpack_cmp(width, input, output, |a, b| a <= b, value)
         },
         Operator::Lt => unsafe {
-            BitPackingCompare::unchecked_unpack_cmp(width, input, bout, |a, b| a < b, value)
+            BitPackingCompare::unchecked_unpack_cmp(width, input, output, |a, b| a < b, value)
         },
     };
-
-    collect_byte_to_bit_4(bout, output);
 }
 
+#[allow(dead_code)]
 pub fn collect_byte_to_bit_4(bools: &[bool; 1024], result: &mut [u64; 16]) {
     for i in 0..16 {
         for j in (0..64).step_by(4) {
@@ -232,19 +208,14 @@ mod tests {
         let arr = PrimitiveArray::from_iter(0u32..1026);
         let arr = bitpack_encode(&arr, 11).unwrap();
 
-        println!("arr {}", arr.clone().to_array().tree_display());
-
         let const_ = ConstantArray::new(1025u32, arr.len()).into_array();
 
         let res = BitPackedEncoding::compare(&BitPackedEncoding, &arr, &const_, Operator::Lt)
             .unwrap()
             .unwrap();
         let vec = res.to_bool().unwrap().boolean_buffer().iter().collect_vec();
-        println!(
-            "res {:?}, {:?}, {:?}",
-            vec.len(),
-            vec.iter().map(|x| *x as usize).sum::<usize>(),
-            vec.iter().map(|x| 1 - *x as usize).sum::<usize>()
-        );
+        assert_eq!(vec.len(), 1026);
+        assert_eq!(vec.iter().map(|x| *x as usize).sum::<usize>(), 1025);
+        assert_eq!(vec.iter().map(|x| 1 - *x as usize).sum::<usize>(), 1);
     }
 }
