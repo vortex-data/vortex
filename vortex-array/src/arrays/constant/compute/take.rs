cbdd4c700014c458bec60fedfe146265b183561b
Robert Kruszewski <github@robertk.io>
2025-03-10 10:24:57 +0000

    fix

--- vortex-array/src/arrays/constant/compute/take.rs
+++ vortex-array/src/arrays/constant/compute/take.rs
@@ -58,7 +58,10 @@ mod tests {
         )
         .unwrap();
         let valid_indices: &[usize] = &[1usize];
-        assert_eq!(taken.to_primitive().unwrap().as_slice::<i32>(), &[42, 42, 42]);
+        assert_eq!(
+            taken.to_primitive().unwrap().as_slice::<i32>(),
+            &[42, 42, 42]
+        );
         assert_eq!(
             taken.validity_mask().unwrap().indices(),
             AllOr::Some(valid_indices)
