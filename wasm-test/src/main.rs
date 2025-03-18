e3c7482a18a9e00e92da2fa159c3a4e566a60a3b
Adam Gutglick <adam@spiraldb.com>
2025-03-18 14:24:34 +0000

    .

--- wasm-test/src/main.rs
+++ wasm-test/src/main.rs
@@ -10,7 +10,7 @@ pub fn main() {
     // Extremely simple test of compression/decompression and a few compute functions.
     let array = PrimitiveArray::new(buffer![1i32; 1024], Validity::AllValid).to_array();
 
-    let compressed = BtrBlocksCompressor.compress(&array).unwrap();
+    let compressed = BtrBlocksCompressor::default().compress(&array).unwrap();
     println!("Compressed size: {}", compressed.len());
     println!("Tree view: {}", compressed.tree_display());
 }
