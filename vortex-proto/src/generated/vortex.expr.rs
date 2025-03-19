f2bb928ac27ffbf291346ff973720b19497f0ec0
Joe Isaacs <joe.isaacs@live.co.uk>
2025-03-19 14:36:45 +0000

    expr

--- vortex-proto/src/generated/vortex.expr.rs
+++ vortex-proto/src/generated/vortex.expr.rs
@@ -5,16 +5,19 @@ pub struct Expr {
     pub id: ::prost::alloc::string::String,
     #[prost(message, repeated, tag = "2")]
     pub children: ::prost::alloc::vec::Vec<Expr>,
+    /// See warning message below.
     #[prost(message, optional, tag = "3")]
     pub kind: ::core::option::Option<Kind>,
 }
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct Kind {
+    /// This enum is very unstable, and will likely be replaced with something more extensible.
     #[prost(oneof = "kind::Kind", tags = "1, 2, 3, 4, 5, 6, 7")]
     pub kind: ::core::option::Option<kind::Kind>,
 }
 /// Nested message and enum types in `Kind`.
 pub mod kind {
+    /// This enum is very unstable, and will likely be replaced with something more extensible.
     #[derive(Clone, PartialEq, ::prost::Oneof)]
     pub enum Kind {
         #[prost(message, tag = "1")]
