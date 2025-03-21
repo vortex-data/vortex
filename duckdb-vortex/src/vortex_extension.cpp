12dc2c21764a3a4d3d2e05112b19ea2632ed573a
Joe Isaacs <joe.isaacs@live.co.uk>
2025-03-21 16:24:46 +0000

    fixme

--- duckdb-vortex/src/vortex_extension.cpp
+++ duckdb-vortex/src/vortex_extension.cpp
@@ -201,6 +201,14 @@ void VortexExtension::Load(DuckDB &db) {
 		}
 		return state;
 	};
+	// typedef unique_ptr<GlobalTableFunctionState> (*table_function_init_global_t)(ClientContext &context,
+	// TableFunctionInitInput &inpu
+
+	vortex_func.init_global = [](ClientContext &context,
+	                             TableFunctionInitInput &input) -> unique_ptr<GlobalTableFunctionState> {
+		auto state = make_uniq<VortexScanSharedState>();
+		return state;
+	};
 
 	vortex_func.init_global = [](ClientContext &context,
 	                             TableFunctionInitInput &input) -> unique_ptr<GlobalTableFunctionState> {
