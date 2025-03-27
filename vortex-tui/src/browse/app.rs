b9309f960b03198ad14a01dc9dcbc5870d135fba
Adam Gutglick <adam@spiraldb.com>
2025-03-27 21:46:06 +0000

    .

--- vortex-tui/src/browse/app.rs
+++ vortex-tui/src/browse/app.rs
@@ -167,10 +167,6 @@ impl LayoutCursor {
     pub fn segment_spec(&self, id: SegmentId) -> &SegmentSpec {
         &self.segment_map[*id as usize]
     }
-
-    pub fn footer(&self) -> &Footer {
-        &self.footer
-    }
 }
 
 #[derive(Default, PartialEq, Eq)]
