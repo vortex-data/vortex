769e81c3c8cd6711926a6d43403493fdafd7d25c
Alexander Droste <alexander.droste@protonmail.com>
2025-04-17 16:45:35 +0100

    clean: duckdb structure

--- duckdb-vortex/src/include/vortex.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-
-// Include Vortex FFI, with the DuckDB FFI feature
-#ifndef ENABLE_DUCKDB_FFI
-#define ENABLE_DUCKDB_FFI
-#endif
-
-#include "vortex.h"
--- duckdb-vortex/src/include/vortex_write.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "duckdb/main/extension_util.hpp"
-
-namespace duckdb {
-	void RegisterVortexWriteFunction(duckdb::DatabaseInstance &instance);
-}
