55f3cabb142f16a52258f77f513cecb99f21afcc
Adam Gutglick <adam@spiraldb.com>
2025-04-24 13:40:26 +0100

    .

--- vortex-datafusion/src/persistent/source.rs
+++ vortex-datafusion/src/persistent/source.rs
@@ -1,14 +1,11 @@
 use std::any::Any;
-use std::collections::VecDeque;
 use std::sync::Arc;
 
 use arrow_schema::SchemaRef;
 use dashmap::DashMap;
-use datafusion::datasource::listing::PartitionedFile;
 use datafusion::datasource::physical_plan::{FileOpener, FileScanConfig, FileSource};
 use datafusion_common::{Result as DFResult, Statistics};
 use datafusion_physical_plan::metrics::ExecutionPlanMetricsSet;
-use itertools::Itertools as _;
 use object_store::ObjectStore;
 use object_store::path::Path;
 use vortex_error::VortexExpect as _;
@@ -156,138 +153,5 @@ impl FileSource for VortexSource {
 
     fn file_type(&self) -> &str {
         VORTEX_FILE_EXTENSION
-    }
-
-    // fn repartitioned(
-    //     &self,
-    //     target_partitions: usize,
-    //     _repartition_file_min_size: usize,
-    //     _output_ordering: Option<LexOrdering>,
-    //     config: &FileScanConfig,
-    // ) -> DFResult<Option<FileScanConfig>> {
-    //     let mut new_config = config.clone();
-    //     let file_groups = std::mem::take(&mut new_config.file_groups);
-    //     new_config.file_groups = repartition_by_size(file_groups, target_partitions);
-
-    //     Ok(Some(new_config))
-    // }
-}
-
-#[allow(dead_code)]
-pub(crate) fn repartition_by_size(
-    file_groups: Vec<Vec<PartitionedFile>>,
-    desired_partitions: usize,
-) -> Vec<Vec<PartitionedFile>> {
-    let total_file_count = file_groups.iter().map(|f| f.len()).sum::<usize>();
-    let total_size = file_groups
-        .iter()
-        .flatten()
-        .map(|f| f.object_meta.size)
-        .sum::<usize>();
-    let target_partition_size = total_size / desired_partitions;
-    let average_partition_file_count = total_file_count / desired_partitions;
-
-    let mut partitions = Vec::with_capacity(desired_partitions);
-
-    let mut curr_partition_size = 0;
-    let mut curr_partition = Vec::with_capacity(average_partition_file_count);
-
-    let mut all_files = VecDeque::from_iter(
-        file_groups
-            .into_iter()
-            .flatten()
-            .sorted_unstable_by_key(|f| f.object_meta.size),
-    );
-
-    while !all_files.is_empty() && partitions.len() < desired_partitions {
-        // If the current partition is empty, we want to bootstrap it with the biggest file we have leftover.
-        let file = if curr_partition.is_empty() {
-            all_files.pop_back()
-        // If we already have files in the partition, we try and fill it up.
-        } else {
-            // Peak at the biggest file left
-            let biggest_file_size = all_files
-                .back()
-                .vortex_expect("We must have at least one item")
-                .object_meta
-                .size;
-
-            let smallest_file_size = all_files
-                .front()
-                .vortex_expect("We must have at least one item")
-                .object_meta
-                .size;
-
-            // We try and find a file on either end that fits in the partition
-            if curr_partition_size + biggest_file_size >= target_partition_size {
-                all_files.pop_front()
-            } else if curr_partition_size + smallest_file_size >= target_partition_size {
-                all_files.pop_back()
-            } else {
-                None
-            }
-        };
-
-        let missing_file = file.is_none();
-        // Add a file to the partition
-        if let Some(file) = file {
-            curr_partition_size += file.object_meta.size;
-            curr_partition.push(file);
-        }
-
-        // If the partition is full, move on to the next one
-        if curr_partition_size >= target_partition_size || missing_file {
-            curr_partition_size = 0;
-            partitions.push(std::mem::replace(
-                &mut curr_partition,
-                Vec::with_capacity(average_partition_file_count),
-            ));
-        }
-    }
-
-    // If we're still missing the last partition
-    if !curr_partition.is_empty() && partitions.len() != desired_partitions {
-        partitions.push(std::mem::take(&mut curr_partition));
-    } else if !curr_partition.is_empty() {
-        for (idx, file) in curr_partition.into_iter().enumerate() {
-            let new_part_idx = idx % partitions.len();
-            partitions[new_part_idx].push(file);
-        }
-    }
-
-    for (idx, file) in all_files.into_iter().enumerate() {
-        let new_part_idx = idx % partitions.len();
-        partitions[new_part_idx].push(file);
-    }
-
-    // Assert that we have the correct number of partitions and that the total number of files is right
-    assert_eq!(total_file_count, partitions.iter().flatten().count());
-
-    partitions
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn basic_repartition_test() {
-        let file_groups = vec![vec![
-            PartitionedFile::new("a", 100),
-            PartitionedFile::new("b", 25),
-            PartitionedFile::new("c", 25),
-            PartitionedFile::new("d", 25),
-            PartitionedFile::new("e", 50),
-        ]];
-
-        repartition_by_size(file_groups, 2);
-
-        let file_groups = vec![
-            (0..100)
-                .map(|idx| PartitionedFile::new(format!("{idx}"), idx))
-                .collect(),
-        ];
-
-        repartition_by_size(file_groups, 16);
     }
 }
