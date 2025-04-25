0ac7bef1a9d6849b4c03d8c2c552eb4d2b87cfea
Nicholas Gates <nick@nickgates.com>
2025-04-25 10:50:55 +0100

    Vortex error struct

--- duckdb-vortex/src/include/vortex_error.hpp
+++ duckdb-vortex/src/include/vortex_error.hpp
@@ -3,8 +3,8 @@
 #include "vortex.hpp"
 
 inline void HandleError(vx_error *error) {
-	if (error != nullptr && error->code != 0) {
-		auto msg = std::string(error->message);
+	if (error != nullptr) {
+		auto msg = std::string(vx_error_get_message(error));
 		vx_error_free(error);
 		throw duckdb::InvalidInputException(msg);
 	}
