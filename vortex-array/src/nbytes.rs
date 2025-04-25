d3d9ad10ac7d8dda098cfe1ac30d10c6a3bdebde
Onur Satici <onur@spiraldb.com>
2025-04-25 15:03:16 +0100

    nbytes to not double count same buffers

--- vortex-array/src/nbytes.rs
+++ vortex-array/src/nbytes.rs
@@ -1,16 +1,24 @@
+use rust_lapper::{Interval, Lapper};
+
 use crate::{Array, ArrayVisitorExt};
 
 pub trait NBytes: Array {
     /// Total size of the array in bytes, including all children and buffers.
     // TODO(ngates): this should return u64
     fn nbytes(&self) -> usize {
-        let mut nbytes = 0;
+        let mut intervals = Vec::new();
         for array in self.depth_first_traversal() {
             for buffer in array.buffers() {
-                nbytes += buffer.len();
+                let slice: &[u8] = buffer.inner().as_ref();
+                let start = slice.as_ptr() as usize;
+                intervals.push(Interval {
+                    start,
+                    stop: start + slice.len(),
+                    val: true,
+                });
             }
         }
-        nbytes
+        Lapper::new(intervals).cov()
     }
 }
 
