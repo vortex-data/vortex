04282f72bf1248b0b65fdec3eab7da65ae6b4ecf
Adam Gutglick <adam@spiraldb.com>
2025-04-25 14:09:39 +0100

    .

--- vortex-file/src/driver.rs
+++ vortex-file/src/driver.rs
@@ -71,7 +71,7 @@ impl CoalescedDriver {
 
             performance_hint,
             max_prefetch_bytes: 32 << 20, // 32 MB
-            max_prefetch_inflight_requests: 4,
+            max_prefetch_inflight_requests: 100,
             first_poll: false,
             state: Default::default(),
             requested: Default::default(),
