aea2b64c7282e6031c69d8d0490fbc0a0a462c16
Adam Gutglick <adam@spiraldb.com>
2025-05-01 21:13:51 +0100

    .

--- vortex/src/lib.rs
+++ vortex/src/lib.rs
@@ -6,7 +6,7 @@ pub use vortex_array::*;
 pub use vortex_file as file;
 pub use {
     vortex_btrblocks as compressor, vortex_buffer as buffer, vortex_dtype as dtype,
-    vortex_error as error, vortex_expr as expr, vortex_flatbuffers as flatbuffers,
+    vortex_error as error, vortex_expr as expr, vortex_flatbuffers as flatbuffers, vortex_io as io,
     vortex_ipc as ipc, vortex_layout as layout, vortex_mask as mask, vortex_proto as proto,
     vortex_scalar as scalar,
 };
