5089f3d90a67778abdc21b6d4d1abd7633b974b3
Joe Isaacs <joe.isaacs@live.co.uk>
2025-05-09 15:53:49 +0100

    its a dict codes are const the dict is const
    Signed-off-by: Joe Isaacs <joe.isaacs@live.co.uk>

--- encodings/dict/src/ops.rs
+++ encodings/dict/src/ops.rs
@@ -1,4 +1,6 @@
-use vortex_array::{Array, ArrayOperationsImpl, ArrayRef};
+use vortex_array::arrays::{ConstantArray, ConstantEncoding};
+use vortex_array::vtable::EncodingVTable;
+use vortex_array::{Array, ArrayOperationsImpl, ArrayRef, ArrayStatistics};
 use vortex_error::VortexResult;
 use vortex_scalar::Scalar;
 
@@ -6,8 +8,14 @@ use crate::DictArray;
 
 impl ArrayOperationsImpl for DictArray {
     fn _slice(&self, start: usize, stop: usize) -> VortexResult<ArrayRef> {
-        DictArray::try_new(self.codes().slice(start, stop)?, self.values().clone())
-            .map(|a| a.into_array())
+        let sliced_code = self.codes().slice(start, stop)?;
+        if sliced_code.is_encoding(ConstantEncoding.id()) {
+            let code = usize::try_from(&sliced_code.scalar_at(0)?)?;
+            return Ok(
+                ConstantArray::new(self.values().scalar_at(code)?, sliced_code.len()).to_array(),
+            );
+        }
+        DictArray::try_new(sliced_code, self.values().clone()).map(|a| a.into_array())
     }
 
     fn _scalar_at(&self, index: usize) -> VortexResult<Scalar> {
