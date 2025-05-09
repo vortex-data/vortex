28fc16e28fefa122da9dab6812f99fca11a317df
Nicholas Gates <nick@nickgates.com>
2025-05-09 15:43:13 +0100

    Variant types
    Signed-off-by: Nicholas Gates <nick@nickgates.com>

--- vortex-array/src/vtable/encode.rs
+++ vortex-array/src/vtable/encode.rs
@@ -8,12 +8,20 @@ pub trait EncodeVTable<V: VTable> {
     ///
     /// If this encoding does not support encoding the given array, then `None` is returned.
     // TODO(ngates): pass some abstract `dyn EncodeOptions`?
-    fn encode(_canonical: &Canonical, _like: Option<&dyn Array>) -> VortexResult<Option<V::Array>>;
+    fn encode(
+        encoding: &V::Encoding,
+        canonical: &Canonical,
+        like: Option<&dyn Array>,
+    ) -> VortexResult<Option<V::Array>>;
 }
 
 /// Default implementation for encodings that do not support encoding.
 impl<V: VTable> EncodeVTable<V> for () {
-    fn encode(_canonical: &Canonical, _like: Option<&dyn Array>) -> VortexResult<Option<V::Array>> {
+    fn encode(
+        _encoding: &V::Encoding,
+        _canonical: &Canonical,
+        _like: Option<&dyn Array>,
+    ) -> VortexResult<Option<V::Array>> {
         Ok(None)
     }
 }
