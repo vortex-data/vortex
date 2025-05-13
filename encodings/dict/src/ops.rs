b1011343967c8d34f540402c0e4ba4a4440c6f4b
Joe Isaacs <joe.isaacs@live.co.uk>
2025-05-13 11:30:29 +0100

    test
    Signed-off-by: Joe Isaacs <joe.isaacs@live.co.uk>

--- encodings/dict/src/ops.rs
+++ encodings/dict/src/ops.rs
@@ -31,3 +31,31 @@ impl ArrayOperationsImpl for DictArray {
         self.values().scalar_at(dict_index)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use vortex_array::arrays::PrimitiveArray;
+    use vortex_array::{Array, ArrayStatistics};
+    use vortex_scalar::Scalar;
+
+    use crate::DictArray;
+
+    #[test]
+    fn test_slice_into_const_dict() {
+        let dict = DictArray::try_new(
+            PrimitiveArray::from_option_iter(vec![Some(0u32), None, Some(1)]).to_array(),
+            PrimitiveArray::from_option_iter(vec![Some(0i32), Some(1), Some(2)]).to_array(),
+        )
+        .unwrap();
+
+        assert_eq!(
+            Some(Scalar::new(dict.dtype().clone(), 0i32.into())),
+            dict.slice(0, 1).unwrap().as_constant()
+        );
+
+        assert_eq!(
+            Some(Scalar::null(dict.dtype().clone())),
+            dict.slice(1, 2).unwrap().as_constant()
+        );
+    }
+}
