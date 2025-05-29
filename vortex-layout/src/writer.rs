d8fe8ee49df245a2853aae5ff3485c52ea23c2e3
Onur Satici <onur@spiraldb.com>
2025-05-29 12:21:44 +0100

    docs
    Signed-off-by: Onur Satici <onur@spiraldb.com>

--- vortex-layout/src/writer.rs
+++ vortex-layout/src/writer.rs
@@ -4,7 +4,11 @@ use vortex_error::VortexResult;
 
 use crate::LayoutRef;
 
+/// A future created by a strategy to yield a layout. It is its own
+/// trait to be potentially extended with new methods.
+// [layout writer]
 pub trait LayoutWriter: Future<Output = VortexResult<LayoutRef>> {}
+// [layout writer]
 
 // Allow async blocks to impl LayoutWriter, this would change if more methods
 // are added to LayoutWriter.
