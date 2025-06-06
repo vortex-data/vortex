88309a00ca6d403c9b2fa2f608b5fc7e3104b865
Joe Isaacs <joe.isaacs@live.co.uk>
2025-06-06 17:45:14 +0100

    feat: list_contains constant list, array value (#3461)
    Support `list_contains({[1,2,3]}, {1, 2, 4}) = {t,t,f}`
    
    This is pretty slow, but that can be refined
    
    Signed-off-by: Joe Isaacs <joe.isaacs@live.co.uk>
    
    ---------
    
    Signed-off-by: Joe Isaacs <joe.isaacs@live.co.uk>

--- vortex-array/src/arrays/bool/test.rs
+++ vortex-array/src/arrays/bool/test.rs
@@ -1,16 +1,31 @@
 use itertools::Itertools;
-use vortex_error::VortexResult;
+use vortex_error::{VortexResult, vortex_bail};
 
 use crate::arrays::BoolArray;
 
 impl BoolArray {
-    pub fn opt_iter(&self) -> VortexResult<impl IntoIterator<Item = Option<bool>>> {
-        let values = self.boolean_buffer().clone();
-        let mask = self.validity_mask()?.to_boolean_buffer();
-        Ok(mask
+    pub fn opt_bool_vec(&self) -> VortexResult<Vec<Option<bool>>> {
+        Ok(self
+            .validity_mask()?
+            .to_boolean_buffer()
             .into_iter()
-            .zip(values.iter())
-            .map(|(valid, value)| valid.then_some(value))
+            .zip(self.boolean_buffer().iter())
+            .map(move |(valid, value)| valid.then_some(value))
             .collect_vec())
+    }
+
+    pub fn bool_vec(&self) -> VortexResult<Vec<bool>> {
+        self.validity_mask()?
+            .to_boolean_buffer()
+            .into_iter()
+            .zip(self.boolean_buffer().iter())
+            .map(move |(valid, value)| {
+                if !valid {
+                    vortex_bail!("trying to get bool values from an array with null elements")
+                }
+
+                Ok(value)
+            })
+            .try_collect()
     }
 }
