bdc84ab7a4bf82e4f527a41bbd1bba98432f9a34
Joe Isaacs <joe.isaacs@live.co.uk>
2025-06-11 11:15:12 +0100

    is constant run end
    Signed-off-by: Joe Isaacs <joe.isaacs@live.co.uk>

---
+++ encodings/runend/src/compute/is_constant.rs
@@ -0,0 +1,29 @@
+use vortex_array::compute::{
+    IsConstantKernel, IsConstantKernelAdapter, IsConstantOpts, is_constant_opts,
+};
+use vortex_array::stats::Min;
+use vortex_array::{Array, register_kernel};
+use vortex_error::VortexResult;
+
+use crate::RunEndVTable;
+
+impl IsConstantKernel for RunEndVTable {
+    fn is_constant(
+        &self,
+        array: &Self::Array,
+        opts: &IsConstantOpts,
+    ) -> VortexResult<Option<bool>> {
+        // If there are known to be me 0 len runs then we can check if constant on the values.
+        if array
+            .ends()
+            .statistics()
+            .get_as_bound::<Min, u64>()
+            .is_some_and(|b| b > 0)
+        {
+            return is_constant_opts(array.values(), opts);
+        }
+        Ok(None)
+    }
+}
+
+register_kernel!(IsConstantKernelAdapter(RunEndVTable).lift());
