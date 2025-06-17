7186d743bba7de6ec654f76921cbf4a006bad3d5
Joe Isaacs <joe.isaacs@live.co.uk>
2025-06-17 09:42:40 +0100

    feat[vortex-array]: add list contains compute fn (#3528)
    Make `list_contains` a compute fn, which specialises on the elements
    being search, not the list array.
    
    ---------
    
    Signed-off-by: Joe Isaacs <joe.isaacs@live.co.uk>

---
+++ encodings/sequence/src/compute/list_contains.rs
@@ -0,0 +1,104 @@
+use vortex_array::arrays::BoolArray;
+use vortex_array::compute::{ListContainsKernel, ListContainsKernelAdapter};
+use vortex_array::{Array, ArrayRef, register_kernel};
+use vortex_error::{VortexExpect, VortexResult};
+
+use crate::array::SequenceVTable;
+use crate::compute::compare::find_intersection_scalar;
+
+impl ListContainsKernel for SequenceVTable {
+    fn list_contains(
+        &self,
+        list: &dyn Array,
+        element: &Self::Array,
+    ) -> VortexResult<Option<ArrayRef>> {
+        let Some(list_scalar) = list.as_constant() else {
+            return Ok(None);
+        };
+
+        let list_elements = list_scalar
+            .as_list()
+            .elements()
+            .vortex_expect("non-null element (checked in entry)");
+
+        let set_indices = list_elements
+            .iter()
+            .flat_map(|elem| {
+                elem.as_primitive().pvalue().and_then(|intercept| {
+                    find_intersection_scalar(
+                        element.base(),
+                        element.multiplier(),
+                        element.len(),
+                        intercept,
+                    )
+                })
+            })
+            .collect::<Vec<_>>();
+
+        let nullability = list.dtype().nullability() | element.dtype().nullability();
+
+        Ok(Some(
+            BoolArray::from_indices(element.len(), set_indices, nullability.into()).to_array(),
+        ))
+    }
+}
+
+register_kernel!(ListContainsKernelAdapter(SequenceVTable).lift());
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
+
+    use vortex_array::ToCanonical;
+    use vortex_array::arrays::ConstantArray;
+    use vortex_array::compute::list_contains;
+    use vortex_dtype::Nullability;
+    use vortex_dtype::PType::I32;
+    use vortex_scalar::Scalar;
+
+    use crate::SequenceArray;
+
+    #[test]
+    fn test_list_contains_seq() {
+        let elements = ConstantArray::new(
+            Scalar::list(
+                Arc::new(I32.into()),
+                vec![1.into(), 3.into()],
+                Nullability::Nullable,
+            ),
+            3,
+        );
+
+        {
+            // [1, 3] in  1
+            //            2
+            //            3
+            let array = SequenceArray::typed_new(1, 1, 3).unwrap();
+
+            let res = list_contains(elements.as_ref(), array.as_ref())
+                .unwrap()
+                .to_bool()
+                .unwrap()
+                .bool_vec()
+                .unwrap();
+
+            assert_eq!(res, vec![true, false, true]);
+        }
+
+        {
+            // [1, 3] in  1
+            //            3
+            //            5
+            let array = SequenceArray::typed_new(1, 2, 3).unwrap();
+
+            let res = list_contains(elements.as_ref(), array.as_ref())
+                .unwrap()
+                .to_bool()
+                .unwrap()
+                .bool_vec()
+                .unwrap();
+
+            assert_eq!(res, vec![true, true, false]);
+        }
+    }
+}
