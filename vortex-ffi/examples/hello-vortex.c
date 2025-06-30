f528d45a935985d36e019ef54cda8bc44ac62a67
Andrew Duffy <andrew@a10y.dev>
2025-06-30 17:23:41 +0100

    fix: FFI example fixed up, hooked into CI
    Signed-off-by: Andrew Duffy <andrew@a10y.dev>

--- vortex-ffi/examples/hello-vortex.c
+++ vortex-ffi/examples/hello-vortex.c
@@ -3,40 +3,65 @@
 #include "vortex.h"
 
 int main(int argc, char *argv[]) {
-    // enable logging
-    vortex_init_logging(LOG_LEVEL_INFO);
-
     if (argc < 2) {
         printf("Usage: %s <VORTEX_FILE_URI>\n", argv[0]);
         return 1;
     }
 
+    vx_error *error = NULL;
+    vx_session *session = vx_session_new();
+    if (session == NULL) {
+        fprintf(stderr, "vx_session_new return NULL\n");
+        return -1;
+    }
+
     // Open the file
     char *path = argv[1];
-    FileOpenOptions open_opts = {
+
+    printf("Opening file %s\n", path);
+
+    vx_file_open_options open_opts = {
       .uri = path,
       .property_keys = NULL,
       .property_vals = NULL,
       .property_len = 0,
     };
     printf("Scanning file: %s\n", path);
-    File *file = File_open(&open_opts);
 
-    // Start scanning, read new rows.
-    ArrayStream *stream = File_scan(file, NULL);
-    int chunk = 0;
-    while (FFIArrayStream_next(stream)) {
-        Array *array = FFIArrayStream_current(stream);
-        int len = FFIArray_len(array);
-        printf("Chunk %d: %d\n", chunk++, len);
-        FFIArray_free(array);
+    const vx_file *file = vx_file_open_reader(&open_opts, session, &error);
+    if (error != NULL) {
+        fprintf(stderr, "error opening file\n");
+
+        return -1;
     }
 
-    printf("Scanning complete\n");
+    // Start scanning, read new rows.
+    vx_array_iterator *scan = vx_file_scan(file, NULL, &error);
 
-    // Cleanup resources.
-    FFIArrayStream_free(stream);
-    File_free(file);
+    int chunk = 0;
+    const vx_array *batch = vx_array_iterator_next(scan, &error);
+    while (batch != NULL) {
+        size_t len = vx_array_len(batch);
+        printf("chunk %d has length %ld\n", chunk++, len);
 
+        // free the batch
+        vx_array_free(batch);
+        // grab the next batch.
+        batch = vx_array_iterator_next(scan, &error);
+    }
+
+    vx_array_iterator_free(scan);
+    vx_file_free(file);
+
+    if (error != NULL) {
+        fprintf(stderr, "failed in scan operation\n");
+        vx_session_free(session);
+        vx_error_free(error);
+        return -1;
+    }
+
+    printf("Scanning completed successfully\n");
+
+    vx_session_free(session);
     return 0;
 }
