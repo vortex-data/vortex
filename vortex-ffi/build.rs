3e39f0666b7e72556b4974a2277c7e6998767f79
Alexander Droste <alexander.droste@protonmail.com>
2025-07-01 16:34:47 +0100

    conditionally write header to file
    Signed-off-by: Alexander Droste <alexander.droste@protonmail.com>

--- vortex-ffi/build.rs
+++ vortex-ffi/build.rs
@@ -2,18 +2,19 @@
 use std::env;
 use std::path::PathBuf;
 
-#[cfg_attr(miri, ignore)]
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let output_file = PathBuf::from(&crate_dir).join("cinclude").join("vortex.h");
     std::fs::create_dir_all(output_file.parent().unwrap()).unwrap();
 
-    cbindgen::Builder::new()
+    if let Ok(builder) = cbindgen::Builder::new()
         .with_crate(&crate_dir)
         .with_config(cbindgen::Config::from_file("cbindgen.toml").unwrap())
         .generate()
-        .unwrap()
-        .write_to_file(&output_file);
+    {
+        // Miri fails on header generation which is why it is written conditionally.
+        builder.write_to_file(&output_file);
+    }
 
     println!("cargo:rerun-if-changed=src/");
     println!("cargo:rerun-if-changed=cbindgen.toml");
