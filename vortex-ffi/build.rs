cc96985f4b6ec7b8a0ea531d7f4d93ec618d6a34
Alexander Droste <alexander.droste@protonmail.com>
2025-07-01 15:39:59 +0100

    fix miri run
    Signed-off-by: Alexander Droste <alexander.droste@protonmail.com>

--- vortex-ffi/build.rs
+++ vortex-ffi/build.rs
@@ -3,10 +3,20 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
+    if let Ok(crate_types) = env::var("CARGO_CFG_CRATE_TYPE") {
+        // Skip C header generation if not building a C-compatible library.
+        // As of now, we do this in order to not run Miri on the build.rs file.
+        if !crate_types.contains("cdylib") && !crate_types.contains("staticlib") {
+            println!(
+                "cargo:warning=Skipping C header generation (not building C-compatible library)"
+            );
+            return;
+        }
+    }
+
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let output_file = PathBuf::from(&crate_dir).join("cinclude").join("vortex.h");
     println!("{crate_dir}");
-
     std::fs::create_dir_all(output_file.parent().unwrap()).unwrap();
 
     cbindgen::Builder::new()
