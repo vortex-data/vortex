53deb39de9b4c7de50f11dd6ba3edebf8d4693cb
Alexander Droste <alexander.droste@protonmail.com>
2025-07-17 16:10:18 +0100

    drop unused canonical_cache
    Signed-off-by: Alexander Droste <alexander.droste@protonmail.com>

--- vortex-duckdb/src/exporter/cache.rs
+++ vortex-duckdb/src/exporter/cache.rs
@@ -5,8 +5,7 @@ use parking_lot::Mutex;
 use std::sync::Arc;
 
 use dashmap::DashMap;
-use vortex::error::{VortexExpect, VortexResult};
-use vortex::{Array, ArrayRef, Canonical, IntoArray};
+use vortex::ArrayRef;
 
 use crate::duckdb::Vector;
 
@@ -17,7 +16,6 @@ use crate::duckdb::Vector;
 #[derive(Default)]
 pub struct ConversionCache {
     pub values_cache: DashMap<usize, (ArrayRef, Arc<Mutex<Vector>>)>,
-    pub canonical_cache: DashMap<usize, (ArrayRef, Canonical)>,
     // A value which must be unique for a given DuckDB pipeline.
     instance_id: u64,
 }
@@ -32,39 +30,5 @@ impl ConversionCache {
 
     pub fn instance_id(&self) -> u64 {
         self.instance_id
-    }
-
-    fn insert_cached_array(&self, arr_value: usize, array: &ArrayRef) -> VortexResult<ArrayRef> {
-        let canon = array.to_canonical()?;
-        self.canonical_cache
-            .insert(arr_value, (array.clone(), canon));
-        Ok(self
-            .canonical_cache
-            .get(&arr_value)
-            .vortex_expect("just added")
-            .1
-            .clone()
-            .into_array())
-    }
-
-    pub fn cached_array(&self, array: &ArrayRef) -> VortexResult<ArrayRef> {
-        let arr_value = Arc::as_ptr(array).addr();
-
-        // Check if we have an entry and extract the information we need
-        let cache_result = self.canonical_cache.get(&arr_value).map(|entry| {
-            let (cached_array_ref, cached_canonical) = entry.value();
-            (cached_array_ref.clone(), cached_canonical.clone())
-        });
-
-        match cache_result {
-            None => self.insert_cached_array(arr_value, array),
-            Some((cached_array, cached_canonical)) => {
-                if Arc::ptr_eq(&cached_array, array) {
-                    Ok(cached_canonical.into_array())
-                } else {
-                    self.insert_cached_array(arr_value, array)
-                }
-            }
-        }
     }
 }
