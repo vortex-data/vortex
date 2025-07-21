301b27f5f6ff5c70c3b66af8892b092475584051
Adam Gutglick <adam@spiraldb.com>
2025-07-21 11:45:59 +0100

    chore: Propagate traces to task executor and a bunch of other places (#3947)
    Signed-off-by: Adam Gutglick <adam@spiraldb.com>

--- vortex-datafusion/src/persistent/format.rs
+++ vortex-datafusion/src/persistent/format.rs
@@ -9,6 +9,7 @@ use async_trait::async_trait;
 use datafusion::arrow::datatypes::{Schema, SchemaRef};
 use datafusion::catalog::Session;
 use datafusion::common::parsers::CompressionTypeVariant;
+use datafusion::common::runtime::SpawnedTask;
 use datafusion::common::stats::Precision;
 use datafusion::common::{
     ColumnStatistics, DataFusionError, GetExt, Result as DFResult, Statistics,
@@ -171,7 +172,7 @@ impl FileFormat for VortexFormat {
             .map(|o| {
                 let store = store.clone();
                 let cache = self.file_cache.clone();
-                tokio::task::spawn(async move {
+                SpawnedTask::spawn(async move {
                     let vxf = cache.try_get(&o, store).await?;
                     let inferred_schema = vxf.dtype().to_arrow_schema()?;
                     VortexResult::Ok((o.location, inferred_schema))
@@ -202,7 +203,7 @@ impl FileFormat for VortexFormat {
         let object = object.clone();
         let store = store.clone();
         let cache = self.file_cache.clone();
-        tokio::task::spawn(async move {
+        SpawnedTask::spawn(async move {
             let vxf = cache.try_get(&object, store.clone()).await.map_err(|e| {
                 DataFusionError::Execution(format!(
                     "Failed to open Vortex file {}: {e}",
