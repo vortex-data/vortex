0be35c8bc685b4449ebcb288e1ff0773c4c985eb
Will Manning <will@willmanning.io>
2025-07-24 17:57:22 -0400

    wip
    Signed-off-by: Will Manning <will@willmanning.io>

--- vortex/benches/single_encoding_throughput.rs
+++ vortex/benches/single_encoding_throughput.rs
@@ -10,9 +10,7 @@ use vortex::arrays::{PrimitiveArray, VarBinViewArray};
 use vortex::compute::cast;
 use vortex::encodings::alp::{RDEncoder, alp_encode};
 use vortex::encodings::dict::builders::dict_encode;
-use vortex::encodings::fastlanes::{
-    DeltaArray, FoRArray, bitpack_to_best_bit_width, delta_compress,
-};
+use vortex::encodings::fastlanes::{DeltaArray, delta_compress};
 use vortex::encodings::fsst::{fsst_compress, fsst_train_compressor};
 use vortex::encodings::pco::PcoArray;
 use vortex::encodings::runend::RunEndArray;
@@ -52,21 +50,29 @@ mod primitive_decompression {
 
     #[divan::bench(name = "bitpacked_compress")]
     fn bench_bitpacked_compress(bencher: Bencher) {
+        use vortex::encodings::fastlanes::bitpack_encode_unchecked;
+
         let (uint_array, _, _) = setup_arrays();
+        let bit_width = 8;
 
         bencher
             .counter(BytesCount::new(NUM_VALUES * 4))
-            .bench_local(|| bitpack_to_best_bit_width(&uint_array).unwrap());
+            .with_inputs(|| uint_array.clone())
+            .bench_values(|a| unsafe { bitpack_encode_unchecked(a, bit_width).unwrap() });
     }
 
     #[divan::bench(name = "bitpacked_decompress")]
     fn bench_bitpacked_decompress(bencher: Bencher) {
+        use vortex::encodings::fastlanes::bitpack_encode;
+
         let (uint_array, _, _) = setup_arrays();
-        let compressed = bitpack_to_best_bit_width(&uint_array).unwrap();
+        let bit_width = 8;
+        let compressed = bitpack_encode(&uint_array, bit_width, None).unwrap();
 
         bencher
             .counter(BytesCount::new(NUM_VALUES * 4))
-            .bench_local(move || compressed.clone().to_canonical().unwrap());
+            .with_inputs(|| compressed.clone())
+            .bench_values(|a| a.to_canonical().unwrap());
     }
 
     #[divan::bench(name = "runend_compress")]
@@ -75,7 +81,8 @@ mod primitive_decompression {
 
         bencher
             .counter(BytesCount::new(NUM_VALUES * 4))
-            .bench_local(|| RunEndArray::encode(uint_array.clone().into_array()).unwrap());
+            .with_inputs(|| uint_array.clone())
+            .bench_values(|a| RunEndArray::encode(a.into_array()).unwrap());
     }
 
     #[divan::bench(name = "runend_decompress")]
@@ -85,7 +92,8 @@ mod primitive_decompression {
 
         bencher
             .counter(BytesCount::new(NUM_VALUES * 4))
-            .bench_local(move || compressed.clone().to_canonical().unwrap());
+            .with_inputs(|| compressed.clone())
+            .bench_values(|a| a.to_canonical().unwrap());
     }
 
     #[divan::bench(name = "delta_compress")]
@@ -94,8 +102,9 @@ mod primitive_decompression {
 
         bencher
             .counter(BytesCount::new(NUM_VALUES * 4))
-            .bench_local(|| {
-                let (bases, deltas) = delta_compress(&uint_array).unwrap();
+            .with_inputs(|| uint_array.clone())
+            .bench_values(|a| {
+                let (bases, deltas) = delta_compress(&a).unwrap();
                 DeltaArray::try_from_delta_compress_parts(
                     bases.into_array(),
                     deltas.into_array(),
@@ -118,7 +127,8 @@ mod primitive_decompression {
 
         bencher
             .counter(BytesCount::new(NUM_VALUES * 4))
-            .bench_local(move || compressed.clone().to_canonical().unwrap());
+            .with_inputs(|| compressed.clone())
+            .bench_values(|a| a.to_canonical().unwrap());
     }
 
     #[divan::bench(name = "dict_compress")]
@@ -127,7 +137,8 @@ mod primitive_decompression {
 
         bencher
             .counter(BytesCount::new(NUM_VALUES * 4))
-            .bench_local(|| dict_encode(uint_array.as_ref()).unwrap());
+            .with_inputs(|| uint_array.clone())
+            .bench_values(|a| dict_encode(a.as_ref()).unwrap());
     }
 
     #[divan::bench(name = "dict_decompress")]
@@ -137,26 +148,8 @@ mod primitive_decompression {
 
         bencher
             .counter(BytesCount::new(NUM_VALUES * 4))
-            .bench_local(move || compressed.clone().to_canonical().unwrap());
-    }
-
-    #[divan::bench(name = "frame_of_reference_compress")]
-    fn bench_for_compress(bencher: Bencher) {
-        let (_, int_array, _) = setup_arrays();
-
-        bencher
-            .counter(BytesCount::new(NUM_VALUES * 4))
-            .bench_local(|| FoRArray::encode(int_array.clone()).unwrap());
-    }
-
-    #[divan::bench(name = "frame_of_reference_decompress")]
-    fn bench_for_decompress(bencher: Bencher) {
-        let (_, int_array, _) = setup_arrays();
-        let compressed = FoRArray::encode(int_array).unwrap();
-
-        bencher
-            .counter(BytesCount::new(NUM_VALUES * 4))
-            .bench_local(move || compressed.clone().to_canonical().unwrap());
+            .with_inputs(|| compressed.clone())
+            .bench_values(|a| a.to_canonical().unwrap());
     }
 
     #[divan::bench(name = "zigzag_compress")]
@@ -165,7 +158,8 @@ mod primitive_decompression {
 
         bencher
             .counter(BytesCount::new(NUM_VALUES * 4))
-            .bench_local(|| zigzag_encode(int_array.clone()).unwrap());
+            .with_inputs(|| int_array.clone())
+            .bench_values(|a| zigzag_encode(a).unwrap());
     }
 
     #[divan::bench(name = "zigzag_decompress")]
@@ -175,7 +169,8 @@ mod primitive_decompression {
 
         bencher
             .counter(BytesCount::new(NUM_VALUES * 4))
-            .bench_local(move || compressed.clone().to_canonical().unwrap());
+            .with_inputs(|| compressed.clone())
+            .bench_values(|a| a.to_canonical().unwrap());
     }
 
     #[divan::bench(name = "alp_compress")]
@@ -184,7 +179,8 @@ mod primitive_decompression {
 
         bencher
             .counter(BytesCount::new(NUM_VALUES * 4))
-            .bench_local(|| alp_encode(&float_array, None).unwrap());
+            .with_inputs(|| float_array.clone())
+            .bench_values(|a| alp_encode(&a, None).unwrap());
     }
 
     #[divan::bench(name = "alp_decompress")]
@@ -194,7 +190,8 @@ mod primitive_decompression {
 
         bencher
             .counter(BytesCount::new(NUM_VALUES * 4))
-            .bench_local(move || compressed.clone().to_canonical().unwrap());
+            .with_inputs(|| compressed.clone())
+            .bench_values(|a| a.to_canonical().unwrap());
     }
 
     #[divan::bench(name = "alp_rd_compress")]
@@ -203,9 +200,10 @@ mod primitive_decompression {
 
         bencher
             .counter(BytesCount::new(NUM_VALUES * 4))
-            .bench_local(|| {
-                let encoder = RDEncoder::new(float_array.as_slice::<f32>());
-                encoder.encode(&float_array)
+            .with_inputs(|| float_array.clone())
+            .bench_values(|a| {
+                let encoder = RDEncoder::new(a.as_slice::<f32>());
+                encoder.encode(&a)
             });
     }
 
@@ -217,7 +215,8 @@ mod primitive_decompression {
 
         bencher
             .counter(BytesCount::new(NUM_VALUES * 4))
-            .bench_local(move || compressed.clone().to_canonical().unwrap());
+            .with_inputs(|| compressed.clone())
+            .bench_values(|a| a.to_canonical().unwrap());
     }
 
     #[divan::bench(name = "pcodec_compress")]
@@ -226,7 +225,8 @@ mod primitive_decompression {
 
         bencher
             .counter(BytesCount::new(NUM_VALUES * 4))
-            .bench_local(|| PcoArray::from_primitive(&float_array, 3, 0).unwrap());
+            .with_inputs(|| float_array.clone())
+            .bench_values(|a| PcoArray::from_primitive(&a, 3, 0).unwrap());
     }
 
     #[divan::bench(name = "pcodec_decompress")]
@@ -236,7 +236,8 @@ mod primitive_decompression {
 
         bencher
             .counter(BytesCount::new(NUM_VALUES * 4))
-            .bench_local(move || compressed.clone().to_canonical().unwrap());
+            .with_inputs(|| compressed.clone())
+            .bench_values(|a| a.to_canonical().unwrap());
     }
 
     #[divan::bench(name = "zstd_compress")]
@@ -245,9 +246,8 @@ mod primitive_decompression {
 
         bencher
             .counter(BytesCount::new(NUM_VALUES * 4))
-            .bench_local(|| {
-                ZstdArray::from_array(uint_array.clone().into_array(), 3, 8192).unwrap()
-            });
+            .with_inputs(|| uint_array.clone())
+            .bench_values(|a| ZstdArray::from_array(a.into_array(), 3, 8192).unwrap());
     }
 
     #[divan::bench(name = "zstd_decompress")]
@@ -257,7 +257,8 @@ mod primitive_decompression {
 
         bencher
             .counter(BytesCount::new(NUM_VALUES * 4))
-            .bench_local(move || compressed.clone().to_canonical().unwrap());
+            .with_inputs(|| compressed.clone())
+            .bench_values(|a| a.to_canonical().unwrap());
     }
 }
 
@@ -290,7 +291,8 @@ mod string_decompression {
 
         bencher
             .counter(BytesCount::new(nbytes))
-            .bench_local(move || dict.clone().to_canonical().unwrap());
+            .with_inputs(|| dict.clone())
+            .bench_values(|a| a.to_canonical().unwrap());
     }
 
     #[divan::bench(name = "fsst_decompress_varbinview")]
@@ -303,6 +305,7 @@ mod string_decompression {
 
         bencher
             .counter(BytesCount::new(nbytes))
-            .bench_local(move || fsst_array.clone().to_canonical().unwrap());
+            .with_inputs(|| fsst_array.clone())
+            .bench_values(|a| a.to_canonical().unwrap());
     }
 }
