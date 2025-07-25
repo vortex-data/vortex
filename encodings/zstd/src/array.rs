d7dc85dfad8bcf4a6357852473ef5a5c00cea8f6
Robert Kruszewski <github@robertk.io>
2025-07-25 15:10:56 +0100

    less
    Signed-off-by: Robert Kruszewski <github@robertk.io>

--- encodings/zstd/src/array.rs
+++ encodings/zstd/src/array.rs
@@ -485,6 +485,7 @@ impl ZstdArray {
         ZstdArray {
             slice_start: self.slice_start + start,
             slice_stop: self.slice_start + stop,
+            stats_set: Default::default(),
             ..self.clone()
         }
     }
