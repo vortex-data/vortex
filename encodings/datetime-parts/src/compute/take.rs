9e1b922e7f7c9c6188d482e724d186e0cd46d8f4
Will Manning <will@willmanning.io>
2025-07-28 17:45:17 -0400

    fixes
    Signed-off-by: Will Manning <will@willmanning.io>

--- encodings/datetime-parts/src/compute/take.rs
+++ encodings/datetime-parts/src/compute/take.rs
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: Copyright the Vortex contributors
 
-use vortex_array::compute::{TakeKernel, TakeKernelAdapter, take, fill_null};
-use vortex_array::{Array, ArrayRef, IntoArray, register_kernel};
+use vortex_array::compute::{TakeKernel, TakeKernelAdapter, fill_null, take};
+use vortex_array::{Array, ArrayRef, IntoArray, ToCanonical, register_kernel};
 use vortex_error::VortexResult;
 use vortex_scalar::Scalar;
 
@@ -10,56 +10,67 @@ use crate::{DateTimePartsArray, DateTimePartsVTable};
 
 impl TakeKernel for DateTimePartsVTable {
     fn take(&self, array: &DateTimePartsArray, indices: &dyn Array) -> VortexResult<ArrayRef> {
+        // we go ahead and canonicalize here to avoid worst-case canonicalizing 3 separate times
+        let indices = indices.to_primitive()?;
+
         let taken_days = take(array.days(), indices)?;
         let taken_seconds = take(array.seconds(), indices)?;
         let taken_subseconds = take(array.subseconds(), indices)?;
-        
+
         // Update the dtype if the nullability changed due to nullable indices
         let dtype = if taken_days.dtype().is_nullable() != array.dtype().is_nullable() {
-            array.dtype().with_nullability(taken_days.dtype().nullability())
+            array
+                .dtype()
+                .with_nullability(taken_days.dtype().nullability())
         } else {
             array.dtype().clone()
         };
-        
-        // DateTimePartsArray requires seconds and subseconds to be non-nullable
-        // If they became nullable due to nullable indices, we need to fill nulls
-        let final_seconds = if taken_seconds.dtype().is_nullable() {
-            // Find the first non-null value in the original seconds array to use as fill value
-            let fill_value = find_first_non_null_value(array.seconds())
-                .unwrap_or_else(|| Scalar::from(0i64));
-            fill_null(taken_seconds.as_ref(), &fill_value)?
+
+        // DateTimePartsArray requires seconds and subseconds to be non-nullable.
+        // If they became nullable due to nullable indices, we need to fill nulls.
+        let (taken_seconds, taken_subseconds) = if taken_seconds.dtype().is_nullable() {
+            if !taken_days.dtype().is_nullable() {
+                vortex_panic!("Mismatched types: days is not nullable, seconds is nullable");
+            }
+            if !taken_subseconds.dtype().is_nullable() {
+                vortex_panic!("Mismatched types: seconds is nullable, subseconds is not");
+            }
+            if !indices.dtype().is_nullable() {
+                vortex_panic!("Mismatched types: indices are not nullable, seconds is nullable");
+            }
+
+            // get the first non-null index, and use that to fill both seconds and subseconds
+            let fill_index: Option<usize> = find_first_non_null_value(indices).map(|i| i.as_());
+
+            let seconds_fill = fill_index
+                .map(|i| taken_seconds.scalar_at(i))
+                .transpose()?
+                .unwrap_or_else(|| {
+                    Scalar::primitive(0i64, Nullability::NonNull).cast(taken_seconds.dtype())
+                });
+
+            let subseconds_fill = fill_index
+                .map(|i| taken_subseconds.scalar_at(i))
+                .transpose()?
+                .unwrap_or_else(|| {
+                    Scalar::primitive(0i64, Nullability::NonNull).cast(taken_subseconds.dtype())
+                });
+
+            let taken_seconds = fill_null(taken_seconds.as_ref(), &seconds_fill)?;
+            let taken_subseconds = fill_null(taken_subseconds.as_ref(), &subseconds_fill)?;
+            (taken_seconds, taken_subseconds)
         } else {
-            taken_seconds
+            (taken_seconds, taken_subseconds)
         };
-        
-        let final_subseconds = if taken_subseconds.dtype().is_nullable() {
-            // Find the first non-null value in the original subseconds array to use as fill value
-            let fill_value = find_first_non_null_value(array.subseconds())
-                .unwrap_or_else(|| Scalar::from(0i64));
-            fill_null(taken_subseconds.as_ref(), &fill_value)?
-        } else {
-            taken_subseconds
-        };
-        
-        Ok(DateTimePartsArray::try_new(
-            dtype,
-            taken_days,
-            final_seconds,
-            final_subseconds,
-        )?
-        .into_array())
+
+        Ok(
+            DateTimePartsArray::try_new(dtype, taken_days, taken_seconds, taken_subseconds)?
+                .into_array(),
+        )
     }
 }
 
-fn find_first_non_null_value(array: &dyn Array) -> Option<Scalar> {
-    for i in 0..array.len() {
-        let scalar = array.scalar_at(i).ok()?;
-        if !scalar.is_null() {
-            return Some(scalar);
-        }
-    }
-    None
-}
+fn find_first_non_null_value(array: &dyn Array) -> Option<Scalar> {}
 
 register_kernel!(TakeKernelAdapter(DateTimePartsVTable).lift());
 
