c93a46251ba36c3d5f516d23953cbef7b1a9ade4
blaginin <dima@spiraldb.com>
2025-07-29 19:54:53 +0100

    We don't need `PartialOrd`
    Signed-off-by: blaginin <dima@spiraldb.com>

--- vortex-scalar/src/scalar_value/mod.rs
+++ vortex-scalar/src/scalar_value/mod.rs
@@ -60,24 +60,6 @@ impl PartialEq for ScalarValue {
 
 impl Eq for ScalarValue {}
 
-// can we simplify Scalar cmp?
-// impl PartialOrd for ScalarValue {
-//     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-//         match (&self.0, &other.0) {
-//             (InnerScalarValue::Null, InnerScalarValue::Null) => Some(Ordering::Equal),
-//             (InnerScalarValue::Bool(l), InnerScalarValue::Bool(r)) => l.partial_cmp(r),
-//             (InnerScalarValue::Primitive(l), InnerScalarValue::Primitive(r)) => l.partial_cmp(r),
-//             (InnerScalarValue::Decimal(l), InnerScalarValue::Decimal(r)) => l.partial_cmp(r),
-//             (InnerScalarValue::Buffer(l), InnerScalarValue::Buffer(r)) => l.partial_cmp(r),
-//             (InnerScalarValue::BufferString(l), InnerScalarValue::BufferString(r)) => {
-//                 l.partial_cmp(r)
-//             }
-//             (InnerScalarValue::List(l), InnerScalarValue::List(r)) => l.as_ref().partial_cmp(r.as_ref()),
-//             (_, _) => None
-//         }
-//     }
-// }
-
 impl ScalarValue {
     /// Serializes the scalar value to Protocol Buffers format.
     pub fn to_protobytes<B: BufMut + Default>(&self) -> B {
