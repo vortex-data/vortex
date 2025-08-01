88b8daabd48291276c3715fd6f978cd3ff4124f0
Joe Isaacs <joe.isaacs@live.co.uk>
2025-08-01 14:59:36 +0100

    fix[duckdb]: make batch number unique over all threads
    Signed-off-by: Joe Isaacs <joe.isaacs@live.co.uk>

--- vortex-tui/src/convert.rs
+++ vortex-tui/src/convert.rs
@@ -1,20 +1,21 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: Copyright the Vortex contributors
 
-use std::fs::File;
 use std::path::PathBuf;
 use std::sync::Arc;
 
 use clap::{Parser, ValueEnum};
-use parquet::arrow::arrow_reader::ParquetRecordBatchReaderBuilder;
+use futures_util::StreamExt;
+use indicatif::ProgressBar;
+use parquet::arrow::ParquetRecordBatchStreamBuilder;
+use tokio::fs::File;
 use vortex::ArrayRef;
 use vortex::arrow::FromArrowArray;
 use vortex::compressor::CompactCompressor;
 use vortex::dtype::DType;
 use vortex::dtype::arrow::FromArrowType;
-use vortex::error::{VortexError, VortexResult};
+use vortex::error::{VortexError, VortexExpect, VortexResult};
 use vortex::file::{VortexLayoutStrategy, VortexWriteOptions};
-use vortex::iter::{ArrayIteratorAdapter, ArrayIteratorExt};
 use vortex::layout::LocalExecutor;
 use vortex::stream::ArrayStreamAdapter;
 
@@ -47,39 +48,32 @@ pub async fn exec_convert(flags: Flags) -> VortexResult<()> {
     }
 
     let output_path = input_path.with_extension("vortex");
-    let file = File::open(input_path)?;
+    let file = File::open(input_path).await?;
 
-    let parquet = ParquetRecordBatchReaderBuilder::try_new(file)?.with_batch_size(BATCH_SIZE);
+    let parquet = ParquetRecordBatchStreamBuilder::new(file)
+        .await?
+        .with_batch_size(BATCH_SIZE);
     let num_rows = parquet.metadata().file_metadata().num_rows();
 
     let dtype = DType::from_arrow(parquet.schema().as_ref());
-    // let mut vortex_stream = parquet
-    //     .build()?
-    //     .map(|record_batch| {
-    //         record_batch
-    //             .map_err(VortexError::from)
-    //             .map(|rb| ArrayRef::from_arrow(rb, false))
-    //     })
-    //     .boxed();
-
-    let vortex_stream = ArrayIteratorAdapter::new(
-        dtype.clone(),
-        parquet.build()?.map(|br| {
-            br.map_err(VortexError::from)
-                .map(|b| ArrayRef::from_arrow(b, false))
-        }),
-    )
-    .into_array_stream();
+    let mut vortex_stream = parquet
+        .build()?
+        .map(|record_batch| {
+            record_batch
+                .map_err(VortexError::from)
+                .map(|rb| ArrayRef::from_arrow(rb, false))
+        })
+        .boxed();
 
     if !flags.quiet {
         // Parquet reader returns batches, rather than row groups. So make sure we correctly
         // configure the progress bar.
-        // let _nbatches = u64::try_from(num_rows)
-        //     .vortex_expect("negative row count?")
-        //     .div_ceil(BATCH_SIZE as u64);
-        // vortex_stream = ProgressBar::new(nbatches)
-        //     .wrap_stream(vortex_stream)
-        //     .boxed();
+        let nbatches = u64::try_from(num_rows)
+            .vortex_expect("negative row count?")
+            .div_ceil(BATCH_SIZE as u64);
+        vortex_stream = ProgressBar::new(nbatches)
+            .wrap_stream(vortex_stream)
+            .boxed();
     }
 
     let executor = Arc::new(LocalExecutor);
@@ -92,7 +86,7 @@ pub async fn exec_convert(flags: Flags) -> VortexResult<()> {
     VortexWriteOptions::default()
         .with_strategy(strategy)
         .write(
-            tokio::fs::File::create(output_path).await?,
+            File::create(output_path).await?,
             ArrayStreamAdapter::new(dtype, vortex_stream),
         )
         .await?;
