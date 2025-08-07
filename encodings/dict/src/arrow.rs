97f6f7448ef079691ebe6ace86b882b5e4d382ef
Robert Kruszewski <github@robertk.io>
2025-08-07 17:09:03 +0100

    feature: Support converting Arrow DictArray into vortex
    Signed-off-by: Robert Kruszewski <github@robertk.io>

---
+++ encodings/dict/src/arrow.rs
@@ -0,0 +1,16 @@
+use arrow_array::types::ArrowDictionaryKeyType;
+use arrow_array::{AnyDictionaryArray, DictionaryArray};
+use vortex_array::ArrayRef;
+use vortex_array::arrow::FromArrowArray;
+use vortex_error::VortexUnwrap;
+
+use crate::DictArray;
+
+impl<K: ArrowDictionaryKeyType> FromArrowArray<&DictionaryArray<K>> for DictArray {
+    fn from_arrow(array: &DictionaryArray<K>, nullable: bool) -> Self {
+        let keys = AnyDictionaryArray::keys(array);
+        let keys = ArrayRef::from_arrow(keys, keys.is_nullable());
+        let values = ArrayRef::from_arrow(array.values().as_ref(), nullable);
+        DictArray::try_new(keys, values).vortex_unwrap()
+    }
+}
