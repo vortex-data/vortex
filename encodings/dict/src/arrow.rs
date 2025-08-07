ad5b40e8e82170aa4a77bbe2075fa6e853f62d4f
Robert Kruszewski <github@robertk.io>
2025-08-07 17:17:20 +0100

    format
    Signed-off-by: Robert Kruszewski <github@robertk.io>

--- encodings/dict/src/arrow.rs
+++ encodings/dict/src/arrow.rs
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright the Vortex contributors
+
 use arrow_array::types::ArrowDictionaryKeyType;
 use arrow_array::{AnyDictionaryArray, DictionaryArray};
 use vortex_array::ArrayRef;
