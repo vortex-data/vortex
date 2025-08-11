5d4f1e69f4d965ed9cc1898ba2b105ee1c97461d
Alexander Droste <alexander.droste@protonmail.com>
2025-08-11 11:14:39 +0100

    more locking
    Signed-off-by: Alexander Droste <alexander.droste@protonmail.com>

--- vortex-duckdb/cpp/table_filter.cpp
+++ vortex-duckdb/cpp/table_filter.cpp
@@ -89,6 +89,9 @@ extern "C" void duckdb_vx_table_filter_get_dynamic(duckdb_vx_table_filter ffi_fi
     }
     auto &filter = reinterpret_cast<TableFilter *>(ffi_filter)->Cast<DynamicFilter>();
 
+    // Hold the lock while accessing the filter data.
+    std::lock_guard<std::mutex> lock(filter.filter_data->lock);
+
     auto data_wrapper = new DynamicFilterDataWrapper(filter.filter_data);
     out->data = reinterpret_cast<duckdb_vx_dynamic_filter_data>(data_wrapper);
     out->comparison_type = static_cast<duckdb_vx_expr_type>(filter.filter_data->filter->comparison_type);
