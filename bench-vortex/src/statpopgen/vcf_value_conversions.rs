13f9119bf42cc345e4a31cbce6bfcf12a7ef590d
Daniel King <dan@spiraldb.com>
2025-08-11 11:24:41 -0400

    fix
    Signed-off-by: Daniel King <dan@spiraldb.com>
    Signed-off-by: Daniel King <daniel.zidan.king@gmail.com>
    Signed-off-by: Daniel King <dan@spiraldb.com>

--- bench-vortex/src/statpopgen/vcf_value_conversions.rs
+++ bench-vortex/src/statpopgen/vcf_value_conversions.rs
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright the Vortex contributors
+
 use arrow_array::builder::*;
 use itertools::Itertools as _;
 use noodles_vcf::Header;
