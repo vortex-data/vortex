f28b75e7630e8aa89144d1ba679fb1973ae45d0d
Robert Kruszewski <github@robertk.io>
2025-08-15 16:15:47 +0100

    bringback
    Signed-off-by: Robert Kruszewski <github@robertk.io>

--- vortex-duckdb/src/exporter/mod.rs
+++ vortex-duckdb/src/exporter/mod.rs
@@ -229,7 +229,8 @@ impl VectorExt for Vector {
                     unsafe { self.ensure_validity_slice(len) }.fill(false);
                 } else {
                     // SAFETY: Caller guaranteees this.
-                    (unsafe { self.ensure_validity_slice(len) }).copy_from_bitslice(sliced_bits);
+                    let validity = unsafe { self.ensure_validity_slice(len) };
+                    validity[..len].copy_from_bitslice(sliced_bits);
                 }
                 true_count == 0
             }
