1836ee398cabb86c9b7853da8a530ab4dd4d4116
Joe Isaacs <joe.isaacs@live.co.uk>
2025-08-18 10:51:32 +0100

    vector
    Signed-off-by: Joe Isaacs <joe.isaacs@live.co.uk>

--- vortex-array/src/lib.rs
+++ vortex-array/src/lib.rs
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: Copyright the Vortex contributors
 
-#![allow(dead_code)]
 #![allow(unused_variables)]
 #![cfg_attr(vortex_nightly, feature(portable_simd))]
 //! Vortex crate containing core logic for encoding and memory representation of [arrays](ArrayRef).
