df5c9747f8a8ea5a79280da5fc7f90096404e33a
Andrew Duffy <a10y@users.noreply.github.com>
2025-08-20 15:56:44 -0400

    fix: downscale FSST offsets (#4301)
    Similar to what we do for ListArray and VarBin, the compressor should be
    downscaling before calling into the IntCompressor.
    
    ---------
    
    Signed-off-by: Andrew Duffy <andrew@a10y.dev>

--- vortex-btrblocks/src/string.rs
+++ vortex-btrblocks/src/string.rs
@@ -2,6 +2,7 @@
 // SPDX-FileCopyrightText: Copyright the Vortex contributors
 
 use vortex_array::arrays::{VarBinArray, VarBinViewArray, VarBinViewVTable};
+use vortex_array::compress::downscale_integer_array;
 use vortex_array::vtable::ValidityHelper;
 use vortex_array::{ArrayRef, IntoArray, ToCanonical};
 use vortex_dict::DictArray;
@@ -237,15 +238,14 @@ impl Scheme for FSSTScheme {
         let fsst = fsst_compress(&stats.src.clone().into_array(), &compressor)?;
 
         let compressed_original_lengths = IntCompressor::compress(
-            &fsst.uncompressed_lengths().to_primitive()?,
+            &downscale_integer_array(fsst.uncompressed_lengths().clone())?.to_primitive()?,
             is_sample,
             allowed_cascading,
             &[],
         )?;
 
-        // We compress the var bin offsets of the FSST codes array.
         let compressed_codes_offsets = IntCompressor::compress(
-            &fsst.codes().offsets().to_primitive()?,
+            &downscale_integer_array(fsst.codes().offsets().clone())?.to_primitive()?,
             is_sample,
             allowed_cascading,
             &[],
