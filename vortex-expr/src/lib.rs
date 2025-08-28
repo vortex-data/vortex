b3375c013ee61024697c0e01200abeee0dfa9007
Daniel King <dan@spiraldb.com>
2025-08-28 10:22:37 -0400

    .
    Signed-off-by: Daniel King <dan@spiraldb.com>

--- vortex-expr/src/lib.rs
+++ vortex-expr/src/lib.rs
@@ -147,7 +147,7 @@ impl dyn VortexExpr + '_ {
     /// Compute result of expression on given batch producing a new batch
     pub fn evaluate(&self, scope: &Scope) -> VortexResult<ArrayRef> {
         let result = self.unchecked_evaluate(scope)?;
-        debug_assert_eq!(
+        assert_eq!(
             result.dtype(),
             &self.return_dtype(scope.dtype())?,
             "Expression {} returned dtype {} but declared return_dtype of {}",
