038760f40bf67015713f3c4484f95639b812b6a7
Marko Bakovic <markobakovic95@gmail.com>
2025-08-29 14:57:37 +0100

    This commit will be squashed.

--- vortex-python/python/vortex/__init__.py
+++ vortex-python/python/vortex/__init__.py
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright the Vortex contributors
 
-from . import _lib, arrays, dataset, expr, file, io, ray, scan
+from . import _lib, arrays, dataset, expr, io, ray, scan, file  # isort: skip
 from ._lib.arrays import (  # pyright: ignore[reportMissingModuleSource]
     AlpArray,
     AlpRdArray,
