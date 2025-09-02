3eab0ebccf5b460224df567073b427985fb093a5
Joe Isaacs <joe.isaacs@live.co.uk>
2025-09-02 15:41:55 +0100

    test
    Signed-off-by: Joe Isaacs <joe.isaacs@live.co.uk>

--- vortex-mask/benches/pack_bool.rs
+++ vortex-mask/benches/pack_bool.rs
@@ -13,8 +13,8 @@ fn main() {
 #[inline]
 pub fn collect_bool<F: FnMut(usize) -> bool>(output: &mut [u64], mut f: F) {
     let len = output.len();
-    let chunks = len / 64;
-    let remainder = len % 64;
+    let chunks = len;
+    // let remainder = len % 64;
     for chunk in 0..chunks {
         let mut packed = 0;
         for bit_idx in 0..64 {
@@ -26,16 +26,16 @@ pub fn collect_bool<F: FnMut(usize) -> bool>(output: &mut [u64], mut f: F) {
         unsafe { *output.get_unchecked_mut(chunk) = packed }
     }
 
-    if remainder != 0 {
-        let mut packed = 0;
-        for bit_idx in 0..remainder {
-            let i = bit_idx + chunks * 64;
-            packed |= (f(i) as u64) << bit_idx;
-        }
-
-        // SAFETY: Already allocated sufficient capacity
-        unsafe { *output.get_unchecked_mut(chunks) = packed }
-    }
+    // if remainder != 0 {
+    //     let mut packed = 0;
+    //     for bit_idx in 0..remainder {
+    //         let i = bit_idx + chunks * 64;
+    //         packed |= (f(i) as u64) << bit_idx;
+    //     }
+    //
+    //     // SAFETY: Already allocated sufficient capacity
+    //     unsafe { *output.get_unchecked_mut(chunks) = packed }
+    // }
 }
 
 #[inline(never)]
@@ -68,7 +68,7 @@ fn arrow_pack_bool2(bencher: Bencher, args: u64) {
 
     let bools = (0..args * 1024).map(|_| rng.random_bool(0.5)).collect_vec();
 
-    bencher
+    let outp = bencher
         .with_inputs(|| {
             (
                 bools.clone(),
@@ -80,14 +80,32 @@ fn arrow_pack_bool2(bencher: Bencher, args: u64) {
                 collect_bool_slice2(&mut output[i * 16..][..16], &bools[i * 1024..][..1024]);
             }
             output
-        })
+        });
+
+    {
+        let output1 = {
+            let mut output = (0..args as usize * 16).map(|_| 0u64).collect_vec();
+            collect_bool_slice(&mut output, &bools);
+            output
+        };
+
+        let output2 = {
+            let mut output = (0..args as usize * 16).map(|_| 0u64).collect_vec();
+            for i in 0..args as usize {
+                collect_bool_slice2(&mut output[i * 16..][..16], &bools[i * 1024..][..1024]);
+            }
+            output
+        };
+
+        assert_eq!(output1, output2);
+    }
 }
 
 #[inline]
 pub fn collect_bool2<F: FnMut(usize) -> bool>(output: &mut [u64], mut f: F) {
-    let len = 1024 / 64;
+    let len = 1024;
     let chunks = len / 64;
-    let remainder = len % 64;
+    // let remainder = len % 64;
     for chunk in 0..chunks {
         let mut packed_l = 0u32;
         let mut packed_u = 0u32;
@@ -106,19 +124,43 @@ pub fn collect_bool2<F: FnMut(usize) -> bool>(output: &mut [u64], mut f: F) {
         // SAFETY: Already allocated sufficient capacity
         unsafe { *output.get_unchecked_mut(chunk) = packed }
     }
-
-    if remainder != 0 {
-        let mut packed = 0;
-        for bit_idx in 0..remainder {
-            let i = bit_idx + chunks * 64;
-            packed |= (f(i) as u64) << bit_idx;
-        }
-
-        // SAFETY: Already allocated sufficient capacity
-        unsafe { *output.get_unchecked_mut(chunks) = packed }
-    }
 }
 #[inline(never)]
 pub fn collect_bool_slice2(output: &mut [u64], input: &[bool]) {
     collect_bool2(output, |i| input[i])
+}
+
+#[test]
+mod tests {
+    use itertools::Itertools;
+    use rand::prelude::StdRng;
+    use rand::{Rng, SeedableRng};
+
+    use crate::{collect_bool_slice, collect_bool_slice2};
+
+    #[test]
+    fn test123() {
+        let args = 5;
+        let mut rng = StdRng::seed_from_u64(0);
+
+        let bools = (0..args * 1024).map(|_| rng.random_bool(0.5)).collect_vec();
+
+        let output1 = {
+            let output = (0..args as usize).map(|_| 0u64).collect_vec();
+            for i in 0..args as usize {
+                collect_bool_slice(&mut output[i * 16..][..16], &bools[i * 1024..][..1024]);
+            }
+            output
+        };
+
+        let output2 = {
+            let output = (0..args as usize).map(|_| 0u64).collect_vec();
+            for i in 0..args as usize {
+                collect_bool_slice2(&mut output[i * 16..][..16], &bools[i * 1024..][..1024]);
+            }
+            output
+        };
+
+        assert_eq!(output1, output2);
+    }
 }
