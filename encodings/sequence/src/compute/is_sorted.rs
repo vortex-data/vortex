e6c8c068c5436287750a29ed4eb4f72b75675257
Dan King <dan@spiraldb.com>
2025-09-15 17:04:21 -0400

    feat: is_sorted for sequence array (#4653)
    Signed-off-by: Daniel King <dan@spiraldb.com>

---
+++ encodings/sequence/src/compute/is_sorted.rs
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright the Vortex contributors
+
+use num_traits::zero;
+use vortex_array::compute::{IsSortedKernel, IsSortedKernelAdapter};
+use vortex_array::register_kernel;
+use vortex_dtype::match_each_native_ptype;
+use vortex_error::VortexResult;
+
+use crate::{SequenceArray, SequenceVTable};
+
+impl IsSortedKernel for SequenceVTable {
+    fn is_sorted(&self, array: &SequenceArray) -> VortexResult<Option<bool>> {
+        let m = array.multiplier();
+        match_each_native_ptype!(m.ptype(), |P| {
+            Ok(Some(m.as_primitive::<P>() >= zero::<P>()))
+        })
+    }
+
+    fn is_strict_sorted(&self, array: &SequenceArray) -> VortexResult<Option<bool>> {
+        let m = array.multiplier();
+        match_each_native_ptype!(m.ptype(), |P| {
+            Ok(Some(m.as_primitive::<P>() > zero::<P>()))
+        })
+    }
+}
+
+register_kernel!(IsSortedKernelAdapter(SequenceVTable).lift());
