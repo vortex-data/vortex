32c3fd7a58566f94283f731c57db08594b0c6405
Alexander Droste <alexander.droste@protonmail.com>
2025-09-17 16:17:15 +0100

    move funcs
    Signed-off-by: Alexander Droste <alexander.droste@protonmail.com>

--- vortex-duckdb/cpp/include/duckdb_vx/vector.h
+++ vortex-duckdb/cpp/include/duckdb_vx/vector.h
@@ -14,14 +14,14 @@ extern "C" {
 void duckdb_vx_vector_slice_to_dictionary(duckdb_vector ffi_vector, duckdb_selection_vector selection_vector,
                                           idx_t selection_vector_length);
 
-void duckdb_vx_set_dictionary_vector_id(duckdb_vector dict, const char *id, unsigned int id_len);
-
-void duckdb_vx_set_dictionary_vector_length(duckdb_vector dict, unsigned int len);
-
 /// Turn vector into a dictionary vector. In contrast to `duckdb_vx_vector_slice_to_dictionary` this
 /// call creates a dictionary that holds a strong reference to its children.
 void duckdb_vx_vector_dictionary(duckdb_vector ffi_vector, duckdb_vector ffi_dict, idx_t dictionary_size,
                                  duckdb_selection_vector ffi_sel_vec, idx_t count);
+
+void duckdb_vx_set_dictionary_vector_id(duckdb_vector dict, const char *id, unsigned int id_len);
+
+void duckdb_vx_set_dictionary_vector_length(duckdb_vector dict, unsigned int len);
 
 // Add the buffer to the string vector (basically, keep it alive as long as the vector).
 void duckdb_vx_string_vector_add_buffer(duckdb_vector ffi_vector, duckdb_vx_data buffer);
