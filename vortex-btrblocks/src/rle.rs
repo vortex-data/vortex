6cab6f8bd3df317b13d78fec98f64970802e3aa9
Joe Isaacs <joe.isaacs@live.co.uk>
2025-10-10 11:33:42 -0400

    chore[array]: `downcast` -> `narrow`
    Signed-off-by: Joe Isaacs <joe.isaacs@live.co.uk>

--- vortex-btrblocks/src/rle.rs
+++ vortex-btrblocks/src/rle.rs
@@ -114,14 +114,14 @@ where
 
         // For indices and offsets, we always use integer compression without dictionary encoding.
         let compressed_indices = IntCompressor::compress_no_dict(
-            &rle_array.indices().to_primitive().downcast()?,
+            &rle_array.indices().to_primitive().narrow()?,
             is_sample,
             allowed_cascading - 1,
             &[],
         )?;
 
         let compressed_offsets = IntCompressor::compress_no_dict(
-            &rle_array.values_idx_offsets().to_primitive().downcast()?,
+            &rle_array.values_idx_offsets().to_primitive().narrow()?,
             is_sample,
             allowed_cascading - 1,
             &[],
