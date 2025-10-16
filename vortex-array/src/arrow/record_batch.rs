70ea6c803aa68187f4e886773547e0c3d7ac5295
Andrew Duffy <andrew@a10y.dev>
2025-10-16 12:19:38 -0400

    revert bad stuff for RecordBatch
    Signed-off-by: Andrew Duffy <andrew@a10y.dev>

--- vortex-array/src/arrow/record_batch.rs
+++ vortex-array/src/arrow/record_batch.rs
@@ -1,18 +1,12 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: Copyright the Vortex contributors
 
-use std::sync::Arc;
-
 use arrow_array::RecordBatch;
 use arrow_array::cast::AsArray;
-use vortex_error::{VortexError, VortexResult, vortex_bail};
-use vortex_mask::Mask;
+use vortex_error::{VortexError, VortexResult, vortex_bail, vortex_ensure};
 
 use crate::arrow::compute::to_arrow_preferred;
-use crate::compute::filter;
-use crate::validity::Validity;
-use crate::vtable::ValidityHelper;
-use crate::{Array, Canonical, ToCanonical};
+use crate::{Array, Canonical};
 
 impl TryFrom<&dyn Array> for RecordBatch {
     type Error = VortexError;
@@ -21,26 +15,13 @@ impl TryFrom<&dyn Array> for RecordBatch {
         let Canonical::Struct(struct_array) = value.to_canonical() else {
             vortex_bail!("RecordBatch can only be constructed from ")
         };
-        // If there are any top-level nulls, mask them from the result.
-        match struct_array.validity() {
-            // Easy
-            Validity::NonNullable | Validity::AllValid => Ok(RecordBatch::from(
-                to_arrow_preferred(struct_array.as_ref())?.as_struct(),
-            )),
-            Validity::AllInvalid => {
-                // New empty struct array
-                let schema = Arc::new(value.dtype().to_arrow_schema()?);
-                Ok(RecordBatch::new_empty(schema))
-            }
-            Validity::Array(validity) => {
-                // Create new ones here instead
-                let valid = Mask::from_buffer(validity.to_bool().boolean_buffer().clone());
-                let masked = filter(value, &valid)?;
 
-                Ok(RecordBatch::from(
-                    to_arrow_preferred(masked.as_ref())?.as_struct(),
-                ))
-            }
-        }
+        vortex_ensure!(
+            struct_array.all_valid(),
+            "RecordBatch can only be constructed from StructArray with no nulls"
+        );
+
+        let array_ref = to_arrow_preferred(struct_array.as_ref())?;
+        Ok(RecordBatch::from(array_ref.as_struct()))
     }
 }
