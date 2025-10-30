710715f80b3d30977c1804e76ea502ff87c777d4
Nicholas Gates <nick@nickgates.com>
2025-10-30 13:03:01 -0400

    Decimal Vector
    Signed-off-by: Nicholas Gates <nick@nickgates.com>

--- vortex-vector/src/decimal/macros.rs
+++ vortex-vector/src/decimal/macros.rs
@@ -12,33 +12,6 @@
 ///
 /// This macro eliminates repetitive match statements when implementing operations that need to work
 /// uniformly across all decimal type variants (`D8`, `D16`, `D32`, `D64`, `D128`, `D256`).
-///
-/// # Examples
-///
-/// ```
-/// use vortex_vector::{DecimalVector, DVectorMut, VectorOps, VectorMutOps, match_each_dvector};
-///
-/// fn get_decimal_len(vector: &DecimalVector) -> usize {
-///     match_each_dvector!(vector, |v| { v.len() })
-/// }
-///
-/// // Works with `D32` decimal vectors.
-/// let d32_vec: DecimalVector = DVectorMut::<i32>::from_iter([1, 2, 3].map(Some))
-///     .freeze()
-///     .into();
-/// assert_eq!(get_decimal_len(&d32_vec), 3);
-///
-/// // Works with `D128` decimal vectors.
-/// let d128_vec: DecimalVector = DVectorMut::<i128>::from_iter([100, 200].map(Some))
-///     .freeze()
-///     .into();
-/// assert_eq!(get_decimal_len(&d128_vec), 2);
-/// ```
-///
-/// Note: The `len` method is already provided by the [`VectorOps`] trait implementation.
-///
-/// [`DecimalVector`]: crate::DecimalVector
-/// [`VectorOps`]: crate::VectorOps
 #[macro_export]
 macro_rules! match_each_dvector {
     ($self:expr, | $vec:ident | $body:block) => {{
@@ -58,31 +31,6 @@ macro_rules! match_each_dvector {
 ///
 /// This macro eliminates repetitive match statements when implementing mutable operations that need
 /// to work uniformly across all decimal type variants (`D8`, `D16`, `D32`, `D64`, `D128`, `D256`).
-///
-/// # Examples
-///
-/// ```
-/// use vortex_vector::{DecimalVectorMut, DVectorMut, VectorMutOps, match_each_dvector_mut};
-///
-/// fn reserve_decimal_space(vector: &mut DecimalVectorMut, additional: usize) {
-///     match_each_dvector_mut!(vector, |v| { v.reserve(additional) })
-/// }
-///
-/// // Works with `D32` mutable decimal vectors.
-/// let mut d32_vec: DecimalVectorMut = DVectorMut::<i32>::from_iter([1, 2].map(Some)).into();
-/// reserve_decimal_space(&mut d32_vec, 10);
-/// assert!(d32_vec.capacity() >= 12);
-///
-/// // Works with `D128` mutable decimal vectors.
-/// let mut d128_vec: DecimalVectorMut = DVectorMut::<i128>::from_iter([100].map(Some)).into();
-/// reserve_decimal_space(&mut d128_vec, 5);
-/// assert!(d128_vec.capacity() >= 6);
-/// ```
-///
-/// Note: The `reserve` method is already provided by the [`VectorMutOps`] trait implementation.
-///
-/// [`DecimalVectorMut`]: crate::DecimalVectorMut
-/// [`VectorMutOps`]: crate::VectorMutOps
 #[macro_export]
 macro_rules! match_each_dvector_mut {
     ($self:expr, | $vec:ident | $body:block) => {{
