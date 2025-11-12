b604cf03942f8d1f42e957bade86797b8bbe5ca0
Connor Tsui <connor.tsui20@gmail.com>
2025-11-12 16:36:58 -0500

    implement filter for primitive vector
    Signed-off-by: Connor Tsui <connor.tsui20@gmail.com>

--- vortex-compute/src/filter/mask.rs
+++ vortex-compute/src/filter/mask.rs
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: Copyright the Vortex contributors
 
-use vortex_mask::Mask;
+use vortex_mask::{Mask, MaskMut};
 
 use crate::filter::Filter;
 
@@ -25,5 +25,21 @@ impl Filter for &Mask {
                 Mask::from(v1.bit_buffer().filter(selection_mask))
             }
         }
+    }
+}
+
+impl Filter for &mut MaskMut {
+    type Output = ();
+
+    fn filter(self, selection_mask: &Mask) {
+        assert_eq!(
+            selection_mask.len(),
+            self.len(),
+            "Selection mask length must equal the mask length"
+        );
+
+        // TODO(connor): There is definitely a better way to do this (in place).
+        let filtered = self.clone().freeze().filter(selection_mask).into_mut();
+        *self = filtered;
     }
 }
