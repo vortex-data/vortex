c28c4b1785c25f1ea42fb7352115ac6d81012282
Onur Satici <onur@spiraldb.com>
2025-11-12 10:46:25 +0000

    mv: dict into vortex-array
    Signed-off-by: Onur Satici <onur@spiraldb.com>

---
+++ vortex-array/src/arrays/dict/compute/min_max.rs
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright the Vortex contributors
+
+use vortex_array::compute::{MinMaxKernel, MinMaxKernelAdapter, MinMaxResult, min_max, take};
+use vortex_array::register_kernel;
+use vortex_error::VortexResult;
+
+use crate::{DictArray, DictVTable};
+
+impl MinMaxKernel for DictVTable {
+    fn min_max(&self, array: &DictArray) -> VortexResult<Option<MinMaxResult>> {
+        min_max(&take(array.values(), array.codes())?)
+    }
+}
+
+register_kernel!(MinMaxKernelAdapter(DictVTable).lift());
