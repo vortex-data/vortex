c4726c982b73da2920103aefd5c6ed2ab5bba433
Daniel King <dan@spiraldb.com>
2025-11-14 10:58:41 -0500

    fix
    Signed-off-by: Daniel King <dan@spiraldb.com>

--- vortex-python/src/expr/mod.rs
+++ vortex-python/src/expr/mod.rs
@@ -241,7 +241,7 @@ pub fn root() -> PyExpr {
 ///
 /// .. seealso::
 ///
-///    :meth:`.vortex.Expr.__getitem__`: access a field of a struct a array.
+///    Use :meth:`.vortex.Expr.__getitem__` to retrieve a field of a struct array.
 #[pyfunction]
 pub fn column<'py>(name: &Bound<'py, PyString>) -> PyResult<Bound<'py, PyExpr>> {
     let py = name.py();
