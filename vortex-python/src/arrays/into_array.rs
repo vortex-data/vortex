af5827cfb99fb3cdd7c40e8f0fa3067bf1a6cb75
Daniel King <dan@spiraldb.com>
2025-11-18 14:22:23 -0500

    spdx
    Signed-off-by: Daniel King <dan@spiraldb.com>

--- vortex-python/src/arrays/into_array.rs
+++ vortex-python/src/arrays/into_array.rs
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright the Vortex contributors
+
 use arrow_array::ffi_stream::ArrowArrayStreamReader;
 use arrow_array::{RecordBatchReader as _, make_array};
 use arrow_data::ArrayData;
