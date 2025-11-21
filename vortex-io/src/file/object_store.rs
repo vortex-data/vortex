b73edec3e355472f68e275f769441f54e740b401
Alexander Droste <alexander.droste@protonmail.com>
2025-11-21 18:36:32 +0100

    Revert "chore[ci]: add windows test to CI (#5439)"
    This reverts commit 63b759f24e13596aab5ae0f7ddf364aa3265caac.
    
    Signed-off-by: Alexander Droste <alexander.droste@protonmail.com>

--- vortex-io/src/file/object_store.rs
+++ vortex-io/src/file/object_store.rs
@@ -2,8 +2,6 @@
 // SPDX-FileCopyrightText: Copyright the Vortex contributors
 
 use std::io;
-#[cfg(not(unix))]
-use std::io::{Read, Seek};
 #[cfg(unix)]
 use std::os::unix::fs::FileExt;
 use std::sync::Arc;
@@ -127,9 +125,8 @@ impl ReadSource for ObjectStoreIoSource {
                         )
                         .await?;
 
-                    #[cfg_attr(unix, allow(unused_mut))]
                     let buffer = match response.payload {
-                        object_store::GetResultPayload::File(mut file, _) => {
+                        object_store::GetResultPayload::File(file, _) => {
                             // SAFETY: We're setting the length to the exact size we're about to read.
                             // The read_exact_at call will either fill the entire buffer or return an error,
                             // ensuring no uninitialized memory is exposed.
@@ -142,7 +139,7 @@ impl ReadSource for ObjectStoreIoSource {
                                         Ok::<_, io::Error>(buffer)
                                     }
                                     #[cfg(not(unix))] {
-                                        file.seek(io::SeekFrom::Start(range.start))?;
+                                        file.seek(range.start)?;
                                         file.read_exact(&mut buffer)?;
                                         Ok::<_, io::Error>(buffer)
                                     }
