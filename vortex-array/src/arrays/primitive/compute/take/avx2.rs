dbed322a5b1944f56fe823d22e958cb74ee0f420
Joe Isaacs <joe.isaacs@live.co.uk>
2025-11-26 10:36:21 +0000

    wip
    Signed-off-by: Joe Isaacs <joe.isaacs@live.co.uk>

--- vortex-array/src/arrays/primitive/compute/take/avx2.rs
+++ vortex-array/src/arrays/primitive/compute/take/avx2.rs
@@ -51,10 +51,6 @@ use crate::validity::Validity;
 pub(super) struct TakeKernelAVX2;
 
 impl TakeImpl for TakeKernelAVX2 {
-    #[expect(
-        clippy::cognitive_complexity,
-        reason = "complexity from nested match_each_* macros"
-    )]
     #[inline(always)]
     fn take(
         &self,
