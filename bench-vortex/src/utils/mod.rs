7e07a0730b6172a7dd15497d27cb2e4167b75761
Adam Gutglick <adam@spiraldb.com>
2025-12-08 17:06:17 +0000

    Remove unused parquet utils
    Signed-off-by: Adam Gutglick <adam@spiraldb.com>

--- bench-vortex/src/utils/mod.rs
+++ bench-vortex/src/utils/mod.rs
@@ -4,11 +4,9 @@
 pub mod constants;
 pub mod file_utils;
 pub mod logging;
-pub mod parquet_utils;
 pub mod runtime;
 
 pub use constants::*;
 pub use file_utils::*;
 pub use logging::*;
-pub use parquet_utils::*;
 pub use runtime::*;
