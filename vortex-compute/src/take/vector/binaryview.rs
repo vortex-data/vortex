f8870ae0e9be729c56179c039cd3cbba08c4f274
Connor Tsui <87130162+connortsui20@users.noreply.github.com>
2025-12-10 18:29:35 -0500

    Feature: add `take` for the view vectors (#5682)
    Tracking Issue: https://github.com/vortex-data/vortex/issues/5652
    
    I separated this out from
    https://github.com/vortex-data/vortex/pull/5653 just in case we wanted
    to discuss how to handle `take` on the view type vectors.
    
    See the module docs for more detail.
    
    After this gets merged I can implement `DictArray` `batch_execute`!
    
    Signed-off-by: Connor Tsui <connor.tsui20@gmail.com>

--- vortex-compute/src/take/vector/binaryview.rs
+++ vortex-compute/src/take/vector/binaryview.rs
@@ -1,13 +1,18 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: Copyright the Vortex contributors
 
-// use std::ops::Deref;
+//! Implementations of `take` on [`BinaryViewVector`].
+//!
+//! take` on the binary view simply performs a `take` on the views of the vector, which means the
+//! resulting vector may have "garbage" data in its child buffers.
+//!
+//! Note that it is on the outer array type to perform compaction / garbage collection.
 
-// use num_traits::AsPrimitive;
-// use vortex_buffer::Buffer;
+use num_traits::AsPrimitive;
+use vortex_buffer::Buffer;
 use vortex_dtype::UnsignedPType;
 use vortex_vector::VectorOps;
-// use vortex_vector::binaryview::BinaryView;
+use vortex_vector::binaryview::BinaryView;
 use vortex_vector::binaryview::BinaryViewType;
 use vortex_vector::binaryview::BinaryViewVector;
 use vortex_vector::primitive::PVector;
@@ -29,11 +34,7 @@ impl<T: BinaryViewType, I: UnsignedPType> Take<PVector<I>> for &BinaryViewVector
 impl<T: BinaryViewType, I: UnsignedPType> Take<[I]> for &BinaryViewVector<T> {
     type Output = BinaryViewVector<T>;
 
-    fn take(self, _indices: &[I]) -> BinaryViewVector<T> {
-        todo!("TODO(connor): Implement `take` for `BinaryViewVector` and figure out rebuilding");
-
-        /*
-
+    fn take(self, indices: &[I]) -> BinaryViewVector<T> {
         let taken_views = take_views(self.views(), indices);
         let taken_validity = self.validity().take(indices);
 
@@ -45,23 +46,19 @@ impl<T: BinaryViewType, I: UnsignedPType> Take<[I]> for &BinaryViewVector<T> {
         unsafe {
             BinaryViewVector::new_unchecked(taken_views, self.buffers().clone(), taken_validity)
         }
-
-        */
     }
 }
 
 fn take_nullable<T: BinaryViewType, I: UnsignedPType>(
-    _bvector: &BinaryViewVector<T>,
-    _indices: &PVector<I>,
+    binary_view: &BinaryViewVector<T>,
+    indices: &PVector<I>,
 ) -> BinaryViewVector<T> {
-    todo!("TODO(connor): Implement `take` for `BinaryViewVector` and figure out rebuilding");
-
-    /*
-
     // We ignore nullability when taking the views since we can let the `Mask` implementation
     // determine which elements are null.
-    let taken_views = take_views(bvector.views(), indices.elements().as_slice());
-    let taken_validity = bvector.validity().take(indices);
+    let taken_views = take_views(binary_view.views(), indices.elements().as_slice());
+
+    // Note that this is **not** the same as the `indices: &[I]` `take` implementation above.
+    let taken_validity = binary_view.validity().take(indices);
 
     debug_assert_eq!(taken_views.len(), taken_validity.len());
 
@@ -69,21 +66,14 @@ fn take_nullable<T: BinaryViewType, I: UnsignedPType>(
     // same length. The views still point into the same buffers which we clone via Arc, so all view
     // references remain valid.
     unsafe {
-        BinaryViewVector::new_unchecked(taken_views, bvector.buffers().clone(), taken_validity)
+        BinaryViewVector::new_unchecked(taken_views, binary_view.buffers().clone(), taken_validity)
     }
-
-    */
 }
-
-/*
 
 /// Takes views at the given indices.
 fn take_views<I: AsPrimitive<usize>>(
     views: &Buffer<BinaryView>,
     indices: &[I],
 ) -> Buffer<BinaryView> {
-    let views_ref = views.deref();
-    Buffer::<BinaryView>::from_trusted_len_iter(indices.iter().map(|i| views_ref[(*i).as_()]))
+    Buffer::<BinaryView>::from_trusted_len_iter(indices.iter().map(|i| views[i.as_()]))
 }
-
-*/
