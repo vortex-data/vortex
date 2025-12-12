80874f3a55ef3de5294cf7de3a45ab710bce00a2
Nicholas Gates <nick@nickgates.com>
2025-12-12 22:05:35 +0000

    Add more arrow execution
    Signed-off-by: Nicholas Gates <nick@nickgates.com>

--- vortex-array/src/arrow/executor/mod.rs
+++ vortex-array/src/arrow/executor/mod.rs
@@ -1,40 +1,30 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: Copyright the Vortex contributors
 
-use std::sync::Arc;
+mod bool;
+mod dictionary;
+mod null;
+mod primitive;
+mod struct_;
+mod temporal;
 
 use arrow_array::ArrayRef as ArrowArrayRef;
-use arrow_array::ArrowPrimitiveType;
-use arrow_array::BooleanArray;
-use arrow_array::NullArray;
-use arrow_array::PrimitiveArray;
 use arrow_array::RecordBatch;
-use arrow_array::StructArray;
 use arrow_array::cast::AsArray;
 use arrow_array::types::*;
 use arrow_schema::DataType;
-use arrow_schema::Field;
-use arrow_schema::Fields;
 use arrow_schema::Schema;
-use vortex_compute::arrow::IntoArrow;
-use vortex_dtype::DType;
-use vortex_dtype::NativePType;
-use vortex_dtype::Nullability;
-use vortex_dtype::PTypeDowncastExt;
 use vortex_error::VortexError;
 use vortex_error::VortexResult;
-use vortex_error::vortex_ensure;
-use vortex_error::vortex_err;
 use vortex_session::VortexSession;
-use vortex_vector::VectorOps;
 
-use crate::Array;
 use crate::ArrayRef;
-use crate::VectorExecutor;
-use crate::arrays::ListVTable;
-use crate::arrays::VarBinVTable;
-use crate::arrow::null_buffer::to_null_buffer;
-use crate::builtins::ArrayBuiltins;
+use crate::arrow::executor::bool::to_arrow_bool;
+use crate::arrow::executor::dictionary::to_arrow_dictionary;
+use crate::arrow::executor::null::to_arrow_null;
+use crate::arrow::executor::primitive::to_arrow_primitive;
+use crate::arrow::executor::struct_::to_arrow_struct;
+use crate::arrow::executor::temporal::to_arrow_temporal;
 
 /// Trait for executing a Vortex array to produce an Arrow array.
 pub trait ArrowArrayExecutor {
@@ -43,30 +33,17 @@ pub trait ArrowArrayExecutor {
     /// If a [`DataType`] is given, the array will be converted to the desired Arrow type.
     fn execute_arrow(
         &self,
-        // TODO(ngates): do we even want optional data type? Or do we make it required and tell
-        //  users to call `DType::into_arrow` to get a default logical type? I'm inclined to think
-        //  the latter is preferable. Although there's a world where the user may want the minimal
-        //  conversion to Arrow without knowing what that conversion is. In which case, should
-        //  DictionaryArray and non-logical types be supported? Should the user provide a list of
-        //  supported Arrow arrays? I dunno...
-        field: Option<&Field>,
+        data_type: &DataType,
         session: &VortexSession,
     ) -> VortexResult<ArrowArrayRef>;
 
+    /// Execute the array to produce an Arrow `RecordBatch` with the given schema.
     fn execute_record_batch(
         &self,
         schema: &Schema,
         session: &VortexSession,
     ) -> VortexResult<RecordBatch> {
-        let array = self.execute_arrow(
-            // TODO(ngates): don't really want to create a new field each time?
-            Some(&Field::new(
-                "",
-                DataType::Struct(schema.fields.clone()),
-                false,
-            )),
-            session,
-        )?;
+        let array = self.execute_arrow(&DataType::Struct(schema.fields.clone()), session)?;
         RecordBatch::try_from(array.as_struct()).map_err(VortexError::from)
     }
 }
@@ -74,210 +51,95 @@ pub trait ArrowArrayExecutor {
 impl ArrowArrayExecutor for ArrayRef {
     fn execute_arrow(
         &self,
-        field: Option<&Field>,
+        data_type: &DataType,
         session: &VortexSession,
     ) -> VortexResult<ArrowArrayRef> {
-        match field {
-            None => {
-                // Special-case the Arrow-shaped encodings that are not part of our vector API
-                // to avoid unnecessary conversions.
-                if let Some(_varbin) = self.as_opt::<VarBinVTable>() {
-                    // Convert directly to preferred Arrow VarBin array.
-                }
-                if let Some(_list) = self.as_opt::<ListVTable>() {
-                    // Convert directly to preferred Arrow List array.
-                }
-
-                let vector = self.execute_vector(session)?;
-                vector.into_arrow()
+        match data_type {
+            DataType::Null => to_arrow_null(self, session),
+            DataType::Boolean => to_arrow_bool(self, session),
+            DataType::Int8 => to_arrow_primitive::<UInt8Type>(self, session),
+            DataType::Int16 => to_arrow_primitive::<UInt16Type>(self, session),
+            DataType::Int32 => to_arrow_primitive::<UInt32Type>(self, session),
+            DataType::Int64 => to_arrow_primitive::<UInt64Type>(self, session),
+            DataType::UInt8 => to_arrow_primitive::<Int8Type>(self, session),
+            DataType::UInt16 => to_arrow_primitive::<Int16Type>(self, session),
+            DataType::UInt32 => to_arrow_primitive::<Int32Type>(self, session),
+            DataType::UInt64 => to_arrow_primitive::<Int64Type>(self, session),
+            DataType::Float16 => to_arrow_primitive::<Float16Type>(self, session),
+            DataType::Float32 => to_arrow_primitive::<Float32Type>(self, session),
+            DataType::Float64 => to_arrow_primitive::<Float64Type>(self, session),
+            DataType::Timestamp(..)
+            | DataType::Date32
+            | DataType::Date64
+            | DataType::Time32(_)
+            | DataType::Time64(_) => to_arrow_temporal(self, data_type, session),
+            DataType::Duration(_) => {
+                todo!()
             }
-
-            // Once we know the target Arrow DataType, how do we get there? Should we allow crates
-            // to register Arrow conversion kernels? Should we wrap up the Vortex array in a
-            // cast expression and re-run the optimizer? Should we just execute to a vector and
-            // then convert?
-            Some(field) => {
-                let nullability: Nullability = field.is_nullable().into();
-                match field.data_type() {
-                    DataType::Null => to_arrow_null(self, session),
-                    DataType::Boolean => to_arrow_bool(self, nullability, session),
-                    DataType::Int8 => to_arrow_primitive::<UInt8Type>(self, nullability, session),
-                    DataType::Int16 => to_arrow_primitive::<UInt16Type>(self, nullability, session),
-                    DataType::Int32 => to_arrow_primitive::<UInt32Type>(self, nullability, session),
-                    DataType::Int64 => to_arrow_primitive::<UInt64Type>(self, nullability, session),
-                    DataType::UInt8 => to_arrow_primitive::<Int8Type>(self, nullability, session),
-                    DataType::UInt16 => to_arrow_primitive::<Int16Type>(self, nullability, session),
-                    DataType::UInt32 => to_arrow_primitive::<Int32Type>(self, nullability, session),
-                    DataType::UInt64 => to_arrow_primitive::<Int64Type>(self, nullability, session),
-                    DataType::Float16 => {
-                        to_arrow_primitive::<Float16Type>(self, nullability, session)
-                    }
-                    DataType::Float32 => {
-                        to_arrow_primitive::<Float32Type>(self, nullability, session)
-                    }
-                    DataType::Float64 => {
-                        to_arrow_primitive::<Float64Type>(self, nullability, session)
-                    }
-                    DataType::Timestamp(..) => {
-                        todo!()
-                    }
-                    DataType::Date32 => {
-                        todo!()
-                    }
-                    DataType::Date64 => {
-                        todo!()
-                    }
-                    DataType::Time32(_) => {
-                        todo!()
-                    }
-                    DataType::Time64(_) => {
-                        todo!()
-                    }
-                    DataType::Duration(_) => {
-                        todo!()
-                    }
-                    DataType::Interval(_) => {
-                        todo!()
-                    }
-                    DataType::Binary => {
-                        todo!()
-                    }
-                    DataType::FixedSizeBinary(_) => {
-                        todo!()
-                    }
-                    DataType::LargeBinary => {
-                        todo!()
-                    }
-                    DataType::BinaryView => {
-                        todo!()
-                    }
-                    DataType::Utf8 => {
-                        todo!()
-                    }
-                    DataType::LargeUtf8 => {
-                        todo!()
-                    }
-                    DataType::Utf8View => {
-                        todo!()
-                    }
-                    DataType::List(_) => {
-                        todo!()
-                    }
-                    DataType::ListView(_) => {
-                        todo!()
-                    }
-                    DataType::FixedSizeList(..) => {
-                        todo!()
-                    }
-                    DataType::LargeList(_) => {
-                        todo!()
-                    }
-                    DataType::LargeListView(_) => {
-                        todo!()
-                    }
-                    DataType::Struct(fields) => to_arrow_struct(self, fields, nullability, session),
-                    DataType::Union(..) => {
-                        todo!()
-                    }
-                    DataType::Dictionary(..) => {
-                        todo!()
-                    }
-                    DataType::Decimal32(..) => {
-                        todo!()
-                    }
-                    DataType::Decimal64(..) => {
-                        todo!()
-                    }
-                    DataType::Decimal128(..) => {
-                        todo!()
-                    }
-                    DataType::Decimal256(..) => {
-                        todo!()
-                    }
-                    DataType::Map(..) => {
-                        todo!()
-                    }
-                    DataType::RunEndEncoded(..) => {
-                        todo!()
-                    }
-                }
+            DataType::Interval(_) => {
+                todo!()
+            }
+            DataType::Binary => {
+                todo!()
+            }
+            DataType::FixedSizeBinary(_) => {
+                todo!()
+            }
+            DataType::LargeBinary => {
+                todo!()
+            }
+            DataType::BinaryView => {
+                todo!()
+            }
+            DataType::Utf8 => {
+                todo!()
+            }
+            DataType::LargeUtf8 => {
+                todo!()
+            }
+            DataType::Utf8View => {
+                todo!()
+            }
+            DataType::List(_) => {
+                todo!()
+            }
+            DataType::ListView(_) => {
+                todo!()
+            }
+            DataType::FixedSizeList(..) => {
+                todo!()
+            }
+            DataType::LargeList(_) => {
+                todo!()
+            }
+            DataType::LargeListView(_) => {
+                todo!()
+            }
+            DataType::Struct(fields) => to_arrow_struct(self, fields, session),
+            DataType::Union(..) => {
+                todo!()
+            }
+            DataType::Dictionary(codes_type, values_type) => {
+                to_arrow_dictionary(self, codes_type, values_type, session)
+            }
+            DataType::Decimal32(..) => {
+                todo!()
+            }
+            DataType::Decimal64(..) => {
+                todo!()
+            }
+            DataType::Decimal128(..) => {
+                todo!()
+            }
+            DataType::Decimal256(..) => {
+                todo!()
+            }
+            DataType::Map(..) => {
+                todo!()
+            }
+            DataType::RunEndEncoded(..) => {
+                todo!()
             }
         }
     }
-}
-
-fn to_arrow_null(array: &ArrayRef, session: &VortexSession) -> VortexResult<ArrowArrayRef> {
-    let null_vector = array
-        .execute_vector(session)?
-        .into_null_opt()
-        .ok_or_else(|| vortex_err!("Failed to convert array to Null vector"))?;
-    Ok(Arc::new(NullArray::new(null_vector.len())))
-}
-
-fn to_arrow_bool(
-    array: &ArrayRef,
-    nullability: Nullability,
-    session: &VortexSession,
-) -> VortexResult<ArrowArrayRef> {
-    let bool_vector = array
-        .execute_vector(session)?
-        .into_bool_opt()
-        .ok_or_else(|| vortex_err!("Failed to convert array to Bool vector"))?;
-    let (bits, validity) = bool_vector.into_parts();
-
-    vortex_ensure!(
-        nullability.is_nullable() || validity.all_true(),
-        "Cannot convert to non-nullable Boolean array with nulls present"
-    );
-
-    Ok(Arc::new(BooleanArray::new(
-        bits.into(),
-        to_null_buffer(validity),
-    )))
-}
-
-fn to_arrow_primitive<T: ArrowPrimitiveType>(
-    array: &ArrayRef,
-    nullability: Nullability,
-    session: &VortexSession,
-) -> VortexResult<ArrowArrayRef>
-where
-    T::Native: NativePType,
-{
-    let array = array.cast(DType::Primitive(T::Native::PTYPE, nullability))?;
-    let vector = array.execute_vector(session)?.into_primitive();
-    let (buffer, validity) = vector.downcast::<T::Native>().into_parts();
-    let null_buffer = to_null_buffer(validity);
-    let buffer = buffer.into_arrow_scalar_buffer();
-    Ok(Arc::new(PrimitiveArray::<T>::new(buffer, null_buffer)))
-}
-
-fn to_arrow_struct(
-    array: &ArrayRef,
-    fields: &Fields,
-    nullability: Nullability,
-    session: &VortexSession,
-) -> VortexResult<ArrowArrayRef> {
-    let validity = array.validity_mask();
-    vortex_ensure!(
-        nullability.is_nullable() || validity.all_true(),
-        "Cannot convert to non-nullable Struct array with nulls present"
-    );
-
-    let mut arrow_fields = Vec::with_capacity(fields.len());
-    for field in fields.iter() {
-        arrow_fields.push(
-            array
-                .get_item(field.name().as_str())?
-                .execute_arrow(Some(field), session)?,
-        );
-    }
-
-    Ok(Arc::new(unsafe {
-        StructArray::new_unchecked(
-            fields.clone(),
-            arrow_fields.into(),
-            to_null_buffer(validity),
-        )
-    }))
 }
---
+++ vortex-array/src/arrow/executor/struct_.rs
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright the Vortex contributors
+
+use std::sync::Arc;
+
+use arrow_array::ArrayRef as ArrowArrayRef;
+use arrow_array::StructArray;
+use arrow_schema::Fields;
+use itertools::Itertools;
+use vortex_error::VortexResult;
+use vortex_error::vortex_ensure;
+use vortex_session::VortexSession;
+
+use crate::ArrayRef;
+use crate::arrays::StructVTable;
+use crate::arrow::ArrowArrayExecutor;
+use crate::arrow::null_buffer::to_null_buffer;
+use crate::builtins::ArrayBuiltins;
+
+pub(super) fn to_arrow_struct(
+    array: &ArrayRef,
+    fields: &Fields,
+    session: &VortexSession,
+) -> VortexResult<ArrowArrayRef> {
+    let validity = array.validity_mask();
+
+    let mut field_arrays = Vec::with_capacity(fields.len());
+    if let Some(array) = array.as_opt::<StructVTable>() {
+        // If the array is already a struct type, then we can convert each field.
+        for (field, child) in fields.iter().zip_eq(array.fields().iter()) {
+            let field_array = child.execute_arrow(field.data_type(), session)?;
+            vortex_ensure!(
+                field.is_nullable() || field_array.null_count() == 0,
+                "Cannot convert field '{}' to non-nullable Arrow field because it contains nulls",
+                field.name()
+            );
+            field_arrays.push(field_array);
+        }
+    } else {
+        // Otherwise, we have some options:
+        //  1. Use get_item expression to extract each field? This is a bit sad because get_item
+        //     will perform the validity masking again.
+        //  2. Execute a full struct vector. But this may do unnecessary work on fields that may
+        //    have a more direct conversion to the desired Arrow field type.
+        //  3. Something else?
+        //
+        // For now, we go with option 1. Although we really ought to figure out CSE for this.
+        for field in fields.iter() {
+            let field_array = array
+                .get_item(field.name().as_str())?
+                .execute_arrow(field.data_type(), session)?;
+            vortex_ensure!(
+                field.is_nullable() || field_array.null_count() == 0,
+                "Cannot convert field '{}' to non-nullable Arrow field because it contains nulls",
+                field.name()
+            );
+            field_arrays.push(field_array);
+        }
+    }
+
+    Ok(Arc::new(unsafe {
+        StructArray::new_unchecked(
+            fields.clone(),
+            field_arrays.into(),
+            to_null_buffer(validity),
+        )
+    }))
+}
