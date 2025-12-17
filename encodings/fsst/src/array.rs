3cfa49aad621e99dd1cff55d024349fe1089e01b
Connor Tsui <connor.tsui20@gmail.com>
2025-12-17 13:06:00 -0500

    whoops
    Signed-off-by: Connor Tsui <connor.tsui20@gmail.com>

--- encodings/fsst/src/array.rs
+++ encodings/fsst/src/array.rs
@@ -329,8 +329,6 @@ impl FSSTArray {
 
     /// Build a [`Decompressor`][fsst::Decompressor] that can be used to decompress values from
     /// this array.
-    ///
-    /// This is private to the crate to avoid leaking `fsst-rs` types as part of the public API.
     pub fn decompressor(&self) -> Decompressor<'_> {
         Decompressor::new(self.symbols().as_slice(), self.symbol_lengths().as_slice())
     }
