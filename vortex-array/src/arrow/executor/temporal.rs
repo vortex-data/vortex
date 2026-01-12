6c6f5cd2e92e67a6c3cda97b2217cd7bab9d0ebf
Joe Isaacs <joe.isaacs@live.co.uk>
2026-01-12 14:40:06 +0000

    fix[array]: execute (use operator)
    Signed-off-by: Joe Isaacs <joe.isaacs@live.co.uk>

--- vortex-array/src/arrow/executor/temporal.rs
+++ vortex-array/src/arrow/executor/temporal.rs
@@ -126,6 +126,8 @@ fn to_arrow_temporal_primitive<T: ArrowTemporalType>(
 where
     T::Native: NativePType,
 {
+    debug_assert!(TemporalMetadata::try_from(array.dtype()).is_ok());
+
     let ext_array = array.execute(ctx)?.into_extension();
     let primitive = ext_array.storage().execute(ctx)?.into_primitive();
     vortex_ensure!(
