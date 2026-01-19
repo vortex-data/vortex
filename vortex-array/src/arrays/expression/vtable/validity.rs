3b436e2d0c79e8e95ec69a7d62b17a54576a5fd7
Nicholas Gates <nick@nickgates.com>
2026-01-19 21:20:13 +0000

    Fix is not null
    Signed-off-by: Nicholas Gates <nick@nickgates.com>

--- vortex-array/src/arrays/expression/vtable/validity.rs
+++ vortex-array/src/arrays/expression/vtable/validity.rs
@@ -1,25 +1,49 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: Copyright the Vortex contributors
 
+use vortex_dtype::DType;
+use vortex_dtype::Nullability;
+use vortex_error::VortexExpect;
 use vortex_error::VortexResult;
 
+use crate::Array;
+use crate::IntoArray;
 use crate::arrays::expression::ExpressionArray;
 use crate::arrays::expression::ExpressionVTable;
+use crate::expr::Literal;
 use crate::validity::Validity;
 use crate::vtable::ValidityVTable;
 
 impl ValidityVTable<ExpressionVTable> for ExpressionVTable {
-    fn is_valid(array: &ExpressionArray, index: usize) -> bool {}
-
-    fn all_valid(array: &ExpressionArray) -> bool {
-        todo!()
-    }
-
-    fn all_invalid(array: &ExpressionArray) -> bool {
-        todo!()
-    }
-
     fn validity(array: &ExpressionArray) -> VortexResult<Validity> {
-        todo!()
+        let validity_expression = array
+            .expression
+            .validity()?
+            .optimize_recursive(array.input.dtype())?;
+
+        Ok(match validity_expression.as_opt::<Literal>() {
+            None => Validity::Array(
+                ExpressionArray {
+                    expression: validity_expression,
+                    dtype: DType::Bool(Nullability::NonNullable),
+                    input: array.input.clone(),
+                    stats: Default::default(),
+                }
+                .into_array(),
+            ),
+            Some(value) => {
+                let is_valid = value
+                    .as_bool()
+                    .value()
+                    .vortex_expect("validity is non-nullable");
+                if is_valid {
+                    // NOTE(ngates): we know it's not Validity::NonNullable else this vtable
+                    //  function would never have been called.
+                    Validity::AllValid
+                } else {
+                    Validity::AllInvalid
+                }
+            }
+        })
     }
 }
