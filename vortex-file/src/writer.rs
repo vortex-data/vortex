ff8ed97672aff9f2f318ef41e73dc81041aae75c
Nicholas Gates <nick@nickgates.com>
2026-01-20 12:55:45 +0000

    merge
    Signed-off-by: Nicholas Gates <nick@nickgates.com>

--- vortex-file/src/writer.rs
+++ vortex-file/src/writer.rs
@@ -145,11 +145,9 @@ impl VortexWriteOptions {
         // serialised array order is deterministic. The serialisation of arrays are done
         // parallel and with an empty context they can register their encodings to the context
         // in different order, changing the written bytes from run to run.
-        let ctx = Arc::new(
-            ArrayContext::new(self.session.arrays().registry().ids().sorted().collect())
-                // Configure a registry just to ensure only known encodings are interned.
-                .with_registry(self.session.arrays().registry().clone()),
-        );
+        let ctx = ArrayContext::new(self.session.arrays().registry().ids().sorted().collect())
+            // Configure a registry just to ensure only known encodings are interned.
+            .with_registry(self.session.arrays().registry().clone());
         let dtype = stream.dtype().clone();
 
         let (mut ptr, eof) = SequenceId::root().split();
