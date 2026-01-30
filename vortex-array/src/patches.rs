e930438e9f6a6dc84b0b29a2434ae4c34df65742
Andrew Duffy <andrew@a10y.dev>
2026-01-30 12:43:21 -0500

    add patches validation when on CPU
    Signed-off-by: Andrew Duffy <andrew@a10y.dev>

--- vortex-array/src/patches.rs
+++ vortex-array/src/patches.rs
@@ -36,11 +36,13 @@ use vortex_vector::primitive::PrimitiveVectorMut;
 
 use crate::Array;
 use crate::ArrayRef;
+use crate::ArrayVisitor;
 use crate::IntoArray;
 use crate::ToCanonical;
 use crate::arrays::PrimitiveArray;
 use crate::compute::cast;
 use crate::compute::filter;
+use crate::compute::is_sorted;
 use crate::compute::take;
 use crate::search_sorted::SearchResult;
 use crate::search_sorted::SearchSorted;
@@ -161,34 +163,38 @@ impl Patches {
         values: ArrayRef,
         chunk_offsets: Option<ArrayRef>,
     ) -> VortexResult<Self> {
-        // vortex_ensure!(
-        //     indices.len() == values.len(),
-        //     "Patch indices and values must have the same length"
-        // );
-        // vortex_ensure!(
-        //     indices.dtype().is_unsigned_int() && !indices.dtype().is_nullable(),
-        //     "Patch indices must be non-nullable unsigned integers, got {:?}",
-        //     indices.dtype()
-        // );
-        // vortex_ensure!(
-        //     indices.len() <= array_len,
-        //     "Patch indices must be shorter than the array length"
-        // );
-        // vortex_ensure!(!indices.is_empty(), "Patch indices must not be empty");
-        //
-        // let max = usize::try_from(&indices.scalar_at(indices.len() - 1)?)
-        //     .map_err(|_| vortex_err!("indices must be a number"))?;
-        // vortex_ensure!(
-        //     max - offset < array_len,
-        //     "Patch indices {max:?}, offset {offset} are longer than the array length {array_len}"
-        // );
-        //
-        // debug_assert!(
-        //     is_sorted(indices.as_ref())
-        //         .unwrap_or(Some(false))
-        //         .unwrap_or(false),
-        //     "Patch indices must be sorted"
-        // );
+        // Perform validation of components when they are host-resident.
+        // This is not possible to do eagerly when the data is on GPU memory.
+        if indices.is_host() && values.is_host() {
+            vortex_ensure!(
+                indices.len() == values.len(),
+                "Patch indices and values must have the same length"
+            );
+            vortex_ensure!(
+                indices.dtype().is_unsigned_int() && !indices.dtype().is_nullable(),
+                "Patch indices must be non-nullable unsigned integers, got {:?}",
+                indices.dtype()
+            );
+            vortex_ensure!(
+                indices.len() <= array_len,
+                "Patch indices must be shorter than the array length"
+            );
+            vortex_ensure!(!indices.is_empty(), "Patch indices must not be empty");
+
+            let max = usize::try_from(&indices.scalar_at(indices.len() - 1)?)
+                .map_err(|_| vortex_err!("indices must be a number"))?;
+            vortex_ensure!(
+                max - offset < array_len,
+                "Patch indices {max:?}, offset {offset} are longer than the array length {array_len}"
+            );
+
+            debug_assert!(
+                is_sorted(indices.as_ref())
+                    .unwrap_or(Some(false))
+                    .unwrap_or(false),
+                "Patch indices must be sorted"
+            );
+        }
 
         Ok(Self {
             array_len,
