a06184dca54bfc1807abf0e30611cf0ded3c67c9
Andrew Duffy <a10y@users.noreply.github.com>
2026-02-03 18:31:31 -0500

    add struct array for C export (#6294)
    Signed-off-by: Andrew Duffy <andrew@a10y.dev>

--- vortex-cuda/cudf-test/src/lib.rs
+++ vortex-cuda/cudf-test/src/lib.rs
@@ -3,16 +3,19 @@
 
 //! This file is a simple C-compatible API that is called from the cudf-test-harness at CI time.
 
-#![allow(clippy::unwrap_used)]
+#![allow(clippy::unwrap_used, clippy::expect_used)]
 
 use std::sync::LazyLock;
 
-use arrow_schema::DataType;
 use arrow_schema::ffi::FFI_ArrowSchema;
 use futures::executor::block_on;
+use vortex::array::Array;
 use vortex::array::IntoArray;
 use vortex::array::arrays::PrimitiveArray;
+use vortex::array::arrays::StructArray;
 use vortex::array::session::ArraySession;
+use vortex::array::validity::Validity;
+use vortex::dtype::FieldNames;
 use vortex::expr::session::ExprSession;
 use vortex::io::session::RuntimeSession;
 use vortex::layout::session::LayoutSession;
@@ -42,9 +45,22 @@ pub extern "C" fn export_array(
 
     let primitive = PrimitiveArray::from_iter(0u32..1024);
 
-    *schema_ptr = FFI_ArrowSchema::try_from(DataType::UInt32).unwrap();
+    let array = StructArray::new(
+        FieldNames::from_iter(["a"]),
+        vec![primitive.into_array()],
+        1024,
+        Validity::NonNullable,
+    )
+    .into_array();
 
-    match block_on(primitive.into_array().export_device_array(&mut ctx)) {
+    let data_type = array
+        .dtype()
+        .to_arrow_dtype()
+        .expect("converting schema to Arrow DataType");
+
+    *schema_ptr = FFI_ArrowSchema::try_from(data_type).expect("data_type to FFI_ArrowSchema");
+
+    match block_on(array.export_device_array(&mut ctx)) {
         Ok(exported) => {
             *array_ptr = exported;
             0
