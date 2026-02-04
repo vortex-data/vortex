4a5ffcc39a28e3722a8060bbd088e5374e201cc9
Adam Gutglick <adam@spiraldb.com>
2026-02-04 13:01:18 +0000

    Try and fix build dir
    Signed-off-by: Adam Gutglick <adam@spiraldb.com>

--- benchmarks/duckdb-bench/build.rs
+++ benchmarks/duckdb-bench/build.rs
@@ -19,6 +19,7 @@
 /// The dynamic DuckDB library is preferred over the static version, as DuckDB's
 /// static lib is not self-contained. This means that it includes symbols which
 /// are not defined as part of the static library.
+
 fn main() {
     // Propagate DuckDB rpath from vortex-duckdb
     let duckdb_lib = std::env::var("DEP_DUCKDB_LIB_DIR").unwrap();
@@ -28,11 +29,16 @@ fn main() {
         println!("cargo:rustc-link-arg=-Wl,-rpath,{duckdb_lib}");
     }
 
-    // Include the custom-labels build instruction for non-macos builds
     #[cfg(not(target_os = "macos"))]
     {
+        // Inline the dynamically exported symbol list for non-macos targets.
+        const DLIST: &str = "{\
+            custom_labels_abi_version;\
+            custom_labels_current_set;\
+            };";
+
         let dlist_path = format!("{}/dlist", std::env::var("OUT_DIR").unwrap());
-        std::fs::write(&dlist_path, include_str!("../dlist")).unwrap();
+        std::fs::write(&dlist_path, DLIST).unwrap();
         println!("cargo:rustc-link-arg=-Wl,-rpath,{duckdb_lib},--dynamic-list={dlist_path}")
     }
 }
