3aaea5e3f737ee723a8a582cb50de60bfa3207cd
Onur Satici <onur@spiraldb.com>
2026-02-04 16:17:44 +0000

    shared array in dict layout
    Signed-off-by: Onur Satici <onur@spiraldb.com>

--- vortex-layout/src/layouts/dict/reader.rs
+++ vortex-layout/src/layouts/dict/reader.rs
@@ -13,12 +13,11 @@ use futures::future::BoxFuture;
 use futures::try_join;
 use vortex_array::Array;
 use vortex_array::ArrayRef;
-use vortex_array::Canonical;
-use vortex_array::ExecutionCtx;
 use vortex_array::IntoArray;
 use vortex_array::MaskFuture;
 use vortex_array::VortexSessionExecute;
 use vortex_array::arrays::DictArray;
+use vortex_array::arrays::SharedArray;
 use vortex_array::expr::Expression;
 use vortex_array::expr::root;
 use vortex_array::optimizer::ArrayOptimizer;
@@ -88,7 +87,6 @@ impl DictReader {
         // We capture the name, so it may be wrong if we re-use the same reader within multiple
         // different parent readers. But that's rare...
         let values_len = self.values_len;
-        let session = self.session.clone();
         self.values_array
             .get_or_init(move || {
                 self.values
@@ -100,10 +98,8 @@ impl DictReader {
                     .vortex_expect("must construct dict values array evaluation")
                     .map_err(Arc::new)
                     .map(move |array| {
-                        // We execute the array to avoid re-evaluating for every split.
                         let array = array?;
-                        let mut ctx = ExecutionCtx::new(session);
-                        Ok(array.execute::<Canonical>(&mut ctx)?.into_array())
+                        Ok(SharedArray::new(array).into_array())
                     })
                     .boxed()
                     .shared()
@@ -122,16 +118,13 @@ impl DictReader {
             return fut.clone();
         }
 
-        let session = self.session.clone();
         self.values_evals
             .entry(expr.clone())
             .or_insert_with(|| {
                 self.values_array()
                     .map(move |array| {
                         let array = array?.apply(&expr)?;
-                        // We execute the array to avoid re-evaluating for every split.
-                        let mut ctx = ExecutionCtx::new(session);
-                        Ok(array.execute::<Canonical>(&mut ctx)?.into_array())
+                        Ok(SharedArray::new(array).into_array())
                     })
                     .boxed()
                     .shared()
