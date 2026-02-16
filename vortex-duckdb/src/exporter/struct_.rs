b8b68b898c6dd2257045612a1ed27208493694f1
Joe Isaacs <joe.isaacs@live.co.uk>
2026-02-16 17:53:34 +0000

    fix
    Signed-off-by: Joe Isaacs <joe.isaacs@live.co.uk>

--- vortex-duckdb/src/exporter/struct_.rs
+++ vortex-duckdb/src/exporter/struct_.rs
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: Copyright the Vortex contributors
 
-use std::ops::Not;
-
 use vortex::array::ExecutionCtx;
 use vortex::array::IntoArray;
 use vortex::array::arrays::BoolArray;
