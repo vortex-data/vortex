ba7e32918d1daa3d99dab05cf79fdecfee281ef5
Joe Isaacs <joe.isaacs@live.co.uk>
2026-02-20 13:45:51 +0000

    chore[btrblocks]: more precise `FoR` `expected_compression_ratio` (#6301)
    Signed-off-by: Joe Isaacs <joe.isaacs@live.co.uk>

--- vortex-python/src/io.rs
+++ vortex-python/src/io.rs
@@ -277,7 +277,7 @@ impl PyVortexWriteOptions {
     /// >>> vx.io.VortexWriteOptions.default().write(sprl, "chonky.vortex")
     /// >>> import os
     /// >>> os.path.getsize('chonky.vortex')
-    /// 216156
+    /// 216020
     /// ```
     ///
     /// Wow, Vortex manages to use about two bytes per integer! So advanced. So tiny.
@@ -289,7 +289,7 @@ impl PyVortexWriteOptions {
     /// ```python
     /// >>> vx.io.VortexWriteOptions.compact().write(sprl, "tiny.vortex")
     /// >>> os.path.getsize('tiny.vortex')
-    /// 55316
+    /// 55088
     /// ```
     ///
     /// Random numbers are not (usually) composed of random bytes!
