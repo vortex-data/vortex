66f1bbe4dd399649b6f38da6668ce4e63a49a8b4
Andrew Duffy <andrew@a10y.dev>
2026-03-18 17:36:19 -0400

    actually make the kernel get used
    Signed-off-by: Andrew Duffy <andrew@a10y.dev>

--- vortex-array/src/arrays/patched/vtable/kernels.rs
+++ vortex-array/src/arrays/patched/vtable/kernels.rs
@@ -2,8 +2,11 @@
 // SPDX-FileCopyrightText: Copyright the Vortex contributors
 
 use crate::arrays::Patched;
+use crate::arrays::dict::TakeExecuteAdaptor;
 use crate::kernel::ParentKernelSet;
 use crate::scalar_fn::fns::binary::CompareExecuteAdaptor;
 
-pub(super) const PARENT_KERNELS: ParentKernelSet<Patched> =
-    ParentKernelSet::new(&[ParentKernelSet::lift(&CompareExecuteAdaptor(Patched))]);
+pub(super) const PARENT_KERNELS: ParentKernelSet<Patched> = ParentKernelSet::new(&[
+    ParentKernelSet::lift(&CompareExecuteAdaptor(Patched)),
+    ParentKernelSet::lift(&TakeExecuteAdaptor(Patched)),
+]);
