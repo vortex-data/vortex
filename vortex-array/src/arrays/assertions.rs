db898d3005a8676e970a2a7cfe29b8b5f5966755
Robert Kruszewski <github@robertk.io>
2026-03-18 21:37:08 -0700

    format
    Signed-off-by: Robert Kruszewski <github@robertk.io>

--- vortex-array/src/arrays/assertions.rs
+++ vortex-array/src/arrays/assertions.rs
@@ -49,36 +49,33 @@ macro_rules! assert_nth_scalar_is_null {
 #[macro_export]
 macro_rules! assert_arrays_eq {
     ($left:expr, $right:expr) => {{
-       let left = $left.clone();
-       let right = $right.clone();
-       if left.dtype() != right.dtype() {
+        let left = $left.clone();
+        let right = $right.clone();
+        if left.dtype() != right.dtype() {
             panic!(
                 "assertion left == right failed: arrays differ in type: {} != {}.\n  left: {}\n right: {}",
                 left.dtype(),
                 right.dtype(),
                 left.display_values(),
                 right.display_values()
-           )
-       }
+            )
+        }
 
-       if left.len() != right.len() {
-           panic!(
-               "assertion left == right failed: arrays differ in length: {} != {}.\n  left: {}\n right: {}",
-               left.len(),
-               right.len(),
-               left.display_values(),
-               right.display_values()
-           )
-       }
+        if left.len() != right.len() {
+            panic!(
+                "assertion left == right failed: arrays differ in length: {} != {}.\n  left: {}\n right: {}",
+                left.len(),
+                right.len(),
+                left.display_values(),
+                right.display_values()
+            )
+        }
 
-       let n = left.len();
-       let mismatched_indices = (0..n)
-       .filter(|i| left.scalar_at(*i).unwrap() != right.scalar_at(*i).unwrap())
+        let n = left.len();
+        let mismatched_indices = (0..n)
+            .filter(|i| left.scalar_at(*i).unwrap() != right.scalar_at(*i).unwrap())
             .collect::<Vec<_>>();
-       if mismatched_indices.len() != 0 {
-        eprintln!("mismatched values: {:?}", mismatched_indices.iter()
-            .map(|i| (left.scalar_at(*i).unwrap(), right.scalar_at(*i).unwrap()))
-            .collect::<Vec<_>>());
+        if mismatched_indices.len() != 0 {
             panic!(
                 "assertion left == right failed: arrays do not match at indices: {}.\n  left: {}\n right: {}",
                 $crate::arrays::format_indices(mismatched_indices),
