683ba3a1e660f24b4eb6d16bf8bcc5c4cc1f9fb3
Adam Gutglick <adam@spiraldb.com>
2026-03-19 13:38:45 +0000

    Canonical variant array (#7026)
    This PR introduces a minimal new canonical array for the new variant
    DType, its not very useable at the moment but I figured its a piece of
    mostly boilerplate code that is worth splitting out of the bigger
    branch.
    
    ---------
    
    Signed-off-by: Adam Gutglick <adam@spiraldb.com>

---
+++ vortex-array/src/arrays/variant/vtable/operations.rs
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright the Vortex contributors
+
+use vortex_error::VortexResult;
+
+use crate::arrays::Variant;
+use crate::scalar::Scalar;
+use crate::vtable::OperationsVTable;
+
+impl OperationsVTable<Variant> for Variant {
+    fn scalar_at(
+        array: &<Variant as crate::vtable::VTable>::Array,
+        index: usize,
+    ) -> VortexResult<Scalar> {
+        array.child().scalar_at(index)
+    }
+}
