f34835222fa76b2839387e46cdf1a3efd0a1245a
Joe Isaacs <joe.isaacs@live.co.uk>
2026-03-24 16:28:37 +0000

    u
    Signed-off-by: Joe Isaacs <joe.isaacs@live.co.uk>

--- vortex-array/src/arrays/dict/vtable/mod.rs
+++ vortex-array/src/arrays/dict/vtable/mod.rs
@@ -225,7 +225,10 @@ impl VTable for Dict {
 
         let DictArrayParts { codes, values, .. } = Arc::unwrap_or_clone(array).into_parts();
 
-        let codes = codes.try_into::<Primitive>().ok().expect("must be primitive");
+        let codes = codes
+            .try_into::<Primitive>()
+            .ok()
+            .expect("must be primitive");
         debug_assert!(values.is_canonical());
         // TODO: add canonical owned cast.
         let values = values.to_canonical()?;
