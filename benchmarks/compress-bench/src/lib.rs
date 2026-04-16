bee85801d95f743d5dad684314539b0cd9d1fcaa
Joe Isaacs <joe.isaacs@live.co.uk>
2026-04-16 14:18:18 -0400

    fix
    Signed-off-by: Joe Isaacs <joe.isaacs@live.co.uk>

--- benchmarks/compress-bench/src/lib.rs
+++ benchmarks/compress-bench/src/lib.rs
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: Copyright the Vortex contributors
 
-use ::vortex::array::VortexSessionExecute;
-use ::vortex::array::arrays::chunked::ChunkedArrayExt;
-use ::vortex::array::arrow::ArrowArrayExecutor;
 #[cfg(feature = "lance")]
 pub use lance_bench::compress::LanceCompressor;
 pub mod parquet;
