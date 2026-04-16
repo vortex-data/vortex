aca6251afb7e5d0f71fa4292598fc3a2eaaa4110
Joe Isaacs <joe.isaacs@live.co.uk>
2026-04-16 19:23:08 +0100

    break: remove `try_from` struct array to record batch (#7488)
    We cannot deprecate this TryFrom so it has to be removed
    Use: ArrowArrayExecutor::execute_record_batch
    
    ---------
    
    Signed-off-by: Joe Isaacs <joe.isaacs@live.co.uk>

--- benchmarks/compress-bench/src/lib.rs
+++ benchmarks/compress-bench/src/lib.rs
@@ -1,33 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: Copyright the Vortex contributors
 
-use std::sync::Arc;
-
-use ::vortex::array::arrays::ChunkedArray;
-use ::vortex::array::arrays::chunked::ChunkedArrayExt;
-use ::vortex::array::arrays::listview::recursive_list_from_list_view;
-use arrow_array::RecordBatch;
-use arrow_schema::Schema;
 #[cfg(feature = "lance")]
 pub use lance_bench::compress::LanceCompressor;
 pub mod parquet;
 pub mod vortex;
-
-pub fn chunked_to_vec_record_batch(
-    chunked: ChunkedArray,
-) -> anyhow::Result<(Vec<RecordBatch>, Arc<Schema>)> {
-    assert!(chunked.nchunks() > 0, "empty chunks");
-
-    let batches = chunked
-        .iter_chunks()
-        .map(|array| {
-            // TODO(connor)[ListView]: The rust Parquet implementation does not support writing
-            // `ListView` to Parquet files yet.
-            let converted_array = recursive_list_from_list_view(array.clone())?;
-            Ok(RecordBatch::try_from(&converted_array)?)
-        })
-        .collect::<anyhow::Result<Vec<_>>>()?;
-
-    let schema = batches[0].schema();
-    Ok((batches, schema))
-}
