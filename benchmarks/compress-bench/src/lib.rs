1c1e756f9b3e84907e320cff45449cd11c27b29a
Joe Isaacs <joe.isaacs@live.co.uk>
2026-04-16 13:43:33 -0400

    fix
    Signed-off-by: Joe Isaacs <joe.isaacs@live.co.uk>

--- benchmarks/compress-bench/src/lib.rs
+++ benchmarks/compress-bench/src/lib.rs
@@ -1,18 +1,11 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: Copyright the Vortex contributors
 
-use std::sync::Arc;
-
 use ::vortex::array::VortexSessionExecute;
-use ::vortex::array::arrays::ChunkedArray;
 use ::vortex::array::arrays::chunked::ChunkedArrayExt;
-use ::vortex::array::arrays::listview::recursive_list_from_list_view;
 use ::vortex::array::arrow::ArrowArrayExecutor;
-use arrow_array::RecordBatch;
-use arrow_schema::Schema;
 #[cfg(feature = "lance")]
 pub use lance_bench::compress::LanceCompressor;
-use vortex_bench::SESSION;
 
 pub mod parquet;
 pub mod vortex;
