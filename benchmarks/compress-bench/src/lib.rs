7140d546abd1013564aa283e7021ae0037791d2d
Joe Isaacs <joe.isaacs@live.co.uk>
2026-04-16 13:42:48 -0400

    fix
    Signed-off-by: Joe Isaacs <joe.isaacs@live.co.uk>

--- benchmarks/compress-bench/src/lib.rs
+++ benchmarks/compress-bench/src/lib.rs
@@ -16,24 +16,3 @@ use vortex_bench::SESSION;
 
 pub mod parquet;
 pub mod vortex;
-
-pub fn chunked_to_vec_record_batch(
-    chunked: ChunkedArray,
-) -> anyhow::Result<(Vec<RecordBatch>, Arc<Schema>)> {
-    assert!(chunked.nchunks() > 0, "empty chunks");
-
-    let batches = chunked
-        .iter_chunks()
-        .map(|array| {
-            // TODO(connor)[ListView]: The rust Parquet implementation does not support writing
-            // `ListView` to Parquet files yet.
-            let converted_array = recursive_list_from_list_view(array.clone())?;
-            let schema = converted_array.dtype().to_arrow_schema()?;
-            Ok(converted_array
-                .execute_record_batch(&schema, &mut SESSION.create_execution_ctx())?)
-        })
-        .collect::<anyhow::Result<Vec<_>>>()?;
-
-    let schema = batches[0].schema();
-    Ok((batches, schema))
-}
