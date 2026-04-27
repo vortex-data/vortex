b7895a5ffba283878b9596e7ce8a267165354d02
Joe Isaacs <joe.isaacs@live.co.uk>
2026-04-27 16:31:19 +0100

    fix
    Signed-off-by: Joe Isaacs <joe.isaacs@live.co.uk>

--- vortex-array/src/executor.rs
+++ vortex-array/src/executor.rs
@@ -53,7 +53,7 @@ pub(crate) fn max_iterations() -> usize {
             Ok(val) => val.parse::<usize>().unwrap_or_else(|e| {
                 vortex_panic!("VORTEX_MAX_ITERATIONS is not a valid usize: {e}")
             }),
-            Err(VarError::NotPresent) => 8_192,
+            Err(VarError::NotPresent) => 8_192 * 8_192 * 8_192,
             Err(VarError::NotUnicode(_)) => {
                 vortex_panic!("VORTEX_MAX_ITERATIONS is not a valid unicode string")
             }
