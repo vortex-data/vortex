aedb153c03c91031339ac2cd72207420e6c0fa65
Andrew Duffy <andrew@a10y.dev>
2026-05-01 14:45:07 -0400

    fix comment
    Signed-off-by: Andrew Duffy <andrew@a10y.dev>

--- vortex-duckdb/src/exporter/geo.rs
+++ vortex-duckdb/src/exporter/geo.rs
@@ -8,7 +8,7 @@ use vortex_geo::extension::WellKnownBinaryData;
 
 use crate::exporter::ColumnExporter;
 
-/// Create a new exporter for geospatial data stored in one of the supported spatial formats.
+/// Create a new exporter for geospatial data stored as Well-Known Binary (WKB) format.
 pub(crate) fn new_wkb_exporter(
     array: WellKnownBinaryData,
     ctx: &mut ExecutionCtx,
