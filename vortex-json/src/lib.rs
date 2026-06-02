7a11901010759519f2f4977d9158cbf1fe166069
Adam Gutglick <adam@spiraldb.com>
2026-06-02 14:28:59 +0100

    .
    Signed-off-by: Adam Gutglick <adam@spiraldb.com>

---
+++ vortex-json/src/lib.rs
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright the Vortex contributors
+
+#![warn(missing_docs)]
+#![warn(clippy::missing_docs_in_private_items)]
+#![warn(clippy::missing_errors_doc)]
+#![warn(clippy::missing_panics_doc)]
+#![warn(clippy::missing_safety_doc)]
+
+//! Extension type and related functionality for a JSON extension type for Vortex.
+
+mod dtype;
