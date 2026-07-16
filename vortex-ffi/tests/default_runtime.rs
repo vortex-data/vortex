34ad733838472e763bbf3ff2a849b022f25563be
Onur Satici <onur@spiraldb.com>
2026-07-16 16:00:42 +0100

    mirignore test
    Signed-off-by: Onur Satici <onur@spiraldb.com>

--- vortex-ffi/tests/default_runtime.rs
+++ vortex-ffi/tests/default_runtime.rs
@@ -8,6 +8,7 @@ use vortex_ffi::ffi_runtime;
 use vortex_ffi::vx_runtime_worker_count;
 
 #[test]
+#[cfg_attr(miri, ignore)]
 fn runtime_is_host_thread_driven_by_default() {
     assert_eq!(vx_runtime_worker_count(), 0);
 
